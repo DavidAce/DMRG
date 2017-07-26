8db6047e75408cdb8cd32681de3b9877900c97a1
DavidAce <kameboy@gmail.com>
2017-07-26 16:30:17 +0200

    fixing docs
    Former-commit-id: d388249d0041362a40a200c019de40d1fea65899

---
+++ docs/search/all_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['l_5flist',['L_list',['../classclass__storage.html#a9a36c1609ecd2cc6dfb9bec4c8369d4d',1,'class_storage']]],
+  ['lambda_5fab',['Lambda_AB',['../class_lambda___a_b.html',1,'']]],
+  ['lblock',['Lblock',['../classclass__superblock.html#a44835bbcd2237965741a07c6cbb702db',1,'class_superblock']]],
+  ['lblock_5flist',['Lblock_list',['../classclass__storage.html#a487d155d2a17e7742a973ec03b881d49',1,'class_storage']]]
+];
---
+++ docs/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mps',['MPS',['../classclass__superblock.html#a9989ee56695bf210091e4a668777b323',1,'class_superblock']]]
+];
