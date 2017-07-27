c52101489e93eb0982cab3a3a80d0d4b7bdadbfd
DavidAce <kameboy@gmail.com>
2017-07-27 17:59:07 +0200

    Documentation

---
+++ docs/search/functions_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['truncate',['truncate',['../classclass__superblock.html#afe9806bba365adb6e728ee87c9adb566',1,'class_superblock']]]
+];
---
+++ docs/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['model',['Model',['../namespace_model.html',1,'']]]
+];
