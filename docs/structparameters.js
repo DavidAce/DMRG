6f2d63c287faa95ab8a7ddd6d6c46afa6e693642
DavidAce <kameboy@gmail.com>
2017-08-04 12:04:22 +0200

    Documentation

---
+++ docs/structparameters.js
@@ -0,0 +1,16 @@
+var structparameters =
+[
+    [ "chi", "structparameters.html#afa9322fcb1eaf8fa8a59bbff17147a13", null ],
+    [ "chi_max", "structparameters.html#a553dc6520847d50eaacc9d34abb19d80", null ],
+    [ "delta_t", "structparameters.html#a0d0b672b278083c7ec42a56b20ee98af", null ],
+    [ "eigSteps", "structparameters.html#af697d1b1df575aaf78933c5feecdd6bf", null ],
+    [ "eigThreshold", "structparameters.html#a1d6a1d8c0fea3e1327871a2721fc2b01", null ],
+    [ "increasing_chi", "structparameters.html#a4b15740080c5fe4b2414170176d46795", null ],
+    [ "max_fdmrg_sweeps", "structparameters.html#a701bae067a0daea4df4952036bbd088e", null ],
+    [ "max_idmrg_length", "structparameters.html#a20b13550b072174f9981485917aa4f5f", null ],
+    [ "max_itebd_steps", "structparameters.html#a26e457f06683d96d4df989ba5fa650b6", null ],
+    [ "profiling", "structparameters.html#a7b5a20578ae4c319e3034f86ab5a500c", null ],
+    [ "SVDThreshold", "structparameters.html#af20971d1671b3c5f6eb7e662e162de9e", null ],
+    [ "time_prec", "structparameters.html#a9120ae166500f60e4765072c915eb11e", null ],
+    [ "verbosity", "structparameters.html#a26149e737925f98f84c64b02a7f35c9f", null ]
+];
