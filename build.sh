ebdf2601b89b0c4f0012df06538ab7cfb7bd9bea
DavidAce <kameboy@gmail.com>
2017-10-30 13:33:56 +0100

    fixed build script
    Former-commit-id: b9b0b810c8bb2503b437ebfce1354b45bd266482

--- build.sh
+++ build.sh
@@ -1,30 +1,4 @@
-#!/bin/bash 
-
-buildtype="Release"
-if [[ "$@" == *"ebug"* ]] 
-then
-	buildtype="Debug" 
-fi
-if [[ "$@" == *"lean"* ]] 
-then
-	echo "Cleaning build"
-	rm -rf build
-	exit 0 
-fi 
-if [[ "$HOSTNAME" == *"triolith"* ]] 
-then
-    echo "We're on triolith!";
-    module add cmake/3.6.1
-    module load_MPS buildenv-intel/2016-3
-    export CC=/software/apps/gcc/5.3.0/build01/bin/gcc
-    export CXX=/software/apps/gcc/5.3.0/build01/bin/g++ 
-else
-    echo "We're on my pc!" 
-fi 
-mkdir build 
-cd build 
-mkdir ${buildtype} 
-cd ${buildtype} 
-echo "Starting Build" 
-cmake -DCMAKE_BUILD_TYPE=${buildtype} ../../
-make
+#!/bin/sh
+cmake -E make_directory build/Release
+cd build/Release
+cmake -j4  -Bbuild/Release --build build -config Release ../../
