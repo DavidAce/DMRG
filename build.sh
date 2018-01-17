7565f16a653b09c8614f5a72a5ef845032a22ab5
DavidAce <kameboy@gmail.com>
2018-01-17 13:57:52 +0100

    correcting build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cmake -E make_directory build/Release
 cd build/Release
-cmake -Bbuild/Release --build build -config Release ../../
-make
+cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ../../
+cmake --build . --target DMRG++ -- -j 4
 
