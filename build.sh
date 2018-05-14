af2810c7e60a7b7f599f5596a2cd72c43746130e
DavidAce <kameboy@gmail.com>
2018-05-14 21:10:34 +0200

    Fixing build script for osx
    Former-commit-id: c97f6bee0eb9a9872b0a0e654fd5cc6b8416d274

--- build.sh
+++ build.sh
@@ -35,38 +35,37 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Either install gfortran (bundled with gcc) or install these libraries manually."
     fi
 
-    echo "Checking if gcc or clang is installed through brew..."
-     if brew ls llvm | grep -q 'clang++'; then
+    echo "Checking if libstdc++ is available"
+    standard_library=$(brew ls gcc | grep -e '/[0-9]/lib/gcc/[0-9]/libstdc++.a' | head -n 1)
+    if [ -z "${standard_library}" ];then
+        echo "Please install standard library available in gcc first"
+        echo "  brew install gcc"
+    fi
+
+    echo "Checking if clang is installed through brew..."
+    if brew ls llvm | grep -q 'clang++'; then
         c_compiler=$(brew ls llvm | grep -e '/bin/clang' | head -n 1)
         cxx_compiler=$(brew ls llvm | grep -e '/bin/clang++' | head -n 1)
-        versionnumber=$(${cxx_compiler} -dumpversion)
-        if [ "${versionnumber}" -lt "5" ]; then
-            echo "Clang version number: ${versionnumber} is too low."
-            exit
-        fi
-        echo "clang-${versionnumber} is installed"
+        echo "clang is installed"
 
     elif brew ls gcc | grep -q 'g++'; then
         c_compiler=$(brew ls gcc | grep -e '/bin/gcc-[0-9]' | head -n 1)
         cxx_compiler=$(brew ls gcc | grep  -e '/bin/g++-[0-9]' | head -n 1)
-        versionnumber=$(${cxx_compiler} -dumpversion)
-        if [ "${versionnumber}" -lt "7" ]; then
-            echo "GCC version number: ${versionnumber} is too low."
-            exit
-        fi
-        echo "gcc-${versionnumber} is installed"
+        echo "gcc is installed"
     else
         echo "Please install gcc (version 7 or higher) or gcc + llvm (version 6 or higher) through brew"
         echo "  brew install gcc"
         echo "  brew install llvm"
         exit
     fi
+    export LIBRARY_PATH=${standard_library}
     export CC=${c_compiler}
     export CXX=${cxx_compiler}
     export FC=${fortran_compiler}
     dcmake_c_compiler="-DCMAKE_C_COMPILER=${c_compiler}"
     dcmake_cxx_compiler="-DCMAKE_CXX_COMPILER=${cxx_compiler}"
     dcmake_fortran_compiler="-DCMAKE_Fortran_COMPILER=${fortran_compiler}"
+    dcmake_library_path="-DCMAKE_LIBRARY_PATH=${standard_library}"
 else
         echo "Could not identify OS"
         exit
@@ -76,5 +75,5 @@ fi
 echo "Starting Build"
 cmake -E make_directory build/${buildtype}
 cd build/${buildtype}
-cmake  ${dcmake_c_compiler} ${dcmake_cxx_compiler} ${dcmake_fortran_compiler} -DCMAKE_BUILD_TYPE=${buildtype} -G "CodeBlocks - Unix Makefiles" ../../
+cmake  ${dcmake_c_compiler} ${dcmake_cxx_compiler} ${dcmake_fortran_compiler} ${dcmake_library_path} -DCMAKE_BUILD_TYPE=${buildtype} -G "CodeBlocks - Unix Makefiles" ../../
 cmake --build . --target DMRG++ -- -j 4
