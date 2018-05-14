041be671ac164a7f85d0ef64e57bb70f4747aa29
DavidAce <kameboy@gmail.com>
2018-05-14 20:48:05 +0200

    Fixing build script for osx
    Former-commit-id: cda8d68d8606efb43ab8d51bb80808a039ede4c4

--- build.sh
+++ build.sh
@@ -28,47 +28,45 @@ if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "OS: Mac OSX"
+    echo "Checking if fortran compiler is available"
+    fortran_compiler=$(brew ls gcc | grep -e '/bin/gfortran-[0-9]' | head -n 1)
+    if [ -z "$VAR" ];then
+        echo "Fortran compiler is not available. Automatic library installation (e.g. GSL and Arpack) will not work"
+        echo "Either install gfortran (bundled with gcc) or install these libraries manually."
+    fi
+
     echo "Checking if gcc or clang is installed through brew..."
      if brew ls llvm | grep -q 'clang++'; then
         c_compiler=$(brew ls llvm | grep -e '/bin/clang' | head -n 1)
-        cxx_compiler=$(brew ls gcc | grep -e '/bin/clang++' | head -n 1)
-        fortran_compiler=$(brew ls gcc | grep -e '/bin/gfortran-[0-9]' | head -n 1)
+        cxx_compiler=$(brew ls llvm | grep -e '/bin/clang++' | head -n 1)
         versionnumber=$(${cxx_compiler} -dumpversion)
         if [ "${versionnumber}" -lt "5" ]; then
             echo "Clang version number: ${versionnumber} is too low."
             exit
         fi
         echo "clang-${versionnumber} is installed"
-        export CC=${c_compiler}
-        export CXX=${cxx_compiler}
-        export FC=${fortran_compiler}
-        dcmake_c_compiler="-DCMAKE_C_COMPILER=${c_compiler}"
-        dcmake_cxx_compiler="-DCMAKE_CXX_COMPILER=${cxx_compiler}"
-        dcmake_fortran_compiler="-DCMAKE_Fortran_COMPILER=${fortran_compiler}"
+
     elif brew ls gcc | grep -q 'g++'; then
         c_compiler=$(brew ls gcc | grep -e '/bin/gcc-[0-9]' | head -n 1)
         cxx_compiler=$(brew ls gcc | grep  -e '/bin/g++-[0-9]' | head -n 1)
-        fortran_compiler=$(brew ls gcc | grep -e '/bin/gfortran-[0-9]' | head -n 1)
         versionnumber=$(${cxx_compiler} -dumpversion)
         if [ "${versionnumber}" -lt "7" ]; then
             echo "GCC version number: ${versionnumber} is too low."
             exit
         fi
         echo "gcc-${versionnumber} is installed"
-        export CC=${c_compiler}
-        export CXX=${cxx_compiler}
-        export FC=${fortran_compiler}
-        dcmake_c_compiler="-DCMAKE_C_COMPILER=${c_compiler}"
-        dcmake_cxx_compiler="-DCMAKE_CXX_COMPILER=${cxx_compiler}"
-        dcmake_fortran_compiler="-DCMAKE_Fortran_COMPILER=${fortran_compiler}"
-
-
     else
         echo "Please install gcc (version 7 or higher) or gcc + llvm (version 6 or higher) through brew"
         echo "  brew install gcc"
         echo "  brew install llvm"
         exit
     fi
+    export CC=${c_compiler}
+    export CXX=${cxx_compiler}
+    export FC=${fortran_compiler}
+    dcmake_c_compiler="-DCMAKE_C_COMPILER=${c_compiler}"
+    dcmake_cxx_compiler="-DCMAKE_CXX_COMPILER=${cxx_compiler}"
+    dcmake_fortran_compiler="-DCMAKE_Fortran_COMPILER=${fortran_compiler}"
 else
         echo "Could not identify OS"
         exit
