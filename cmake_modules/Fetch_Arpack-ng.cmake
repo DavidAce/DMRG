96f42d309fcef2d07d19190286b8b66d456c47d5
DavidAce <kameboy@gmail.com>
2018-05-17 12:43:51 +0200

    Libs working fine. Code wonky again

--- cmake_modules/Fetch_Arpack-ng.cmake
+++ cmake_modules/Fetch_Arpack-ng.cmake
@@ -16,11 +16,23 @@ if(ARPACK_LIBRARIES)
     return()
 else()
     message(STATUS "Arpack-ng will be installed into ${INSTALL_DIRECTORY}/arpack-ng on first build.")
+
+    #####################################################################
+    ### Prepare lists with generator expressions, replacing all semicolons.
+    ### Otherwise, passing raw lists results  in only the first element
+    ### of the list to be passed.
+    ####################################################################
+    string (REPLACE ";" "$<SEMICOLON>" BLAS_LIBRARIES_GENERATOR     "${BLAS_LIBRARIES}")
+    string (REPLACE ";" "$<SEMICOLON>" LAPACK_LIBRARIES_GENERATOR   "${LAPACK_LIBRARIES}")
+    string (REPLACE ";" "$<SEMICOLON>" EXTRA_LDLAGS_GENERATOR       "${EXTRA_LDLAGS}")
+    ####################################################################
+
+
     include(ExternalProject)
     ExternalProject_Add(library_ARPACK
             GIT_REPOSITORY      https://github.com/opencollab/arpack-ng.git
-#            GIT_TAG             master # Latest version has problems with fortran linking. so stick with this version instead.
-            GIT_TAG             3.5.0 # Latest version has problems with fortran linking. so stick with this version instead.
+            GIT_TAG             master # Latest version has problems with fortran linking. so stick with this version instead.
+#            GIT_TAG             3.5.0 # Latest version has problems with fortran linking. so stick with this version instead.
             PREFIX              "${INSTALL_DIRECTORY}/arpack-ng"
             UPDATE_COMMAND ""
 #            BUILD_IN_SOURCE 1
@@ -34,12 +46,13 @@ else()
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
             -DCMAKE_INSTALL_MESSAGE=NEVER #Avoid unnecessary output to console
             -DCMAKE_C_FLAGS=-w
-            -DEXAMPLES=OFF
+            -DEXAMPLES=ON
             -DCMAKE_BUILD_TYPE=Release
-            -DMPI=OFF
-            -DBUILD_SHARED_LIBS=OFF
-            -DBLAS_LIBRARIES:PATH=${BLAS_LIBRARIES}
-            -DLAPACK_LIBRARIES:PATH=${LAPACK_LIBRARIES}
+            -DMPI=ON
+            -DBUILD_SHARED_LIBS=ON
+            -DBLAS_LIBRARIES=${BLAS_LIBRARIES_GENERATOR}
+            -DLAPACK_LIBRARIES=${LAPACK_LIBRARIES_GENERATOR}
+            -DEXTRA_LDLAGS=${EXTRA_LDLAGS_GENERATOR}
             DEPENDS blas lapack
             )
 
@@ -47,7 +60,7 @@ else()
     set(ARPACK_INCLUDE_DIRS ${INSTALL_DIR}/include)
     add_library(arpack UNKNOWN IMPORTED)
     set_target_properties(arpack PROPERTIES
-            IMPORTED_LOCATION ${INSTALL_DIR}/lib/libarpack${CMAKE_STATIC_LIBRARY_SUFFIX}
+            IMPORTED_LOCATION ${INSTALL_DIR}/lib/libarpack${CMAKE_SHARED_LIBRARY_SUFFIX}
             INTERFACE_LINK_LIBRARIES "${GFORTRAN_LIB};${BLAS_LIBRARIES};${LAPACK_LIBRARIES}"
             INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
     add_dependencies(arpack library_ARPACK)
--- cmake_modules/Find_dont_install_INTELMKL.cmake
+++ cmake_modules/Find_dont_install_INTELMKL.cmake
@@ -3,36 +3,56 @@
 # If you want to use Eigen and MKL you need to add #define EIGEN_USE_MKL_ALL before including <Eigen..>
 # Adding a -DEIGEN_USE_MKL_ALL here may conflict with arpack++
 
-set(MKL_USE_STATIC_LIBS OFF)
+set(MKL_USE_STATIC_LIBS ON)
 set(MKL_MULTI_THREADED OFF)
-set(MKL_USE_SINGLE_DYNAMIC_LIBRARY ON)
+set(MKL_USE_SINGLE_DYNAMIC_LIBRARY OFF)
 find_package(MKL)
 if (MKL_FOUND)
+
+    # Remove the libmkl_intel_lp64 library, which is only used when compiling with Intel Fortran.
+    list(REMOVE_ITEM MKL_LIBRARIES ${MKL_INTEL_LP64_LIBRARY})
+    list(INSERT MKL_LIBRARIES 0 ${MKL_GF_LP64_LIBRARY})
+    list(REMOVE_DUPLICATES MKL_LIBRARIES)
+
     add_definitions(-DMKL_AVAILABLE)
-    set(MKL_FLAGS -v -m64 -I${MKLROOT}/include)
-    set(MKL_LFLAGS -v -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_rt -lpthread -lm -ldl)
-    add_library(mkl SHARED IMPORTED)
+    set(MKL_FLAGS -m64 -I${MKL_ROOT}/include)
+    set(MKL_LFLAGS -L${MKL_ROOT}/lib/intel64 -lmkl_gf_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl)
+
+    # Make a handle library for convenience. This "mkl" library is available throughout this cmake project later.
+    add_library(mkl INTERFACE)
     set_target_properties(mkl PROPERTIES
-            IMPORTED_LOCATION "${MKL_LIBRARIES}"
-            INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIR}"
-            LINK_FLAGS "${MKL_LFLAGS}"
-            COMPILE_FLAGS "${MKL_FLAGS}"
+            INTERFACE_LINK_LIBRARY "${MKL_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORY "${MKL_INCLUDE_DIR}"
+            INTERFACE_LINK_FLAGS "${MKL_LFLAGS}"
+            INTERFACE_COMPILE_OPTIONS "${MKL_FLAGS}"
             )
-    target_link_libraries(${PROJECT_NAME} PUBLIC mkl)
+    target_link_libraries(${PROJECT_NAME} PUBLIC mkl ${MKL_LIBRARIES} ${MKL_LFLAGS})
+    target_compile_options(${PROJECT_NAME} PUBLIC ${MKL_FLAGS})
+    target_include_directories(${PROJECT_NAME} PUBLIC ${MKL_INCLUDE_DIR})
+
+
+    # BLAS and LAPACK are included in the MKL.
+    set(BLAS_LIBRARIES ${MKL_LIBRARIES})
+    set(LAPACK_LIBRARIES ${MKL_LIBRARIES})
+    set(EXTRA_LDLAGS ${MKL_LFLAGS}) #This one is needed if any sub projects wants to link its own stuff using MKL. For instance, arpack-ng.
+
 
     # Make the rest of the build structure aware of blas and lapack included in MKL.
     add_library(blas INTERFACE)
     set_target_properties(blas PROPERTIES
-            INTERFACE_LINK_LIBRARIES "${MKL_LIBRARIES}")
+            INTERFACE_LINK_LIBRARIES        "${BLAS_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORY     "${MKL_INCLUDE_DIR}"
+            INTERFACE_LINK_FLAGS            "${MKL_LFLAGS}"
+            INTERFACE_COMPILE_OPTIONS       "${MKL_FLAGS}"
+            )
 
     add_library(lapack INTERFACE)
     set_target_properties(lapack PROPERTIES
-            INTERFACE_LINK_LIBRARIES "${MKL_LIBRARIES}")
-
-
-    set(BLAS_LIBRARIES   ${MKL_LIBRARIES})
-    set(LAPACK_LIBRARIES ${MKL_LIBRARIES})
-
+            INTERFACE_LINK_LIBRARIES        "${LAPACK_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORY     "${MKL_INCLUDE_DIR}"
+            INTERFACE_LINK_FLAGS            "${MKL_LFLAGS}"
+            INTERFACE_COMPILE_OPTIONS       "${MKL_FLAGS}"
+            )
 
 
     #   Test features
@@ -62,6 +82,7 @@ if (MKL_FOUND)
     message("MKL_FLAGS                                 : ${MKL_FLAGS}" )
     message("MKL_LFLAGS                                : ${MKL_LFLAGS}" )
     message("MKL_ROOT                                  : ${MKL_ROOT}" )
+    message("MKLROOT                                   : ${MKLROOT}" )
     message("MKL_USE_SINGLE_DYNAMIC_LIBRARY            : ${MKL_USE_SINGLE_DYNAMIC_LIBRARY}" )
     message("MKL_USE_STATIC_LIBS                       : ${MKL_USE_STATIC_LIBS}" )
     message("BLAS_LIBRARIES                            : ${BLAS_LIBRARIES}" )
@@ -71,9 +92,20 @@ if (MKL_FOUND)
 endif()
 
 
+#
 #get_cmake_property(_variableNames VARIABLES)
 #foreach (_variableName ${_variableNames})
-#    #            if("${_variableName}" MATCHES "HDF5")
-#    message(STATUS "${_variableName}=${${_variableName}}")
-#    #            endif()
+#    if("${_variableName}" MATCHES "BLAS"
+#            OR "${_variableName}" MATCHES "blas"
+#            OR "${_variableName}" MATCHES "LAPACK"
+#            OR "${_variableName}" MATCHES "lapack")
+#        message(STATUS "${_variableName}=${${_variableName}}")
+#    endif()
 #endforeach()
+
+#    get_cmake_property(_variableNames VARIABLES)
+#    foreach (_variableName ${_variableNames})
+#        if("${_variableName}" MATCHES "MKL" OR "${_variableName}" MATCHES "mkl")
+#            message(STATUS "${_variableName}=${${_variableName}}")
+#        endif()
+#    endforeach()
