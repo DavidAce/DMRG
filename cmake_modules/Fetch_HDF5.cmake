70ab9cd179d3dab109dde24acf15960e8a9b0347
DavidAce <kameboy@gmail.com>
2018-05-18 05:07:25 +0200

    Linking hdf5 in trusty
    Former-commit-id: 6ba7dac859d8a5c6d187795b6c2ab87cac903d23

--- cmake_modules/Fetch_HDF5.cmake
+++ cmake_modules/Fetch_HDF5.cmake
@@ -40,6 +40,16 @@ if(HDF5_FOUND AND HDF5_LIBRARIES AND HDF5_CXX_LIBRARIES AND HDF5_HL_LIBRARIES AN
     set(HDF5_CXX_LIBRARIES     ${HDF5_CXX_LIBRARY_hdf5_cpp})
     set(HDF5_HL_CXX_LIBRARIES  ${HDF5_CXX_LIBRARY_hdf5_hl_cpp})
 
+
+    set(HDF5_LINKER_FLAGS ${HDF5_CXX_LIBRARY_NAMES})
+    list(REMOVE_ITEM HDF5_LINKER_FLAGS hdf5_cpp hdf5)
+
+    add_definitions(${HDF5_DEFINITIONS})
+    # Add convenience libraries
+    add_library(hdf5::hdf5           STATIC IMPORTED)
+    add_library(hdf5::hdf5_hl        STATIC IMPORTED)
+    add_library(hdf5::hdf5_cpp       STATIC IMPORTED)
+    add_library(hdf5::hdf5_hl_cpp    STATIC IMPORTED)
     get_cmake_property(_variableNames VARIABLES)
     foreach (_variableName ${_variableNames})
         if("${_variableName}" MATCHES "HDF5" OR "${_variableName}" MATCHES "hdf5")
@@ -47,13 +57,6 @@ if(HDF5_FOUND AND HDF5_LIBRARIES AND HDF5_CXX_LIBRARIES AND HDF5_HL_LIBRARIES AN
         endif()
     endforeach()
 
-    set(HDF5_LINKER_FLAGS $HDF5_CXX_LIBRARY_NAMES})
-    add_definitions(${HDF5_DEFINITIONS})
-    # Add convenience libraries
-    add_library(hdf5::hdf5           STATIC IMPORTED)
-    add_library(hdf5::hdf5_hl        STATIC IMPORTED)
-    add_library(hdf5::hdf5_cpp       STATIC IMPORTED)
-    add_library(hdf5::hdf5_hl_cpp    STATIC IMPORTED)
 else()
     message(STATUS "HDF5 will be installed into ${INSTALL_DIRECTORY}/hdf5 on first build.")
 
