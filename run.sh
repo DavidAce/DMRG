163c5a1a1a0ee731c7743adee50a088cc1b7db9f
DavidAce <kameboy@gmail.com>
2018-05-18 17:25:38 +0200

    Endless bugs
    Former-commit-id: 8ed71720c54544220c4bcbe232f0785c2abb8be7

--- run.sh
+++ run.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 PROGNAME=$0
-default_mode="Release"
-default_threads="2"
-default_file=""
+
 usage() {
   cat << EOF >&2
-Usage: $PROGNAME [-t <target>][-m <mode>] [-j <num_threads>] [-i <input_file>]
+Usage: $PROGNAME [-t <target>][-m <mode>] [-f <input_file>]
 
 -t <target>      : DMRG++    | all   | any test target
 -m <mode>        : Release   | Debug
--j <num_threads> : Number of threads used by CMake
--i <input_file>  : Full or relative path to the input file
+-f <input_file>  : Full or relative path to the input file
 EOF
   exit 1
 }
 
-mode=$default_mode
-threads=$default_threads
-file=$default_file
-while getopts t:m:j:i: o; do
+target="all"
+mode="Release"
+file="input.cfg"
+
+while getopts t:m:f: o; do
   case $o in
     (t) target=$OPTARG;;
     (m) mode=$OPTARG;;
-    (j) threads=$OPTARG;;
-    (i) file=$OPTARG;;
+    (f) file=$OPTARG;;
     (*) usage
   esac
 done
@@ -31,4 +28,4 @@ shift "$((OPTIND - 1))"
 
 
 
-./build/${mode}/DMRG++ $file
+./build/$mode/$target $file
