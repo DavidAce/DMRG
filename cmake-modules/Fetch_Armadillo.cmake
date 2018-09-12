549adbdf7bcc0ec225beefd941a5b51ddc0d98cf
DavidAce <kameboy@gmail.com>
2018-09-12 02:32:55 +0200

    Merged new feature from mbl_selfdual_ising

--- cmake-modules/Fetch_Armadillo.cmake
+++ cmake-modules/Fetch_Armadillo.cmake
@@ -1,7 +1,19 @@
 
 # Try finding Armadillo in system
 find_package(Armadillo)
-if(ARMADILLO_FOUND)
+
+if (NOT ARMADILLO_FOUND)
+# Try finding armadillo as module library
+find_library(ARMADILLO_LIBRARIES
+        NAMES armadillo
+        PATHS "$ENV{ARMADILLO_DIR}/lib"
+        )
+find_path(ARMADILLO_INCLUDE_DIRS
+        NAMES armadillo
+        PATHS "$ENV{ARMADILLO_DIR}/include"
+        )
+endif()
+if(ARMADILLO_FOUND OR ARMADILLO_LIBRARIES)
     message(STATUS "ARMADILLO found in system: ${ARMADILLO_LIBRARIES}")
     add_library(armadillo STATIC IMPORTED)
 #else()
@@ -17,6 +29,15 @@ endif()
 # Install from source if not found
 if(NOT ARMADILLO_LIBRARIES)
     message(STATUS "ARMADILLO will be installed into ${INSTALL_DIRECTORY}/armadillo on first build.")
+    #####################################################################
+    ### Prepare lists with generator expressions, replacing all semicolons.
+    ### Otherwise, passing raw lists results  in only the first element
+    ### of the list to be passed.
+    ####################################################################
+    string (REPLACE ";" "$<SEMICOLON>" BLAS_LIBRARIES_GENERATOR     "${BLAS_LIBRARIES};${EXTRA_LDLAGS}")
+    string (REPLACE ";" "$<SEMICOLON>" LAPACK_LIBRARIES_GENERATOR   "${LAPACK_LIBRARIES}")
+    string (REPLACE ";" "$<SEMICOLON>" EXTRA_LDLAGS_GENERATOR       "${EXTRA_LDLAGS}")
+    ####################################################################
 
     include(ExternalProject)
     ExternalProject_Add(library_ARMADILLO
@@ -32,9 +53,9 @@ if(NOT ARMADILLO_LIBRARIES)
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
             -DARMA_USE_LAPACK=true
             -DARMA_USE_BLAS=true
-            -DBLAS_LIBRARIES=${BLAS_LIBRARIES}
-            -DLAPACK_LIBRARY=${LAPACK_LIBRARIES}
-            -DLAPACK_NAMES=${LAPACK_LIBRARIES}
+            -DBLAS_LIBRARIES=${BLAS_LIBRARIES_GENERATOR}
+            -DLAPACK_LIBRARY=${LAPACK_LIBRARIES_GENERATOR}
+            -DLAPACK_NAMES=${LAPACK_LIBRARIES_GENERATOR}
             -DDETECT_HDF5=OFF
             DEPENDS arpack blas lapack gfortran
             )
