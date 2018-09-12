8a2471f8f6634666ad177cc48d53f13ba865ed6e
DavidAce <kameboy@gmail.com>
2018-09-12 02:30:14 +0200

    fixing build scripts
    Former-commit-id: 254b043d0eb82c225d31e0110bcd4342a0f8adc5

--- cmake-modules/Fetch_OpenBLAS.cmake
+++ cmake-modules/Fetch_OpenBLAS.cmake
@@ -13,61 +13,70 @@ message(STATUS "SEARCHING FOR OpenBLAS IN SYSTEM...")
 set(BLA_VENDOR OpenBLAS)
 set(BLAS_VERBOSE OFF)
 set(BLA_STATIC ON)
-find_package(BLAS)
-find_package(LAPACK)
-#set(BLA_VENDOR OpenBLAS)
-#find_package(LAPACK)
 
-if (NOT BLAS_openblas_LIBRARY OR NOT LAPACK_openblas_LIBRARY)
+
+if (EXISTS "$ENV{BLAS_DIR}")
     # Try finding armadillo as module library
+    message(STATUS "Attempting to find BLAS and LAPACK from environment modules.")
     find_library(BLAS_openblas_LIBRARY
             NAMES libopenblas.a
             PATHS "$ENV{BLAS_DIR}/lib"
             NO_DEFAULT_PATH
             )
+
+    find_path(BLAS_INCLUDE_DIRS
+            NAMES openblas_config.h
+            PATHS "$ENV{BLAS_DIR}/include"
+            NO_DEFAULT_PATH
+            )
+    if (BLAS_openblas_LIBRARY)
+        set(OpenBLAS_MULTITHREADED 1 )
+    endif()
+else()
+    find_library(BLAS_openblas_LIBRARY
+            NAMES libopenblas.a
+            PATHS "/usr/lib/x86_64-linux-gnu"
+            NO_DEFAULT_PATH
+            )
+#    find_library(LAPACK_openblas_LIBRARY
+#            NAMES liblapacke.a
+#            PATHS "/usr/lib/x86_64-linux-gnu"
+#            NO_DEFAULT_PATH
+#            )
     find_path(BLAS_INCLUDE_DIRS
             NAMES cblas.h
-            PATHS "$ENV{BLAS_DIR}/include"
+            PATHS "/usr/include" "/usr/include/x86_64-linux-gnu"
             NO_DEFAULT_PATH
             )
-
-    find_library(LAPACK_openblas_LIBRARY
-            NAMES libopenblas.a
-            PATHS "$ENV{BLAS_DIR}/lib"
-            NO_DEFAULT_PATH
-            )
-    find_path(LAPACK_INCLUDE_DIRS
-            NAMES cblas.h
-            PATHS "$ENV{BLAS_DIR}/include"
-            NO_DEFAULT_PATH
-            )
-    if (BLAS_openblas_LIBRARY AND LAPACK_openblas_LIBRARY)
-        set(BLAS_FOUND 1)
-        set(LAPACK_FOUND 1)
+#    find_path(LAPACK_INCLUDE_DIRS
+#            NAMES lapacke.h
+#            PATHS "/usr/include" "/usr/include/x86_64-linux-gnu"
+#            NO_DEFAULT_PATH
+#            )
+    if (BLAS_openblas_LIBRARY)
+        set(OpenBLAS_MULTITHREADED 1 )
     endif()
 endif()
 
 
 
+if(BLAS_openblas_LIBRARY)
+    message(STATUS "BLAS FOUND    : ${BLAS_openblas_LIBRARY}")
+    message(STATUS "BLAS INCLUDE  : ${BLAS_INCLUDE_DIRS}")
 
 
-if(BLAS_openblas_LIBRARY AND LAPACK_openblas_LIBRARY)
-    message(STATUS "BLAS FOUND IN SYSTEM: ${BLAS_openblas_LIBRARY}")
-    message(STATUS "                      ${BLAS_INCLUDE_DIRS}")
-    message(STATUS "LAPACK FOUND IN SYSTEM: ${LAPACK_openblas_LIBRARY}")
-    message(STATUS "                        ${LAPACK_INCLUDE_DIRS}")
+#    message(STATUS "LAPACK FOUND IN SYSTEM: ${LAPACK_openblas_LIBRARY}")
+#    message(STATUS "                        ${LAPACK_INCLUDE_DIRS}")
 
     #For convenience, define these variables
     add_library(blas STATIC IMPORTED)
     add_library(lapack STATIC IMPORTED)
     set(BLAS_LIBRARIES     ${BLAS_openblas_LIBRARY})
-    set(LAPACK_LIBRARIES   ${LAPACK_openblas_LIBRARY})
+    set(LAPACK_LIBRARIES   ${BLAS_openblas_LIBRARY})
+    set(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS})
     add_definitions(-DOpenBLAS_AVAILABLE)
 
-endif()
-#exit (1)
-
-if(NOT BLAS_FOUND OR NOT LAPACK_FOUND)
+else()
     message(STATUS "OpenBLAS will be installed into ${INSTALL_DIRECTORY}/OpenBLAS on first build.")
     set(OpenBLAS_MULTITHREADED 0 )
     set(OpenBLAS_USE_OPENMP 0) # Openmp doesnt work on clang it seems
@@ -84,19 +93,22 @@ if(NOT BLAS_FOUND OR NOT LAPACK_FOUND)
             INSTALL_COMMAND $(MAKE) PREFIX=<INSTALL_DIR> install
             DEPENDS gfortran
             )
-    #NO_LAPACKE=${OpenBLAS_USE_OTHER} NO_CBLAS=${OpenBLAS_USE_OTHER} BINARY64=1 QUIET_MAKE=0
     ExternalProject_Get_Property(library_OpenBLAS INSTALL_DIR)
     add_library(blas UNKNOWN IMPORTED)
     add_library(lapack UNKNOWN IMPORTED)
     add_dependencies(blas         library_OpenBLAS)
     add_dependencies(lapack       library_OpenBLAS)
     set(BLAS_INCLUDE_DIRS ${INSTALL_DIR}/include)
-    set(BLAS_LIBRARIES ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
-    set(LAPACK_LIBRARIES ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
-    set(BLAS_LIBRARIES_STATIC ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
-    set(LAPACK_LIBRARIES_STATIC ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
-    set(BLAS_LIBRARIES_SHARED ${INSTALL_DIR}/lib/libopenblas${CMAKE_SHARED_LIBRARY_SUFFIX})
-    set(LAPACK_LIBRARIES_SHARED ${INSTALL_DIR}/lib/libopenblas${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(LAPACK_INCLUDE_DIRS ${INSTALL_DIR}/include)
+
+    set(BLAS_LIBRARIES           ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(LAPACK_LIBRARIES         ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(BLAS_LIBRARIES_STATIC    ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(LAPACK_LIBRARIES_STATIC  ${INSTALL_DIR}/lib/libopenblas${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(BLAS_LIBRARIES_SHARED    ${INSTALL_DIR}/lib/libopenblas${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(LAPACK_LIBRARIES_SHARED  ${INSTALL_DIR}/lib/libopenblas${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(LAPACKE_FROM_OPENBLAS 1)
+
 endif()
 set_target_properties(blas PROPERTIES
         IMPORTED_LOCATION               "${BLAS_LIBRARIES}"
@@ -109,17 +121,21 @@ set_target_properties(blas PROPERTIES
 set_target_properties(lapack PROPERTIES
         IMPORTED_LOCATION               "${LAPACK_LIBRARIES}"
         INTERFACE_LINK_LIBRARIES        "${LAPACK_LIBRARIES};gfortran"
-        INTERFACE_INCLUDE_DIRECTORY     "${BLAS_INCLUDE_DIRS}"
+        INTERFACE_INCLUDE_DIRECTORY     "${LAPACK_INCLUDE_DIRS}"
         INTERFACE_LINK_FLAGS            ""
         INTERFACE_COMPILE_OPTIONS       ""
         )
 
 
-
 target_link_libraries(${PROJECT_NAME} PRIVATE blas lapack)
 target_include_directories(${PROJECT_NAME} PRIVATE ${BLAS_INCLUDE_DIRS})
-add_definitions(-DOpenBLAS_AVAILABLE)
-
+target_include_directories(${PROJECT_NAME} PRIVATE ${LAPACK_INCLUDE_DIRS})
 if(OpenBLAS_MULTITHREADED)
     target_link_libraries(${PROJECT_NAME} PRIVATE -lpthread)
+    list(APPEND BLAS_LIBRARIES        "-lpthread")
+    list(APPEND BLAS_LIBRARIES_STATIC "-lpthread")
+    list(APPEND BLAS_LIBRARIES_SHARED "-lpthread")
+    set(EXTRA_LDLAGS "-lpthread")
 endif()
+add_definitions(-DOpenBLAS_AVAILABLE)
+
