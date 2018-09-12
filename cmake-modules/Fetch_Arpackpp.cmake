c2107d8f57c0867572f808c083a65342030fc0b2
DavidAce <kameboy@gmail.com>
2018-09-12 13:18:05 +0200

    Tweaking build scripts

--- cmake-modules/Fetch_Arpackpp.cmake
+++ cmake-modules/Fetch_Arpackpp.cmake
@@ -12,9 +12,20 @@ find_path(ARPACKPP_INCLUDE_DIR
         PATHS /usr/include/arpack++ /usr/include /usr/local/
         )
 
+if (NOT ARPACKPP_LIBRARIES OR NOT ARPACKPP_INCLUDE_DIR)
+    # Try finding arpack as module library
+    set(ARPACKPP_LIBRARIES "")
+    message(STATUS "SEARCHING FOR ARPACK IN LOADED MODULES")
+    find_path(ARPACKPP_INCLUDE_DIR
+            NAMES ardsnsym.h arpack++
+            PATHS $ENV{ARPACKPP_DIR}/include
+            NO_DEFAULT_PATH
+            )
+endif()
+
 
 message(STATUS "Note that old versions of Arpack++ (e.g. the default in Ubuntu Trusty 14.04 LTS) may fail to compile, requiring '-fpermissive'.")
-if (ARPACKPP_LIBRARIES AND NOT "${OS_PROPERTIES}" MATCHES "trusty" )
+if (ARPACKPP_LIBRARIES OR ARPACKPP_INCLUDE_DIR AND NOT "${OS_PROPERTIES}" MATCHES "trusty" )
     message(STATUS "Arpack++ library found in system: ${ARPACKPP_LIBRARIES}")
     message(STATUS "Arpack++ include found in system: ${ARPACKPP_INCLUDE_DIR}")
     add_library(arpack++ INTERFACE)
@@ -38,7 +49,7 @@ else()
             ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include && find <INSTALL_DIR>/include -maxdepth 1 -type l -delete &&
             ${CMAKE_COMMAND} -E create_symlink <SOURCE_DIR>/include <INSTALL_DIR>/include/arpack++
             DEPENDS blas lapack arpack gfortran
-    )
+            )
 
     ExternalProject_Get_Property(library_ARPACK++ INSTALL_DIR)
     add_library(arpack++ INTERFACE)
