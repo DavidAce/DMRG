539224afcec5b4d6fd28909c06a506217e76c38f
DavidAce <kameboy@gmail.com>
2018-10-23 14:10:42 +0200

    Added copy script -- Investigating sparcity
    Former-commit-id: 6d03413b27cce446f9c548503f9e456e545c59ce

--- source/general/class_eigsolver_armadillo.cpp
+++ source/general/class_eigsolver_armadillo.cpp
@@ -5,11 +5,31 @@
 #include "class_eigsolver_armadillo.h"
 #include <armadillo>
 
-void class_eigsolver_armadillo::eig_sym(int L, std::complex<double> *hermitian_matrix, double *eigvals, std::complex<double> *eigvecs){
+
+void class_eigsolver_armadillo::subtract_phase(std::complex<double>  *eigvecs, int L) {
+        using namespace std::complex_literals;
+        auto uL = static_cast<unsigned int> (L);
+        arma::cx_mat arma_eigvecs(eigvecs,uL,uL,false,false);
+        for (int i = 0; i < L; i++) {
+            auto begin = arma_eigvecs.begin() + i * L;
+            auto end = begin + L;
+            std::complex<double> inv_phase = -1.0i * std::arg(arma_eigvecs[i * L]);
+            std::complex<double> exp_inv_phase = std::exp(inv_phase);
+            std::transform(begin, end, begin,
+                           [exp_inv_phase](std::complex<double> num) -> std::complex<double>
+                           { return (num * exp_inv_phase); });
+        }
+}
+
+
+void class_eigsolver_armadillo::eig_sym(int L, std::complex<double> *hermitian_matrix, double *eigvals, std::complex<double> *eigvecs, bool remove_phase){
     // for matrices with complex elements
     auto uL = static_cast<unsigned int> (L);
     arma::cx_mat arma_matrix(hermitian_matrix, uL, uL ,false, false);
     arma::vec    arma_eigvals(eigvals,uL,false,false);
     arma::cx_mat arma_eigvecs(eigvecs,uL,uL,false,false);
     arma::eig_sym(arma_eigvals, arma_eigvecs, arma_matrix, "dc");
+    if (remove_phase){
+        subtract_phase(eigvecs, L);
+    }
 }
