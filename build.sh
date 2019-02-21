98fd1d7a4f8dfe3917a07d452c808668c160bbbd
DavidAce <kameboy@gmail.com>
2019-02-21 21:34:52 +0100

    Adapting build script for Tetralith
    Former-commit-id: ce536cfeb48d10fadba446946aca60e1a43f24f9

--- build.sh
+++ build.sh
@@ -111,6 +111,8 @@ if [[ "$HOSTNAME" == *"tetralith"* ]];then
     module load zlib/1.2.8
     export CC=gcc
     export CXX=g++
+    LD_LIBRARY_PATH=/usr/local/lib64/:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH
 elif [[ "$HOSTNAME" == *"anderson"* ]];then
     module load arpack-ng_${march}_3.6.2
     module load arpack++
