471e0f0816a5daff3ea7f7e788f6af16ef5ef4be
DavidAce <kameboy@gmail.com>
2019-02-21 23:52:56 +0100

    Wrote submit script for tetralith
    Former-commit-id: c0fca83edd98e6564716d74b439731141c392000

--- batch_scripts/run_tetralith.sh
+++ batch_scripts/run_tetralith.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-#SBATCH --job-name               =DMRG
-#SBATCH --time                   =0-1:30:00
-#SBATCH --time-min               =0-0:30:00
-#SBATCH --mem-per-cpu            =2000
-#SBATCH --cpus-per-task          =1
-#SBATCH --ntasks                 =1
-#SBATCH --ntasks-per-core        =1
-#SBATCH --kill-on-invalid-dep    =yes
+#SBATCH --job-name=DMRG
+#SBATCH --time=0-1:30:00
+#SBATCH --time-min=0-0:30:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --kill-on-invalid-dep=yes
 
 echo "Running on tetralith"
 echo $@
