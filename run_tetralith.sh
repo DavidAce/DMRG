15b3d84a1c1a9a7b8bcc9b40093131837e3cd9cb
DavidAce <kameboy@gmail.com>
2019-02-21 22:42:39 +0100

    Adapting run script for Tetralith
    Former-commit-id: 4561fb438cf84c8105c2f9d3f180307c8964917f

--- run_tetralith.sh
+++ run_tetralith.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
 #SBATCH --exclusive
-#SBATCH --job-name              = DMRG
-#SBATCH --time                  = 0-1:30:00
-#SBATCH --time-min              = 0-0:30:00
-#SBATCH --error                 = log.error
-#SBATCH --output                = log.out
-#SBATCH --mem-per-cpu           = 2000
-#SBATCH --cpus-per-task         = 1
-#SBATCH --ntasks                = 1
-#SBATCH --ntasks-per-core       = 1
-#SBATCH --kill-on-invalid-dep   =yes
-
-if [[ "$HOSTNAME" == *"tetralith"* ]];then
-    echo "Running on tetralith"
-    echo $@
-    exec=$1
-    file=$2
-    module load buildenv-gcc/2018a-eb
-    module load GCCcore/7.3.0
-    module load CMake/3.12.1
-    module load zlib/1.2.8
-    export CC=gcc
-    export CXX=g++
-    echo $LD_LIBRARY_PATH
-    LD_LIBRARY_PATH=$(gcc -print-file-name=libstdc++.so)
-    export LD_LIBRARY_PATH
-    export OMP_NUM_THREADS=1
+#SBATCH --job-name               DMRG
+#SBATCH --time                   0-1:30:00
+#SBATCH --time-min               0-0:30:00
+#SBATCH --error                  log.error
+#SBATCH --output                 log.out
+#SBATCH --mem-per-cpu            2G
+#SBATCH --cpus-per-task          1
+#SBATCH --ntasks                 1
+#SBATCH --ntasks-per-core        1
+#SBATCH --kill-on-invalid-dep    yes
 
 
-    $exec $file
-fi
+echo "Running on tetralith"
+echo $@
+exec=$1
+file=$2
+module load buildenv-gcc/2018a-eb
+module load GCCcore/7.3.0
+module load CMake/3.12.1
+module load zlib/1.2.8
+export CC=gcc
+export CXX=g++
+echo $LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$(gcc -print-file-name=libstdc++.so)
+export LD_LIBRARY_PATH
+export OMP_NUM_THREADS=1
+
+
+$exec $file
+
