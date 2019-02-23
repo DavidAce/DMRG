c261d6454c0d2861b17f765b08166f15cb03df42
DavidAce <kameboy@gmail.com>
2019-02-23 15:58:02 +0100

    Adding symlink support to copy results
    Former-commit-id: ea1fd037a5f5cfa67694eab507186340f0e74771

--- batch_scripts/print_num_outputs.sh
+++ batch_scripts/print_num_outputs.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-#find -L output/ -type d -print0 | while read -d '' -r dir; do
-#    files=("$dir"/*)
-#    printf "%5d files in directory %s\n" "${#files[@]}" "$dir"
-#done
 
-for i in $(find -L output  -maxdepth 1 -type d) ; do
+dir=$1
+for i in $(find -L $dir  -maxdepth 1 -type d) ; do
       echo -n $i": " ;
       (find $i -type f | wc -l) ;
 done
---
+++ batch_scripts/run_tetralith_bunch_parallel.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --exclusive
+#SBATCH --job-name=DMRG
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=0-2:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --kill-on-invalid-dep=yes
+
+
+exec=${1}
+bunchfile=${2}
+bunchbase=$(basename $bunchfile .txt)
+outdir=logs/$bunchbase
+mkdir -p $outdir
+
+cat $bunchfile | parallel  $exec {} ">" $outdir/{/.}.out
+
+
+# Explanation
+
+# cat $bunchfile will print all the lines in a bunchfile. Each line is in the bunchfile the path to an input file that
+# the executable exec takes as input argument. The lines look like "../input/L_12/mbl_232_blabla.cfg"
+
+# The lines are piped with "|" into GNU parallel, which takes a list as input argument. Each element of the list
+# is expanded with {}, and you can extract from {} a basename without extension  using {/.} See more in the manual for
+# parallel
+
+# For each line, parallel will run exec with that line as input argument. The output of each execution is piped with ">"
+# into file $outdir/{/.}.out   which will expand to something like  logs/bunch_0/mbl_232_blabla.out
+
+# There are many, many more arguments you can give to parallel, to control the number of cores, memory requirements per thread etc.
