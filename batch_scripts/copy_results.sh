3d4eac9642522b13cec89399858ce5d205e15177
DavidAce <kameboy@gmail.com>
2019-02-23 14:13:52 +0100

    Adding symlink support to copy results

--- batch_scripts/copy_results.sh
+++ batch_scripts/copy_results.sh
@@ -13,6 +13,7 @@ Usage              : $PROGNAME [-f <input_file>] [-h] [-m <mode>] [-t <target>]
 -s <source dir>    : Source relative to current dir (default = .)
 -t <target dir>    : Target directory (default = tmp)
 -u <user>          : User at target machine (default = david)
+-L                 : Follow symbolic links
 EOF
   exit 1
 }
@@ -21,16 +22,18 @@ default_pfx="/mnt/Barracuda/Projects/mbl_transition"
 default_src="."
 default_tgt="tmp"
 default_usr="david"
-dry_run=false
-while getopts a:dhp:s:t:u: o; do
+dry_run=""
+follow_sym=""
+while getopts a:dhp:s:t:u:L o; do
       case $o in
         (a) default_adr=$OPTARG;;
-        (d) dry_run=true;;
+        (d) dry_run="--dry-run";;
         (h) usage ;;
         (p) default_pfx=$OPTARG;;
         (s) default_src=$OPTARG;;
         (t) default_tgt=$OPTARG;;
         (u) default_usr=$OPTARG;;
+        (L) follow_sym="-L" ;;
         (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
         (?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
         (*) usage ;;
@@ -46,4 +49,4 @@ fi
 #if [ $OPTIND -eq 1 ]; then echo "No flags were passed"; usage ;exit 1; fi
 
 
-rsync -r --progress --update $default_src ${default_usr}@${default_adr}:${default_pfx}/${default_tgt}
+rsync -r $follow_sym $dry_run --progress --update $default_src ${default_usr}@${default_adr}:${default_pfx}/${default_tgt}
