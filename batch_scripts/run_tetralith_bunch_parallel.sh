96e0c1c2f0f48bbfd5791b1615b8d02a0647d321
DavidAce <kameboy@gmail.com>
2019-03-22 00:10:33 +0100

    Better handling of input arguments
    Former-commit-id: da5063d1d3f881c32ef434d7134d665275508b12

--- batch_scripts/run_tetralith_bunch_parallel.sh
+++ batch_scripts/run_tetralith_bunch_parallel.sh
@@ -15,7 +15,7 @@ bunchbase=$(basename $bunchfile .txt)
 outdir=logs/$bunchbase
 mkdir -p $outdir
 
-cat $bunchfile | parallel -n 2 --colsep ' ' $exec {0} {1} ">" $outdir/{/.}_{1}.out
+cat $bunchfile | parallel -n 2 --colsep ' ' $exec {1} {2} ">" $outdir/{/.}_{2}.out
 
 
 # Explanation
