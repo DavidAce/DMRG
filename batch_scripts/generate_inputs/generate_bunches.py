998d851339011aa47c1a0b8c48964e3e231aa0db
DavidAce <kameboy@gmail.com>
2019-03-21 15:09:27 +0100

    Added support for seed as command line argument

--- batch_scripts/generate_inputs/generate_bunches.py
+++ batch_scripts/generate_inputs/generate_bunches.py
@@ -7,8 +7,12 @@ os.chdir(parentpath)
 
 src_directory='input'
 tgt_directory='bunch'
-bunch_size = 800
+
+realizations = 5
+bunch_size = 5
 bunch_list = []
+seed_counter = 0
+
 if not os.path.exists(tgt_directory):
     os.makedirs(tgt_directory)
 
@@ -18,22 +22,29 @@ def chunks(l, n):
         # Create an index range for l of n items:
         yield l[i:i+n]
 
-current_bunch = []
+complete_list = []
 for dirName, subdirList, fileList in os.walk(src_directory):
     for src_filename in fileList:
-        current_bunch.append(dirName + '/' +src_filename)
+        for realization in range(realizations):
+            complete_list.append(dirName + '/' + src_filename + ' ' + str(seed_counter))
+            seed_counter = seed_counter + 1
 
-#current_bunch.sort()
-random.shuffle(current_bunch)
-bunch_list = chunks(current_bunch,bunch_size)
+random.shuffle(complete_list)
+bunch_list = chunks(complete_list, bunch_size)
 
-
+num_bunches = 0
 for i, bunch in enumerate(bunch_list):
     filepath = tgt_directory + '/bunch_' + str(i) + '.txt'
+    num_bunches = i+1
     with open(filepath, 'w') as file_handler:
         for item in bunch:
             file_handler.write("{}\n".format(item))
 
+print("Seeds        :   ", seed_counter)
+print("Bunches      :   ", num_bunches)
+print("Bunch size   :   ", bunch_size)
+print("Sim types    :   ", seed_counter/realizations)
+print("Realizations :   ", realizations)
 
 
 
--- batch_scripts/generate_inputs/generate_input_sweep.py
+++ batch_scripts/generate_inputs/generate_input_sweep.py
@@ -6,12 +6,11 @@ import numpy as np
 # while replacing the fields stated in find_replace.
 
 template_filename = 'input_template.cfg'
-basename    = 'mbl_'
+basename    = 'mbl'
 location    = "../input"
 os.makedirs(location, exist_ok=True)
 
 
-realizations = np.arange(0,2000,1)  # Number of copies for each point on the sweep
 sites        = np.linspace(12,40,8,dtype=int)
 lambdas      = np.linspace(0,0.2,3)
 deltas       = [0] # np.linspace(-1.0,1.0,5)
@@ -21,30 +20,29 @@ h_log_mean   = J_log_mean - deltas
 num_total = 0
 settings = []
 input_filenames = []
-print("Generating", len(sites) * len(lambdas) * len(J_log_mean) * len(h_log_mean) * len(realizations), "input files")
+print("Generating", len(sites) * len(lambdas) * len(J_log_mean) * len(h_log_mean) , "input files")
 for num_L in sites:
     for num_l in range(len(lambdas)):
         for num_j in range(len(J_log_mean)):
             for num_h in range(len(h_log_mean)):
                 input_filename_dir = location + '/' + 'L_'+ str(num_L)
                 os.makedirs(input_filename_dir, exist_ok=True)
-                for num_r in realizations:
-                    input_filename = input_filename_dir + '/' + basename + str(num_total) + '_l' + str(num_l) + '_J'+ str(num_j) + '_h'+ str(num_h) + '_' + str(num_r) + '.cfg'
-                    settings = {
-                        "model::selfdual_tf_rf_ising::J_log_mean"     : "{:.2f}".format(J_log_mean[num_j]),
-                        "model::selfdual_tf_rf_ising::h_log_mean"     : "{:.2f}".format(h_log_mean[num_h]),
-                        "model::selfdual_tf_rf_ising::lambda"         : "{:.2f}".format(lambdas[num_l]),
-                        "model::selfdual_tf_rf_ising::J_sigma"        : "1.0",
-                        "model::selfdual_tf_rf_ising::h_sigma"        : "1.0",
-                        "model::seed"                                 : str(num_total),
-                        "xdmrg::num_sites"                            : str(num_L),
-			            "xdmrg::chi_max"                              : "64",
-                        "fdmrg::num_sites"                            : str(num_L),
-                        "fdmrg::chi_max"                              : "64",
-                        "hdf5::output_filename"                       : 'output/L_'+ str(num_L) + '/l_'+str(num_l) + '/J_' +str(num_j) + '/h_'+ str(num_h)+ '/' + basename + str(num_r) + '.h5'
-                    }
-                    num_total = num_total + 1
-                    generate_input_file(settings, input_filename, template_filename)
+                input_filename = input_filename_dir + '/' + basename + '_l' + str(num_l) + '_J'+ str(num_j) + '_h'+ str(num_h) + '.cfg'
+                settings = {
+                    "model::selfdual_tf_rf_ising::J_log_mean"     : "{:.2f}".format(J_log_mean[num_j]),
+                    "model::selfdual_tf_rf_ising::h_log_mean"     : "{:.2f}".format(h_log_mean[num_h]),
+                    "model::selfdual_tf_rf_ising::lambda"         : "{:.2f}".format(lambdas[num_l]),
+                    "model::selfdual_tf_rf_ising::J_sigma"        : "1.0",
+                    "model::selfdual_tf_rf_ising::h_sigma"        : "1.0",
+                    "model::seed"                                 : str(num_total),
+                    "xdmrg::num_sites"                            : str(num_L),
+                    "xdmrg::chi_max"                              : "64",
+                    "fdmrg::num_sites"                            : str(num_L),
+                    "fdmrg::chi_max"                              : "64",
+                    "hdf5::output_filename"                       : 'output/L_'+ str(num_L) + '/l_'+str(num_l) + '/J_' +str(num_j) + '/h_'+ str(num_h)+ '/' + basename + '.h5'
+                }
+                num_total = num_total + 1
+                generate_input_file(settings, input_filename, template_filename)
 
 # generate_input_files(settings, input_filenames, template_filename,location)
 
