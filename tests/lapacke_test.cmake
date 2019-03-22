873a575b5ac3907122f003578469d0eb21b73f05
DavidAce <kameboy@gmail.com>
2019-03-22 19:46:39 +0100

    Investigating eigenvalue bug on cluster
    Former-commit-id: 0589b8e2c04fcf51bab2836d9bd524ff27da736d

---
+++ tests/lapacke_test.cmake
@@ -0,0 +1,33 @@
+add_executable(lapacke_test_target tests/lapacke_test.cpp)
+set_target_properties(lapacke_test_target PROPERTIES OUTPUT_NAME  lapacke_test)
+target_include_directories(lapacke_test_target PUBLIC source)
+
+################################################
+###  Force cmake to find .a library suffixes ###
+################################################
+if(STATIC_BUILD)
+    set(CUSTOM_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${CUSTOM_SUFFIX} ${CMAKE_FIND_LIBRARY_SUFFIXES})
+    target_link_libraries  (lapacke_test_target PRIVATE -static)                  ### Static linkage
+else()
+    set(CUSTOM_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ${CUSTOM_SUFFIX} ${CMAKE_FIND_LIBRARY_SUFFIXES})
+endif()
+
+target_link_libraries  (lapacke_test_target PRIVATE -Wl,--no-allow-shlib-undefined )
+target_link_libraries  (lapacke_test_target PRIVATE -Wl,--no-as-needed )
+target_link_libraries  (lapacke_test_target PRIVATE -Wl,--no-undefined )
+
+target_link_libraries(lapacke_test_target PRIVATE -lstdc++fs)
+target_link_libraries(lapacke_test_target PRIVATE -flto)
+target_link_libraries(lapacke_test_target PRIVATE  blas lapack lapacke h5pp::h5pp Eigen3::Eigen spdlog::spdlog)
+
+
+set_target_properties  (lapacke_test_target PROPERTIES CXX_STANDARD_REQUIRED 17)
+target_compile_features(lapacke_test_target PRIVATE cxx_std_17)
+target_compile_options (lapacke_test_target PRIVATE ${COMMON_OPTIONS})                                   ### Common options
+target_compile_options (lapacke_test_target PRIVATE "$<$<CONFIG:DEBUG>:${DEBUG_OPTIONS}>")               ### Debug build options
+target_compile_options (lapacke_test_target PRIVATE "$<$<CONFIG:RELEASE>:${RELEASE_OPTIONS}>")           ### Release build options
+add_test(NAME lapacke_test COMMAND lapacke_test_target)
+
+add_dependencies(lapacke_test_target blas lapack lapacke h5pp::h5pp Eigen3::Eigen spdlog::spdlog)
