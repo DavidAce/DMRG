a53f718bc93b8972f82c57f4f32d40f8f9d9cc3f
DavidAce <kameboy@gmail.com>
2019-03-23 10:08:44 +0100

    Investigating eigenvalue bug on cluster
    Former-commit-id: bfa82ebed6ff51d6f8b03717d7113147899b2e3f

--- tests/lapacke_test.cmake
+++ tests/lapacke_test.cmake
@@ -1,7 +1,9 @@
 add_executable(lapacke_test_target tests/lapacke_test.cpp
-                                   source/general/class_eigsolver.cpp
-#                                   source/general/arpack_extra/arpackpp_solver.cpp
-                                   source/general/class_tic_toc.cpp)
+        source/general/class_eigsolver.h
+        source/general/class_eigsolver.cpp
+        source/general/arpack_extra/arpackpp_solver.cpp
+        source/general/arpack_extra/matrix_product_stl.cpp
+        source/general/class_tic_toc.cpp)
 
 
 set_target_properties(lapacke_test_target PROPERTIES OUTPUT_NAME  lapacke_test)
@@ -25,7 +27,7 @@ target_link_libraries  (lapacke_test_target PRIVATE -Wl,--no-undefined )
 
 target_link_libraries(lapacke_test_target PRIVATE -lstdc++fs)
 target_link_libraries(lapacke_test_target PRIVATE -flto)
-target_link_libraries(lapacke_test_target PRIVATE  blas lapack lapacke  h5pp::h5pp hdf5 Eigen3::Eigen spdlog::spdlog)
+target_link_libraries(lapacke_test_target PRIVATE  arpack++  h5pp::h5pp hdf5 Eigen3::Eigen spdlog::spdlog )
 
 
 set_target_properties  (lapacke_test_target PROPERTIES CXX_STANDARD_REQUIRED 17)
@@ -35,4 +37,4 @@ target_compile_options (lapacke_test_target PRIVATE "$<$<CONFIG:DEBUG>:${DEBUG_O
 target_compile_options (lapacke_test_target PRIVATE "$<$<CONFIG:RELEASE>:${RELEASE_OPTIONS}>")           ### Release build options
 add_test(NAME lapacke_test COMMAND lapacke_test_target)
 
-add_dependencies(lapacke_test_target blas lapack lapacke  h5pp::h5pp hdf5 Eigen3::Eigen spdlog::spdlog)
+add_dependencies(lapacke_test_target arpack++  h5pp::h5pp hdf5 Eigen3::Eigen spdlog::spdlog )
