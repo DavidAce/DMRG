bf59fe78f2747fcd8d998e367600d4cc84f5a040
DavidAce <kameboy@gmail.com>
2019-03-23 08:36:48 +0100

    Investigating eigenvalue bug on cluster
    Former-commit-id: 40843e149dc8c5a9a824a172b1f09f55d48552c0

--- source/general/nmspc_math.h
+++ source/general/nmspc_math.h
@@ -11,8 +11,8 @@
 #include <functional>
 #include <memory>
 #include <utility>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_integration.h>
+//#include <gsl/gsl_errno.h>
+//#include <gsl/gsl_integration.h>
 #include <numeric>
 #include <Eigen/Core>
 
@@ -99,50 +99,50 @@ namespace Math {
 
 
     /*! \brief class for integration */
-    template<typename F>
-    class gsl_quad {
-        F f;
-        int limit;
-        std::unique_ptr<gsl_integration_workspace,
-                std::function<void(gsl_integration_workspace *)>
-        > workspace;
-
-        static double gsl_wrapper(double x, void *p) {
-            gsl_quad *t = reinterpret_cast<gsl_quad *>(p);
-            return t->f(x);
-        }
-
-    public:
-        gsl_quad(F f, int limit)
-                : f(f), limit(limit), workspace(gsl_integration_workspace_alloc(limit), gsl_integration_workspace_free) {}
-
-        double integrate(double min, double max, double epsabs, double epsrel) {
-            gsl_function gsl_f;
-            gsl_f.function = &gsl_wrapper;
-            gsl_f.params = this;
-
-            double result, error;
-            if (!std::isinf(min) && !std::isinf(max)) {
-                gsl_integration_qags(&gsl_f, min, max,
-                                     epsabs, epsrel, limit,
-                                     workspace.get(), &result, &error);
-            } else if (std::isinf(min) && !std::isinf(max)) {
-                gsl_integration_qagil(&gsl_f, max,
-                                      epsabs, epsrel, limit,
-                                      workspace.get(), &result, &error);
-            } else if (!std::isinf(min) && std::isinf(max)) {
-                gsl_integration_qagiu(&gsl_f, min,
-                                      epsabs, epsrel, limit,
-                                      workspace.get(), &result, &error);
-            } else {
-                gsl_integration_qagi(&gsl_f,
-                                     epsabs, epsrel, limit,
-                                     workspace.get(), &result, &error);
-            }
-
-            return result;
-        }
-    };
+//    template<typename F>
+//    class gsl_quad {
+//        F f;
+//        int limit;
+//        std::unique_ptr<gsl_integration_workspace,
+//                std::function<void(gsl_integration_workspace *)>
+//        > workspace;
+//
+//        static double gsl_wrapper(double x, void *p) {
+//            gsl_quad *t = reinterpret_cast<gsl_quad *>(p);
+//            return t->f(x);
+//        }
+//
+//    public:
+//        gsl_quad(F f, int limit)
+//                : f(f), limit(limit), workspace(gsl_integration_workspace_alloc(limit), gsl_integration_workspace_free) {}
+//
+//        double integrate(double min, double max, double epsabs, double epsrel) {
+//            gsl_function gsl_f;
+//            gsl_f.function = &gsl_wrapper;
+//            gsl_f.params = this;
+//
+//            double result, error;
+//            if (!std::isinf(min) && !std::isinf(max)) {
+//                gsl_integration_qags(&gsl_f, min, max,
+//                                     epsabs, epsrel, limit,
+//                                     workspace.get(), &result, &error);
+//            } else if (std::isinf(min) && !std::isinf(max)) {
+//                gsl_integration_qagil(&gsl_f, max,
+//                                      epsabs, epsrel, limit,
+//                                      workspace.get(), &result, &error);
+//            } else if (!std::isinf(min) && std::isinf(max)) {
+//                gsl_integration_qagiu(&gsl_f, min,
+//                                      epsabs, epsrel, limit,
+//                                      workspace.get(), &result, &error);
+//            } else {
+//                gsl_integration_qagi(&gsl_f,
+//                                     epsabs, epsrel, limit,
+//                                     workspace.get(), &result, &error);
+//            }
+//
+//            return result;
+//        }
+//    };
 
     /*! \fn compute_integral(F func,
      *                    std::pair<double, double> const &range,
@@ -168,15 +168,15 @@ namespace Math {
      * ```
      *
      */
-    template<typename F>
-    double compute_integral(F func,
-                            std::pair<double, double> const &range,
-                            double epsabs = 1.49e-8, double epsrel = 1.49e-8,
-                            int limit = 50) {
-        return gsl_quad<F>(func, limit).integrate(range.first, range.second, epsabs, epsrel);
-
-
-    }
+//    template<typename F>
+//    double compute_integral(F func,
+//                            std::pair<double, double> const &range,
+//                            double epsabs = 1.49e-8, double epsrel = 1.49e-8,
+//                            int limit = 50) {
+//        return gsl_quad<F>(func, limit).integrate(range.first, range.second, epsabs, epsrel);
+//
+//
+//    }
 
 
 
