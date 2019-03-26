6dc1b683b3e818da3f573596cc38460fd30177f7
DavidAce <kameboy@gmail.com>
2019-03-26 02:03:54 +0100

    Investigating bug on cluster

--- cmake-modules/Fetch_OpenBLAS.cmake
+++ cmake-modules/Fetch_OpenBLAS.cmake
@@ -147,8 +147,8 @@ else()
 #            export LDFLAGS="-L${GFORTRAN_PATH} $LDFLAGS" &&
             BUILD_IN_SOURCE 1
             BUILD_COMMAND
-                        export LD_LIBRARY_PATH=${GFORTRAN_PATH}:$LD_LIBRARY_PATH &&
-                        export LDFLAGS="-l${GFORTRAN_LIB} $LDFLAGS" &&
+                        export LD_LIBRARY_PATH=${GFORTRAN_PATH} &&
+                        export LDFLAGS=-l${GFORTRAN_LIB} &&
                         $(MAKE) TARGET=SANDYBRIDGE
                         DYNAMIC_ARCH=1
                         USE_THREAD=${OpenBLAS_MULTITHREADED}
@@ -161,7 +161,7 @@ else()
                         FFLAGS=-frecursive
                         BINARY64=64
                         GEMM_MULTITHREAD_THRESHOLD=16
-                        LDFLAGS=-l${GFORTRAN_LIB} -l${QUADMATH_LIB}
+                        LDFLAGS="-l${GFORTRAN_LIB} -l${QUADMATH_LIB}"
                         FFLAGS=-O3 -Wno-maybe-uninitialized -Wno-conversion -Wno-unused-but-set-variable -Wno-unused-variable
             INSTALL_COMMAND $(MAKE) PREFIX=<INSTALL_DIR> install
             DEPENDS gfortran
