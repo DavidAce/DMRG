dc1cbd51a4d48b9c497d88a18ccc1a4f14704b33
DavidAce <kameboy@gmail.com>
2019-07-22 23:23:01 +0200

    Adding gflags for glog and ceres
    Former-commit-id: 45ea9058aadfe9a740b0470d64abd30e38500a74

--- cmake-modules/Fetch_Glog.cmake
+++ cmake-modules/Fetch_Glog.cmake
@@ -1,29 +1,35 @@
 
+#find_package(glog 0.4 PATHS ${INSTALL_DIRECTORY}/glog $ENV{EBROOTGLOG} $ENV{GLOG_DIR} $ENV{glog_DIR} NO_DEFAULT_PATH)
+message(STATUS "Searching for glog")
+find_library(
+        GLOG_LIBRARIES
+        NAMES libglog${CUSTOM_SUFFIX}
+        PATH_SUFFIXES lib lib64
+        PATHS  ${INSTALL_DIRECTORY}/glog $ENV{EBROOTGLOG} $ENV{GLOG_DIR} $ENV{glog_DIR}
+        NO_DEFAULT_PATH)
+
+find_path(
+        GLOG_INCLUDE_DIR
+        NAMES glog/logging.h
+        PATH_SUFFIXES include glog/include
+        PATHS  ${INSTALL_DIRECTORY}/glog $ENV{EBROOTGLOG} $ENV{GLOG_DIR} $ENV{glog_DIR}
+        NO_DEFAULT_PATH)
 
 
-message(STATUS "Searching for glog 0.4.x")
-find_package(glog 0.4 PATHS ${INSTALL_DIRECTORY}/glog $ENV{EBROOTGLOG} $ENV{GLOG_DIR} $ENV{glog_DIR} NO_DEFAULT_PATH)
-
-if(TARGET glog::glog)
+if(GLOG_LIBRARIES AND GLOG_INCLUDE_DIR)
     # For some reason glog imports libunwind.so even though we asked for static libraries.
     # In addition, libglog.a hides in the property "LOCATION" instead of its rightful
-    # place "INTERFACE_LINK_LIBRARIES".
-
-    get_target_property(GLOG_INCLUDE_DIR glog::glog INTERFACE_INCLUDE_DIRECTORIES)
-    get_target_property(GLOG_LIBRARIES   glog::glog LOCATION)
-    # In addition, the library may be .so
-    get_filename_component(glog_dir ${GLOG_LIBRARIES} DIRECTORY)
-    get_filename_component(glog_we  ${GLOG_LIBRARIES} NAME_WE)
-    set(GLOG_LIBRARIES "${glog_dir}/${glog_we}${CUSTOM_SUFFIX}")
-
-    set_target_properties(glog::glog PROPERTIES INTERFACE_LINK_LIBRARIES "${GLOG_LIBRARIES}")
-    set_target_properties(glog::glog PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${GLOG_INCLUDE_DIR}")
-    message(STATUS "Searching for glog 0.4.x - Success: LIB: ${GLOG_LIBRARIES}")
-    message(STATUS "Searching for glog 0.4.x - Success: INC: ${GLOG_INCLUDE_DIR}")
+    # place "INTERFACE_LINK_LIBRARIES"... so we do this manually
+    add_library(glog INTERFACE)
+    add_dependencies(glog gflags)
+    set_target_properties(glog PROPERTIES INTERFACE_LINK_LIBRARIES ${GLOG_LIBRARIES})
+    set_target_properties(glog PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${GLOG_INCLUDE_DIR})
+    message(STATUS "Searching for glog - Success: LIB: ${GLOG_LIBRARIES}")
+    message(STATUS "Searching for glog - Success: INC: ${GLOG_INCLUDE_DIR}")
     include(cmake-modules/PrintTargetProperties.cmake)
-    print_target_properties(glog::glog)
+    print_target_properties(glog)
 else()
-    message(STATUS "Searching for glog 0.4.x - failed")
+    message(STATUS "Searching for glog - failed")
     message(STATUS "glog will be installed into ${INSTALL_DIRECTORY}/glog on first build.")
 
     include(ExternalProject)
@@ -38,15 +44,16 @@ else()
             -DCMAKE_BUILD_TYPE=Release
             -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
             -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-            DEPENDS gflags::gflags
+            DEPENDS gflags
             )
 
     ExternalProject_Get_Property(external_GLOG INSTALL_DIR)
     add_library(glog INTERFACE)
-    add_library(glog::glog ALIAS glog)
     set(GLOG_INCLUDE_DIR ${INSTALL_DIR}/include)
     set(glog_DIR ${INSTALL_DIR}/lib/cmake/glog)
-    add_dependencies(glog external_GLOG gflags::gflags)
-    target_link_libraries(glog INTERFACE ${INSTALL_DIR}/lib/libglog${CUSTOM_SUFFIX} gflags::gflags)
+    add_dependencies(glog external_GLOG gflags)
+    target_link_libraries(glog INTERFACE ${INSTALL_DIR}/lib/libglog${CUSTOM_SUFFIX} gflags)
     target_include_directories(glog INTERFACE ${GLOG_INCLUDE_DIR})
 endif()
+
+
