6bf48edb7163f99c38a150ce770bd7963cde5f55
DavidAce <kameboy@gmail.com>
2019-07-22 10:01:29 +0200

    Adding support for easybuild modules

--- cmake-modules/Fetch_Arpackpp.cmake
+++ cmake-modules/Fetch_Arpackpp.cmake
@@ -1,36 +1,49 @@
 
 
 
-
-message(STATUS "SEARCHING FOR ARPACK++ IN SYSTEM...")
-find_library(ARPACKPP_LIBRARIES
+if(NOT ARPACKPP_LIBRARIES)
+message(STATUS "Searching for Arpack++ in system")
+    find_library(ARPACKPP_LIBRARIES
         NAMES arpackpp arpack++ libarpack2++ libarpack++ libarpackpp
         PATH_SUFFIXES lib lib32 lib64
         PATHS /usr/include/arpack++ /usr/include /usr/local/
         )
-find_path(ARPACKPP_INCLUDE_DIR
+    find_path(ARPACKPP_INCLUDE_DIR
         NAMES ardsnsym.h
         PATHS /usr/include/arpack++ /usr/include /usr/local/
         )
+    if(NOT ARPACKPP_LIBRARIES OR NOT ARPACKPP_INCLUDE_DIR)
+        message(STATUS "Searching for Arpack++ in system -- failed")
+    else()
+        message(STATUS "Searching for Arpack++ in system -- success: ${ARPACKPP_LIBRARIES}")
+        message(STATUS "Note that old versions of Arpack++ (e.g. the default in Ubuntu Trusty 14.04 LTS) may fail to compile, they require '-fpermissive'.")
+    endif()
+endif()
+
 
 if (NOT ARPACKPP_LIBRARIES OR NOT ARPACKPP_INCLUDE_DIR)
     # Try finding arpack as module library
     set(ARPACKPP_LIBRARIES "")
-    message(STATUS "SEARCHING FOR ARPACK IN LOADED MODULES")
+    message(STATUS "Searching for Arpack++ in module paths")
     find_path(ARPACKPP_INCLUDE_DIR
             NAMES ardsnsym.h arpack++
+            PATH_SUFFIXES include
             PATHS
-            $ENV{EBROOTARPACKPLUSPLUS}/include
-            $ENV{ARPACKPP_DIR}/include
+            $ENV{EBROOTARPACKPLUSPLUS}
+            $ENV{ARPACKPP_DIR}
             NO_DEFAULT_PATH
             )
+    if(NOT ARPACKPP_INCLUDE_DIR)
+        message(STATUS "Searching for Arpack++ in module paths -- failed")
+    else()
+        message(STATUS "Searching for Arpack++ in module paths -- success: ${ARPACKPP_LIBRARIES}")
+    endif()
 endif()
 
 
-message(STATUS "Note that old versions of Arpack++ (e.g. the default in Ubuntu Trusty 14.04 LTS) may fail to compile, requiring '-fpermissive'.")
-if (ARPACKPP_LIBRARIES OR ARPACKPP_INCLUDE_DIR AND NOT "${OS_PROPERTIES}" MATCHES "trusty" )
-    message(STATUS "Arpack++ library found in system: ${ARPACKPP_LIBRARIES}")
-    message(STATUS "Arpack++ include found in system: ${ARPACKPP_INCLUDE_DIR}")
+if (ARPACKPP_LIBRARIES OR ARPACKPP_INCLUDE_DIR)
+    message(STATUS "Arpack++ library found: ${ARPACKPP_LIBRARIES}")
+    message(STATUS "Arpack++ include found: ${ARPACKPP_INCLUDE_DIR}")
     add_library(arpack++ INTERFACE)
     target_link_libraries(arpack++ INTERFACE ${ARPACKPP_LIBRARIES} arpack)
     target_include_directories(arpack++ INTERFACE ${ARPACKPP_INCLUDE_DIR})
