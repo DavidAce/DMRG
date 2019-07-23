995ccc3049825f5c0a7b7e006cba48ce16df891d
DavidAce <kameboy@gmail.com>
2019-07-23 10:08:40 +0200

    Adding support for detecting parallel hdf5 build (and discard if necessary)

--- cmake-modules/Fetch_Eigen3.cmake
+++ cmake-modules/Fetch_Eigen3.cmake
@@ -33,8 +33,8 @@ if(TARGET blas)
 endif()
 
 if(EIGEN3_FOUND AND TARGET Eigen3::Eigen)
-    get_target_property(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
     target_compile_options(Eigen3::Eigen INTERFACE ${EIGEN3_COMPILER_FLAGS})
+    target_include_directories(Eigen3::Eigen INTERFACE ${EIGEN3_INCLUDE_DIR})
     add_library(Eigen3 INTERFACE)
     target_link_libraries(Eigen3 INTERFACE Eigen3::Eigen)
 
