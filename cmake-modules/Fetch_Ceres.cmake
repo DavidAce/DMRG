6407f4fef4782c70a30fffceb29a3a25c076f6c5
DavidAce <kameboy@gmail.com>
2019-07-29 07:49:24 +0200

    Fixing gflags static link
    Former-commit-id: f3b9473910c0847eaa8c8bf3ab307edf3402822f

--- cmake-modules/Fetch_Ceres.cmake
+++ cmake-modules/Fetch_Ceres.cmake
@@ -35,7 +35,7 @@ else()
     if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" )
         set(FLAGS "${FLAGS} -Wno-implicit-fallthrough -Wno-deprecated-declarations -Wno-ignored-attributes -Wno-int-in-bool-context -Wno-maybe-uninitialized -Wno-enum-compare -Wno-unused-local-typedefs")
     elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-        set(FLAGS "${FLAGS} --gcc-toolchain=${GCC_TOOLCHAIN}")
+        set(FLAGS "${FLAGS} -v --gcc-toolchain=${GCC_TOOLCHAIN}")
         set(FLAGS "${FLAGS} -stdlib=libstdc++ -Wno-deprecated-declarations -Wno-ignored-attributes -Wno-invalid-partial-specialization -Wno-missing-braces -Wno-overloaded-virtual -Wno-uninitialized")
     endif()
 
@@ -63,13 +63,12 @@ else()
             -DCUSTOM_BLAS:BOOL=OFF
             -DEIGEN_INCLUDE_DIR:PATH=${EIGEN3_INCLUDE_DIR}
             -DEIGEN_INCLUDE_DIR_HINTS:PATH=${EIGEN3_INCLUDE_DIR}
-            -DEIGEN_PREFER_EXPORTED_EIGEN_CMAKE_CONFIGURATION:BOOL=OFF
+            -DEIGEN_PREFER_EXPORTED_EIGEN_CMAKE_CONFIGURATION:BOOL=FALSE
+            -DGFLAGS_PREFER_EXPORTED_GFLAGS_CMAKE_CONFIGURATION:BOOL=FALSE
             -DGFLAGS_INCLUDE_DIR:PATH=${GFLAGS_INCLUDE_DIR}
             -DGFLAGS_LIBRARY:PATH=${GFLAGS_LIBRARIES}
             -DGLOG_INCLUDE_DIR:PATH=${GLOG_INCLUDE_DIR}
             -DGLOG_LIBRARY:PATH=${GLOG_LIBRARIES}
-#            -DGLOG_LIBRARY_DIR_HINTS:PATH=${GLOG_LIBRARIES}
-#            -DGLOG_INCLUDE_DIR_HINTS:PATH=${GLOG_INCLUDE_DIR}
             -DGLOG_PREFER_EXPORTED_GLOG_CMAKE_CONFIGURATION:BOOL=FALSE
             -DLAPACK:BOOL=OFF
             -DEIGENSPARSE:BOOL=ON
