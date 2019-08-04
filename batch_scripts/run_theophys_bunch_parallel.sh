d6c50790aa3f6631f64311ba90b9897edc46d086
DavidAce <kameboy@gmail.com>
2019-08-04 13:36:12 +0200

    Adding job array support for batch scripts

---
+++ batch_scripts/run_theophys_bunch_parallel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=DMRG
+#SBATCH --time=0-4:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --kill-on-invalid-dep=yes
+#SBATCH --output=logs/DMRG-%A_%a.out
+#SBATCH --error=logs/DMRG-%A_%a.err
+#SBATCH --requeue
+
+exec=${1}
+inputfile=${2}
+mkdir -p logs
+ulimit -c unlimited
+echo "Running job $SLURM_ARRAY_JOB_ID, iter $SLURM_ARRAY_TASK_ID at $(hostname) with inputfile $inputfile"
+$exec $inputfile $SLURM_ARRAY_TASK_ID
+
+
---
+++ batch_scripts/submit_theophys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+
+exec=../build/Release/DMRG++
+inputfiles=$(find -L input -type f -name '*.cfg')
+nsims=100
+count=0
+nmax=$((nmin+nsims))
+for inputfile in $inputfiles; do
+    [ -e "$inputfile" ] || continue
+    nmin=$((count*nsims))
+    nmax=$((count*nsims+nsims-1))
+    echo "Submitting array=[$nmin - $nmax]"
+    sbatch  --array=$nmin-$nmax run_theophys_bunch_parallel.sh $exec $inputfile
+    count=$((count+1))
+done
