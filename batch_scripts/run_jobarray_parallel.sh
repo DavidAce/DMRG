fd2cebd6943ecc93174105cb8adc4be5fa61e489
DavidAce <kameboy@gmail.com>
2019-08-08 16:24:10 +0200

    Improved options for submit script with GNU Parallel

--- batch_scripts/run_jobarray_parallel.sh
+++ batch_scripts/run_jobarray_parallel.sh
@@ -17,11 +17,18 @@ outdir=logs/jobarray_$SLURM_ARRAY_JOB_ID
 mkdir -p $outdir
 
 echo "Running job $SLURM_ARRAY_JOB_ID, id [$nmin - $nmax] at $HOSTNAME with inputfile $inputfile"
-echo "CPUS ON  NODE: $SLURM_CPUS_ON_NODE"
-echo "CPUS PER NODE: $SLURM_JOB_CPUS_PER_NODE"
-echo "CPUS PER TASK: $SLURM_CPUS_PER_TASK"
-echo "MEM PER CPU  : $SLURM_MEM_PER_CPU"
-echo "MEM PER NODE : $SLURM_MEM_PER_NODE"
+echo "ARRAY JOB ID     : $SLURM_ARRAY_JOB_ID"
+echo "ARRAY TASK COUNT : $SLURM_ARRAY_TASK_COUNT"
+echo "ARRAY TASK ID    : $SLURM_ARRAY_TASK_ID"
+echo "ARRAY TASK MAX   : $SLURM_ARRAY_TASK_MAX"
+echo "ARRAY TASK MIN   : $SLURM_ARRAY_TASK_MIN"
+echo "ARRAY TASK STEP  : $SLURM_ARRAY_TASK_STEP"
+
+echo "CPUS ON  NODE  : $SLURM_CPUS_ON_NODE"
+echo "CPUS PER NODE  : $SLURM_JOB_CPUS_PER_NODE"
+echo "CPUS PER TASK  : $SLURM_CPUS_PER_TASK"
+echo "MEM PER CPU    : $SLURM_MEM_PER_CPU"
+echo "MEM PER NODE   : $SLURM_MEM_PER_NODE"
 
 
 parallel --memfree $SLURM_MEM_PER_CPU --joblog $outdir/$inputbase.log --results $outdir $exec $inputfile {} ::: $(seq $nmin $nmax)
