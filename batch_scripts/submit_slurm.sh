c4228c7ce0254a4d0c5201855f7b604498409ef9
DavidAce <kameboy@gmail.com>
2019-08-08 13:50:20 +0200

    Improved options for build script
    Former-commit-id: 3c5adb42f23699d7f6926435768de2043b76b997

--- batch_scripts/submit_slurm.sh
+++ batch_scripts/submit_slurm.sh
@@ -6,40 +6,45 @@ PROGNAME=$0
 usage() {
   cat << EOF >&2
 
-Usage                : $PROGNAME [-options] with the following options:
--h                   : Help. Shows this text.
--b <build type>      : Release | RelWithDebInfo | Debug | Profile |  (default = Release)
--e                   : Enable --exclusive mode. (default off)
--j <job name>        : Job name. (default=DMRG)
--m <memory (MB)>     : Reserved amount of ram for each task in MB. (default = 4000)
--n <num sims>        : Number of simulations (default 10)
--o <other>           : Other options passed to sbatch
--p <partition>       : Partition name (default = all)
--r <requeue>         : Enable --requeue, for requeuing in case of failure (default OFF)
--s <num simult.>     : Max number of simultaneous tasks to run for each job array (default = 32)
--t <time>            : Time for each run (default = 1:00:00, i.e. 1 hour)
+Usage                               : $PROGNAME [-options] with the following options:
+-h                                  : Help. Shows this text.
+-b <build type>                     : Release | RelWithDebInfo | Debug | Profile |  (default = Release)
+-e                                  : Enable --exclusive mode. (default off)
+-g                                  : Enable GNU Parallel. (default off)
+-j <job name>                       : Job name. (default=DMRG)
+-k <step size>                      : Step size of job arrays,for use with GNU Parallel (default = 32)
+-m <memory (MB)>                    : Reserved amount of ram for each task in MB. (default = 4000)
+-n <num sims/arrays>                : Number of simulations (default 10). If running with GNU Parallel, this is the number of arrays, and simulations = num arrays * bunch size.
+-o <other>                          : Other options passed to sbatch
+-p <partition>                      : Partition name (default = all)
+-r <requeue>                        : Enable --requeue, for requeuing in case of failure (default OFF)
+-s <max simultaneous tasks>         : Simultaneous tasks for each job array (default = 32)
+-t <time>                           : Time for each run (default = 1:00:00, i.e. 1 hour)
 EOF
   exit 1
 }
 
 build=Release
 nsims=10
-nsimu=%32
+maxtasks=%32
 jobname=DMRG
+stepsize=:32
 mem=4000
 time=--time=0-1:00:00
-while getopts hb:ej:m:n:o:p:rs:t: o; do
+while getopts hb:ej:k:m:n:o:p:rs:t: o; do
     case $o in
         (h) usage ;;
         (b) build=$OPTARG;;
         (e) exclusive=--exclusive;;
+        (g) gnuparallel=true;;
         (j) jobname=$OPTARG;;
+        (k) stepsize=:$OPTARG;;
         (m) mem=$OPTARG;;
         (n) nsims=$OPTARG;;
         (o) other=$OPTARG;;
         (p) partition=--partition=$OPTARG;;
         (r) requeue=--requeue;;
-        (s) nsimu=%$OPTARG;;
+        (s) maxtasks=%$OPTARG;;
         (t) time=--time=0-$OPTARG;;
         (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
         (*) usage ;;
@@ -67,9 +72,19 @@ for inputfile in $inputfiles; do
     nmin=$((count*nsims))
     nmax=$((count*nsims+nsims-1))
     #echo "Submitting array=[$nmin - $nmax]"
-    sbatch $partition $requeue $exclusive $time $other \
+
+    if [ "$gnuparallel" = true ]; then
+        sbatch $partition $requeue $exclusive $time $other \
             --mem-per-cpu=$mem \
-            --array=$nmin-$nmax$nsimu --job-name=$jobname \
+            --array=$nmin-$nmax$stepsize --job-name=$jobname \
+            run_jobarray_parallel.sh $exec $inputfile
+    else
+        sbatch $partition $requeue $exclusive $time $other \
+            --mem-per-cpu=$mem \
+            --array=$nmin-$nmax$maxtasks --job-name=$jobname \
             run_jobarray.sh $exec $inputfile
+    fi
+
+
     count=$((count+1))
 done
