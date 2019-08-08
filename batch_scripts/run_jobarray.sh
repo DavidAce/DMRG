5a874083628ecfe8a283fc0d911dc16a0b3e9c3f
DavidAce <kameboy@gmail.com>
2019-08-08 10:41:32 +0200

    Improved options for batch submission

--- batch_scripts/run_jobarray.sh
+++ batch_scripts/run_jobarray.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=DMRG
-#SBATCH --mem-per-cpu=4000
 #SBATCH --kill-on-invalid-dep=yes
 #SBATCH --output=logs/DMRG-%A_%a.out
 #SBATCH --error=logs/DMRG-%A_%a.err
-#SBATCH --requeue
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --oversubscribe
@@ -14,5 +11,8 @@ inputfile=${2}
 mkdir -p logs
 ulimit -c unlimited
 echo "Running job $SLURM_ARRAY_JOB_ID, iter $SLURM_ARRAY_TASK_ID at $HOSTNAME with inputfile $inputfile"
+echo "CPUS ON  NODE: $SLURM_CPUS_ON_NODE"
+echo "CPUS PER NODE: $SLURM_JOB_CPUS_PER_NODE"
+echo "CPUS PER TASK: $SLURM_CPUS_PER_TASK"
 $exec $inputfile $SLURM_ARRAY_TASK_ID
 
--- batch_scripts/submit_slurm.sh
+++ batch_scripts/submit_slurm.sh
@@ -6,34 +6,41 @@ PROGNAME=$0
 usage() {
   cat << EOF >&2
 
-Usage            : $PROGNAME  [-h ] [-m <mode>] [-n <num sims>] [-p <partition>] [-t <time>] [-o <other>]
--h               : Help. Shows this text.
--b <build type>  : Release | RelWithDebInfo | Debug | Profile |  (default = Release)
--e               : Enable --exclusive mode. (default off)
--m <memory (MB)> : Reserved amount of ram for each task in MB. (default = 4000)
--n <num sims>    : Number of simulations (default 10)
--p <partition>   : Partition name (default = )
--t <time>        : Time for each run (default = 4:00:00)
--o <other>       : Other options passed to sbatch
+Usage                : $PROGNAME [-options] with the following options:
+-h                   : Help. Shows this text.
+-b <build type>      : Release | RelWithDebInfo | Debug | Profile |  (default = Release)
+-e                   : Enable --exclusive mode. (default off)
+-j <job name>        : Job name. (default=DMRG)
+-m <memory (MB)>     : Reserved amount of ram for each task in MB. (default = 4000)
+-n <num sims>        : Number of simulations (default 10)
+-o <other>           : Other options passed to sbatch
+-p <partition>       : Partition name (default = all)
+-r <requeue>         : Enable --requeue, for requeuing in case of failure (default OFF)
+-s <num simult.>     : Max number of simultaneous tasks to run for each job array (default = 32)
+-t <time>            : Time for each run (default = 1:00:00, i.e. 1 hour)
 EOF
   exit 1
 }
 
 build=Release
 nsims=10
-partition=all
-time=0-4:00:00
-exclusive=""
+nsimu=%32
+jobname=DMRG
 mem=4000
-while getopts hb:em:n:p:t: o; do
+time=--time=0-1:00:00
+while getopts hb:ej:m:n:o:p:rt: o; do
     case $o in
         (h) usage ;;
-        (b) mode=$OPTARG;;
+        (b) build=$OPTARG;;
         (e) exclusive=--exclusive;;
+        (j) jobname=$OPTARG;;
         (m) mem=$OPTARG;;
         (n) nsims=$OPTARG;;
-        (p) partition=$OPTARG;;
-        (t) time=0-$OPTARG;;
+        (o) other=$OPTARG;;
+        (p) partition=--partition=$OPTARG;;
+        (r) requeue=--requeue;;
+        (s) nsimu=%$OPTARG;;
+        (t) time=--time=0-$OPTARG;;
         (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
         (*) usage ;;
   esac
@@ -45,7 +52,7 @@ echo "Running $nsims realizations for each inputfile"
 export OMP_NUM_THREADS=1
 
 
-exec=../build/$mode/DMRG++
+exec=../build/$build/DMRG++
 if test -f "$exec"; then
     echo "Found exec: $exec"
 else
@@ -55,12 +62,14 @@ fi
 
 inputfiles=$(find -L input -type f -name '*.cfg')
 count=0
-nmax=$((nmin+nsims))
 for inputfile in $inputfiles; do
     [ -e "$inputfile" ] || continue
     nmin=$((count*nsims))
     nmax=$((count*nsims+nsims-1))
-    echo "Submitting array=[$nmin - $nmax]"
-    sbatch --partition=$partition --time=$time --array=$nmin-$nmax $exclusive run_jobarray.sh $exec $inputfile
+    #echo "Submitting array=[$nmin - $nmax]"
+    sbatch $partition $requeue $exclusive $time $other \
+            --hint=compute_bound \
+            --array=$nmin-$nmax$nsimu --job-name=$jobname \
+            run_jobarray.sh $exec $inputfile
     count=$((count+1))
 done
