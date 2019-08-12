4ed7d048bd559bd96eb1bbf01b8e08edd2b647ed
DavidAce <kameboy@gmail.com>
2019-08-12 08:38:22 +0200

    preparing new e distribution run

--- batch_scripts/run_parallel.sh
+++ batch_scripts/run_parallel.sh
@@ -24,7 +24,7 @@ echo "MEM PER CPU    : $SLURM_MEM_PER_CPU"
 echo "MEM PER NODE   : $SLURM_MEM_PER_NODE"
 
 
-parallel --memfree $SLURM_MEM_PER_CPU --joblog $outdir/$inputbase.log "$exec -i $inputfile -r $realization -s {} &> $outdir/$inputbase_$realization{}.out" ::: $(seq $nmin $nmax)
+parallel --memfree $SLURM_MEM_PER_CPU --joblog $outdir/$inputbase.log "$exec -i $inputfile -r $realization -s {} &> $outdir/${inputbase}_${realization}_{}.out" ::: $(seq $nmin $nmax)
 #parallel --memfree $SLURM_MEM_PER_CPU --joblog $outdir/$inputbase.log $exec $inputfile {} ">" $outdir/$inputbase_{}.out ::: $(seq $nmin $nmax)
 
 
