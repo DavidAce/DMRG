761785a724acc32fcf28de33e98b1a6d8f8f0fb0
DavidAce <aceituno@kth.se>
2019-09-26 17:32:39 +0200

    Added quick overview script to see some results at a glance
    Former-commit-id: a1281a50deec8d53f9a8e4011458431e1003a465

--- batch_scripts/quick_overview.py
+++ batch_scripts/quick_overview.py
@@ -6,10 +6,19 @@ import numpy as np
 h5directory = 'output'
 regex = re.compile(r'\d+')
 
-print("{:15} {:8} {:>8} {:>8} {:>10} {:>10} {:>10}".format("Realization", "Variance","Time", "Resets", "Converged", "Saturated", "Succeeded"))
+
 for dirName, subdirList, fileList in os.walk(h5directory):
     subdirList.sort()
     fileList.sort()
+    realization_num  = []
+    variance         = []
+    walltime         = []
+    resets           = []
+    converged        = []
+    saturated        = []
+    succeeded        = []
+
+    print("{:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Realization", "Variance","Time", "Resets", "Converged", "Saturated", "Succeeded"))
     for h5path in fileList:
         # print("Filepath: ", h5path)
         try:
@@ -22,23 +31,31 @@ for dirName, subdirList, fileList in os.walk(h5directory):
 
         try:
             realization_name = h5path.replace('.h5', '')
-            realization_num = [int(x) for x in regex.findall(realization_name)][-1]
-            variance = h5file['xDMRG/measurements/energy_variance_per_site'][-1]
-            walltime = h5file['xDMRG/sim_status/wall_time'][-1]
-            resets   = h5file['xDMRG/sim_status/num_resets'][-1]
-            converged  = h5file['xDMRG/sim_status/simulation_has_converged'][-1]
-            saturated  = h5file['xDMRG/sim_status/simulation_has_saturated'][-1]
-            succeeded  = h5file['xDMRG/sim_status/simulation_has_succeeded'][-1]
-            print("{:<15} {:<8.4f} {:>8.4f} {:>8} {:>10} {:>10} {:>10}".format(
-                realization_num,
-                np.log10(variance),
-                walltime/60,
-                resets,
-                converged,
-                saturated,
-                succeeded))
+            realization_num .append([int(x) for x in regex.findall(realization_name)][-1])
+            variance .append(h5file['xDMRG/measurements/energy_variance_per_site'][-1])
+            walltime .append(h5file['xDMRG/sim_status/wall_time'][-1])
+            resets   .append(h5file['xDMRG/sim_status/num_resets'][-1])
+            converged.append(h5file['xDMRG/sim_status/simulation_has_converged'][-1])
+            saturated.append(h5file['xDMRG/sim_status/simulation_has_saturated'][-1])
+            succeeded.append(h5file['xDMRG/sim_status/simulation_has_succeeded'][-1])
+            print("{:<15} {:<12.4f} {:>12.4f} {:>12} {:>12} {:>12} {:>12}".format(
+                realization_num[-1],
+                np.log10(variance[-1]),
+                walltime[-1]/60,
+                resets[-1],
+                converged[-1],
+                saturated[-1],
+                succeeded[-1]))
 
         except Exception as er:
             print("Could not read dataset. Reason: ", er)
             continue
-
+    print("{:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Total sims", "Avg Var","Avg Time", "Avg Resets", "Sum Con", "Sum Sat", "Sum Suc"))
+    print("{:<15} {:<12.4f} {:>12.4f} {:>12.3f} {:>12} {:>12} {:>12}".format(
+        len(realization_num),
+        np.log10(np.nanmean(variance)),
+        np.mean(walltime) / 60,
+        np.mean(resets),
+        np.sum(converged),
+        np.sum(saturated),
+        np.sum(succeeded)))
