2438fa15562eef05562cbbdee0855eb23d3c2070
DavidAce <aceituno@kth.se>
2019-09-26 18:42:08 +0200

    Added quick overview script to see some results at a glance
    Former-commit-id: c4020bc99ec103c0e2c14e0c6032ae7762a4dd91

--- batch_scripts/quick_overview.py
+++ batch_scripts/quick_overview.py
@@ -7,11 +7,11 @@ from datetime import datetime
 import argparse
 
 parser = argparse.ArgumentParser(description='Quick overview of batch simulation')
-parser.add_argument('-S','--summary', action='store_true', help='Summary only')
-parser.add_argument('-s','--save', action='store_true', help='Save to file')
-parser.add_argument('-f','--filename', type=str, help='Save to file with filename',default='overview')
-parser.add_argument('-t','--timestamp', action='store_true', help='Add timestamp to filename')
-parser.add_argument('-d','--directory', type=str, help='Search for hdf5 files in directory', default='output')
+parser.add_argument('-S', '--summary', action='store_true', help='Summary only')
+parser.add_argument('-s', '--save', action='store_true', help='Save to file')
+parser.add_argument('-f', '--filename', type=str, help='Save to file with filename', default='overview')
+parser.add_argument('-t', '--timestamp', action='store_true', help='Add timestamp to filename')
+parser.add_argument('-d', '--directory', type=str, help='Search for hdf5 files in directory', default='output')
 args = parser.parse_args()
 
 if args.timestamp:
@@ -30,49 +30,54 @@ for dirName, subdirList, fileList in os.walk(args.directory):
         continue
     subdirList.sort()
     fileList.sort()
-    realization_num  = []
-    variance         = []
-    walltime         = []
-    resets           = []
-    converged        = []
-    saturated        = []
-    succeeded        = []
-    finished         = []
+    chainlen  = []
+    realization_num = []
+    variance  = []
+    walltime  = []
+    resets    = []
+    converged = []
+    saturated = []
+    succeeded = []
+    finished  = []
 
     if not args.summary:
-        header = "{:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Realization", "Variance","Time", "Resets", "Converged", "Saturated", "Succeeded", "Finished")
+        header = "{:8} {:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Length", "Realization", "Variance", "Time",
+                                                                                     "Resets", "Converged", "Saturated",
+                                                                                     "Succeeded", "Finished")
         print(header)
         if args.save:
             file.write(header)
     for h5path in fileList:
         # print("Filepath: ", h5path)
         try:
-            h5file = h5py.File(dirName + '/' + h5path, 'r',swmr=True)
+            h5file = h5py.File(dirName + '/' + h5path, 'r', swmr=True)
             if not isinstance(h5file, h5py.File):
                 raise IOError("File not readable")
         except Exception as er:
-            print("Could not open file [", h5path,"] Reason: ",er)
+            print("Could not open file [", h5path, "] Reason: ", er)
             continue
 
         try:
             realization_name = h5path.replace('.h5', '')
-            realization_num .append([int(x) for x in regex.findall(realization_name)][-1])
-            variance .append(h5file['xDMRG/measurements/energy_variance_per_site'][-1])
-            walltime .append(h5file['xDMRG/sim_status/wall_time'][-1])
-            resets   .append(h5file['xDMRG/sim_status/num_resets'][-1])
+            chainlen.append(h5file['xDMRG/measurements/length'][-1])
+            realization_num.append([int(x) for x in regex.findall(realization_name)][-1])
+            variance.append(h5file['xDMRG/measurements/energy_variance_per_site'][-1])
+            walltime.append(h5file['xDMRG/sim_status/wall_time'][-1])
+            resets.append(h5file['xDMRG/sim_status/num_resets'][-1])
             converged.append(h5file['xDMRG/sim_status/simulation_has_converged'][-1])
             saturated.append(h5file['xDMRG/sim_status/simulation_has_saturated'][-1])
             succeeded.append(h5file['xDMRG/sim_status/simulation_has_succeeded'][-1])
             try:
-                finished .append(h5file['common/finOK'][-1])
+                finished.append(h5file['common/finOK'][-1])
             except:
                 finished.append(0)
 
             if not args.summary:
-                entry = "{:<15} {:<12.4f} {:>12.4f} {:>12} {:>12} {:>12} {:>12} {:>12}".format(
+                entry = "{:<8} {:<15} {:<12.4f} {:>12.4f} {:>12} {:>12} {:>12} {:>12} {:>12}".format(
+                    chainlen[-1],
                     realization_num[-1],
                     np.log10(variance[-1]),
-                    walltime[-1]/60,
+                    walltime[-1] / 60,
                     resets[-1],
                     converged[-1],
                     saturated[-1],
@@ -85,8 +90,11 @@ for dirName, subdirList, fileList in os.walk(args.directory):
         except Exception as er:
             print("Could not read dataset. Reason: ", er)
             continue
-    header="{:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Total sims", "Avg Var","Avg Time", "Avg Resets", "Sum Con", "Sum Sat", "Sum Suc", "Sum Fin")
-    entry="{:<15} {:<12.4f} {:>12.4f} {:>12.3f} {:>12} {:>12} {:>12} {:>12}".format(
+    header = "{:8} {:15} {:12} {:>12} {:>12} {:>12} {:>12} {:>12} {:>12}".format("Length","Total sims", "Avg Var", "Avg Time",
+                                                                                 "Avg Resets", "Sum Con", "Sum Sat",
+                                                                                 "Sum Suc", "Sum Fin")
+    entry = "{:<8} {:<15} {:<12.4f} {:>12.4f} {:>12.3f} {:>12} {:>12} {:>12} {:>12}".format(
+        np.int(np.mean(chainlen)),
         len(realization_num),
         np.log10(np.nanmean(variance)),
         np.mean(walltime) / 60,
