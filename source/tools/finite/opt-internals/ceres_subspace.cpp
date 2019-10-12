300107064670e51f0b9dbb00074796c2be09b002
DavidAce <aceituno@kth.se>
2019-10-12 17:41:18 +0200

    Even lower function tolerance in ceres -> 1e-12 when stuck

--- source/tools/finite/opt-internals/ceres_subspace.cpp
+++ source/tools/finite/opt-internals/ceres_subspace.cpp
@@ -474,7 +474,7 @@ tools::finite::opt::internal::ceres_subspace_optimization(const class_finite_sta
 
     if(sim_status.simulation_has_got_stuck){
 //        options.min_line_search_step_size = std::numeric_limits<double>::epsilon();
-        options.function_tolerance = 1e-8; //Operations are cheap in subspace, so you can afford low tolerance
+        options.function_tolerance = 1e-12; //Operations are cheap in subspace, so you can afford low tolerance
         options.max_num_iterations = 4000;
         options.gradient_tolerance = 1e-4;
         options.max_solver_time_in_seconds = 60*10;//60*2;
