4d88bd31ebdef5db4cbb398dbeb315cdf9e50011
DavidAce <aceituno@kth.se>
2019-12-09 16:01:36 +0100

    Extended omp testing

--- source/general/nmspc_omp.h
+++ source/general/nmspc_omp.h
@@ -18,16 +18,24 @@ public:
     #ifdef _OPENMP
     Eigen::ThreadPool       tp;
     Eigen::ThreadPoolDevice dev;
-    OMP(size_t num_threads_):
-    num_threads(num_threads_),
-    tp(num_threads),
-    dev(&tp, num_threads)
-    {}
+    explicit OMP(size_t num_threads_):
+        num_threads(num_threads_),
+        tp(num_threads),
+        dev(&tp, num_threads)
+        {}
+    explicit OMP():
+        num_threads(std::thread::hardware_concurrency()),
+        tp(std::thread::hardware_concurrency()),
+        dev(&tp, num_threads)
+        {}
     #else
     Eigen::DefaultDevice dev;
     OMP(size_t num_threads_):
-    num_threads(num_threads_)
-    {}
+        num_threads(num_threads_)
+        {}
+    explicit OMP():
+        num_threads(1)
+        {}
     #endif
 
 };
