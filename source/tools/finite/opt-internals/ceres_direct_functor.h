861420e8007688e83233f6212892aecc06f6ae99
DavidAce <aceituno@kth.se>
2019-12-09 23:23:45 +0100

    More detailed ceres test
    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
    
    
    Former-commit-id: f8c482c7c8492554d82a76dff640109dbb13980d

--- source/tools/finite/opt-internals/ceres_direct_functor.h
+++ source/tools/finite/opt-internals/ceres_direct_functor.h
@@ -26,7 +26,7 @@ namespace tools::finite::opt{
             void get_Hv   (const VectorType &v) const;
             void get_H2v  (const VectorType &v) const;
         public:
-//            EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+            EIGEN_MAKE_ALIGNED_OPERATOR_NEW
             explicit ceres_direct_functor(const class_state_finite & state, const class_simulation_status &sim_status);
             bool Evaluate(const double* v_double_double,
                           double* fx,
