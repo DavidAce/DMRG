3240cb6f873044e9c64ea694c34a497bb13c1605
DavidAce <aceituno@kth.se>
2019-12-11 09:47:00 +0100

    More robust lapacke checks

--- cmake-modules/FindLAPACKE.cmake
+++ cmake-modules/FindLAPACKE.cmake
@@ -1,6 +1,6 @@
 
 
-function(CheckLapackeCompiles REQUIRED_FLAGS REQUIRED_DEFINITIONS REQUIRED_LIBRARIES_UNPARSED REQUIRED_INCLUDES)
+function(CheckLapackeCompiles TAG REQUIRED_FLAGS REQUIRED_DEFINITIONS REQUIRED_LIBRARIES_UNPARSED REQUIRED_INCLUDES REQUIRED_TARGET)
 #    message(STATUS "Checking if lapacke headers are working")
     set(REQUIRED_LIBRARIES)
     include(cmake-modules/getExpandedTarget.cmake)
@@ -12,7 +12,21 @@ function(CheckLapackeCompiles REQUIRED_FLAGS REQUIRED_DEFINITIONS REQUIRED_LIBRA
             list(APPEND REQUIRED_LIBRARIES ${elem})
         endif()
     endforeach()
-    #   Test features
+
+    foreach(elem ${REQUIRED_TARGET})
+        if(TARGET ${elem})
+            expand_target_libs(${elem} expanded_libs)
+            expand_target_incs(${elem} expanded_incs)
+            expand_target_opts(${elem} expanded_opts)
+            list(APPEND REQUIRED_LIBRARIES ${expanded_libs})
+            list(APPEND REQUIRED_INCLUDES  ${expanded_incs})
+            list(APPEND REQUIRED_FLAGS     ${expanded_opts})
+        endif()
+    endforeach()
+    string(REPLACE ";" " " REQUIRED_FLAGS      "${REQUIRED_FLAGS}") # Needs to be a space-separated list
+
+
+#   Test features
     include(CheckCXXSourceCompiles)
     set(CMAKE_REQUIRED_FLAGS        ${REQUIRED_FLAGS})
     set(CMAKE_REQUIRED_DEFINITIONS  ${REQUIRED_DEFINITIONS})
@@ -44,19 +58,12 @@ function(CheckLapackeCompiles REQUIRED_FLAGS REQUIRED_DEFINITIONS REQUIRED_LIBRA
            info = LAPACKE_dgels(LAPACK_ROW_MAJOR,'N',m,n,nrhs,*a,lda,*b,ldb);
            return(info);
         }
-        " LAPACKE_COMPILES)
-    if(LAPACKE_COMPILES)
-#        message(STATUS "Lapacke compiles with the following parameters:")
-        set(LAPACKE_COMPILES TRUE PARENT_SCOPE)
+        " LAPACKE_COMPILES_${TAG})
+    if(LAPACKE_COMPILES_${TAG})
+        set(LAPACKE_COMPILES_${TAG} TRUE PARENT_SCOPE)
     else()
-#        message(STATUS "Lapacke does not compile with the following parameters:")
-        set(LAPACKE_COMPILES FALSE PARENT_SCOPE)
+        set(LAPACKE_COMPILES_${TAG} FALSE PARENT_SCOPE)
     endif()
-#    message(STATUS "    flags        : " ${REQUIRED_FLAGS})
-#    message(STATUS "    definitions  : " ${REQUIRED_DEFINITIONS})
-#    message(STATUS "    libraries    : " ${REQUIRED_LIBRARIES})
-#    message(STATUS "    includes     : " ${REQUIRED_INCLUDES})
-
 endfunction()
 
 
@@ -67,20 +74,14 @@ if (NOT TARGET lapacke)
     if(TARGET mkl)
         # Try finding lapacke in MKL library
         message(STATUS "Searching for Lapacke in Intel MKL.")
-        get_target_property(MKL_INCLUDE_DIR mkl INTERFACE_INCLUDE_DIRECTORIES)
-        get_target_property(MKL_LIBRARIES mkl INTERFACE_LINK_LIBRARIES)
-
         if(MKL_INCLUDE_DIR)
-            CheckLapackeCompiles(" "   "-DMKL_AVAILABLE"  "${MKL_LIBRARIES}" "${MKL_INCLUDE_DIR}")
+            CheckLapackeCompiles("MKL" ""  "-DMKL_AVAILABLE"  "" "" "mkl")
         endif()
 
-        if(LAPACKE_COMPILES)
+        if(LAPACKE_COMPILES_MKL)
             add_library(lapacke INTERFACE)
-            add_dependencies(lapacke INTERFACE mkl)
-            target_link_libraries(lapacke INTERFACE ${MKL_LIBRARIES})
-            target_include_directories(lapacke SYSTEM INTERFACE ${MKL_INCLUDE_DIR})
+            target_link_libraries(lapacke INTERFACE mkl)
             message(STATUS "Searching for Lapacke in Intel MKL - Success")
-            return()
         else()
             message(STATUS "Searching for Lapacke in Intel MKL - failed")
         endif()
@@ -93,17 +94,13 @@ endif()
 if (NOT TARGET lapacke)
     if (TARGET OpenBLAS)
         message(STATUS "Searching for Lapacke in OpenBLAS")
-        get_target_property(LAPACKE_OPENBLAS_LIBRARY   OpenBLAS INTERFACE_LINK_LIBRARIES)
-        get_target_property(LAPACKE_OPENBLAS_INCLUDE   OpenBLAS INTERFACE_INCLUDE_DIRECTORIES)
         if(LAPACKE_OPENBLAS_LIBRARY AND LAPACKE_OPENBLAS_INCLUDE)
-            CheckLapackeCompiles(" "   " "  "${LAPACKE_OPENBLAS_LIBRARY}" "${LAPACKE_OPENBLAS_INCLUDE}")
+            CheckLapackeCompiles("OpenBLAS" "" "" "" "" "OpenBLAS")
         endif()
-        if(LAPACKE_COMPILES)
+        if(LAPACKE_COMPILES_OpenBLAS)
             add_library(lapacke INTERFACE)
-            add_dependencies(lapacke INTERFACE OpenBLAS)
             target_link_libraries(lapacke INTERFACE OpenBLAS)
             message(STATUS "Searching for Lapacke in OpenBLAS - Success")
-            return()
         else()
             message(STATUS "Searching for Lapacke in OpenBLAS - failed")
         endif()
@@ -113,38 +110,43 @@ endif()
 
 
 if (NOT TARGET lapacke)
-    message(STATUS "Searching for Lapacke in system")
-    find_path(LAPACKE_INCLUDE_DIR
-            NAMES lapacke.h
-            HINTS ${LAPACKE_DIR} $ENV{CONDA_PREFIX}
-            PATHS
-            ${CMAKE_INSTALL_PREFIX}
-            ${CMAKE_INSTALL_PREFIX}/OpenBLAS
-            $ENV{BLAS_DIR}/include
-            /usr/include
-            /usr/include/x86_64-linux-gnu
-            )
-    find_library(LAPACKE_LIBRARY
-            NAMES liblapacke${CUSTOM_SUFFIX}
-            HINTS ${LAPACKE_DIR} $ENV{CONDA_PREFIX}
-            PATHS
-            $ENV{BLAS_DIR}/lib
-            ${CMAKE_INSTALL_PREFIX}
-            ${CMAKE_INSTALL_PREFIX}/OpenBLAS
-            /usr/lib
-            /usr/local/lib
-            )
-    if(LAPACKE_INCLUDE_DIR OR LAPACKE_LIBRARY)
-        CheckLapackeCompiles(" "   " "  "${LAPACKE_LIBRARY} " "${LAPACKE_INCLUDE_DIR} ")
-    endif()
-    if(LAPACKE_COMPILES)
-        add_library(lapacke INTERFACE IMPORTED)
-        target_link_libraries(lapacke INTERFACE ${LAPACKE_LIBRARY})
-        target_include_directories(lapacke SYSTEM INTERFACE ${LAPACKE_INCLUDE_DIR})
-        message(STATUS "Searching for Lapacke in system - Success: ${LAPACKE_LIBRARY}")
-        return()
-    else()
-        message(STATUS "Searching for Lapacke in system - failed")
+    if(TARGET lapack)
+        message(STATUS "Searching for Lapacke in system")
+        find_path(LAPACKE_INCLUDE_DIR
+                NAMES lapacke.h
+                HINTS ${LAPACKE_DIR} $ENV{CONDA_PREFIX}
+                PATHS
+                ${CMAKE_INSTALL_PREFIX}
+                ${CMAKE_INSTALL_PREFIX}/OpenBLAS
+                $ENV{BLAS_DIR}/include
+                /usr/include
+                /usr/include/x86_64-linux-gnu
+                )
+        find_library(LAPACKE_LIBRARY
+                NAMES liblapacke${CUSTOM_SUFFIX}
+                HINTS ${LAPACKE_DIR} $ENV{CONDA_PREFIX}
+                PATHS
+                $ENV{BLAS_DIR}/lib
+                ${CMAKE_INSTALL_PREFIX}
+                ${CMAKE_INSTALL_PREFIX}/OpenBLAS
+                /usr/lib
+                /usr/local/lib
+                )
+        if(LAPACKE_INCLUDE_DIR OR LAPACKE_LIBRARY)
+            CheckLapackeCompiles("SYSTEM" " "   " "
+                    "${LAPACKE_LIBRARY}"
+                    "${LAPACKE_INCLUDE_DIR}"
+                    "lapack"
+                    )
+        endif()
+        if(LAPACKE_COMPILES_SYSTEM)
+            add_library(lapacke INTERFACE IMPORTED)
+            target_link_libraries(lapacke INTERFACE ${LAPACKE_LIBRARY} lapack)
+            target_include_directories(lapacke SYSTEM INTERFACE ${LAPACKE_INCLUDE_DIR})
+            message(STATUS "Searching for Lapacke in system - Success: ${LAPACKE_LIBRARY}")
+        else()
+            message(STATUS "Searching for Lapacke in system - failed")
+        endif()
     endif()
 endif()
 
