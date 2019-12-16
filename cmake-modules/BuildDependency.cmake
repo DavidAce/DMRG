94c3cc50c90643cd31a27e6c7f25ab31ea76c352
DavidAce <aceituno@kth.se>
2019-12-16 07:50:06 +0100

    Linking order for lapacke/arpack is tricky
    Former-commit-id: 6558a9158bc3337569113f45086ae7aea1b8f1f8

--- cmake-modules/BuildDependency.cmake
+++ cmake-modules/BuildDependency.cmake
@@ -10,36 +10,36 @@ function(build_dependency dep_name install_dir extra_flags)
             ${PROJECT_SOURCE_DIR}/cmake-modules/external_${dep_name}
             WORKING_DIRECTORY ${build_dir}
             RESULT_VARIABLE config_result
-            ERROR_VARIABLE  config_error
+#            ERROR_VARIABLE  config_error
 
     )
     if(${config_result})
-        message(STATUS "Got non-zero exit code while configuring ${dep_name}")
+        message(STATUS "Got non-zero exit code ${config_result} while configuring ${dep_name}")
         message(STATUS  "build_dir         : ${build_dir}")
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "config_result     : ${config_result}")
         message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(WRITE ${build_dir}/stdout_config ${config_result})
-        file(WRITE ${build_dir}/stderr_config ${config_error})
+#        file(WRITE ${build_dir}/stdout_config ${config_result})
+#        file(WRITE ${build_dir}/stderr_config ${config_error})
     endif()
 
 
     execute_process(COMMAND  ${CMAKE_COMMAND} --build . --target all  --parallel
             WORKING_DIRECTORY "${build_dir}"
             RESULT_VARIABLE build_result
-            ERROR_VARIABLE  build_error
+#            ERROR_VARIABLE  build_error
             )
 
     if(${build_result})
-        message(STATUS "Got non-zero exit code while building ${dep_name}")
+        message(STATUS "Got non-zero exit code ${build_result} while building ${dep_name}")
         message(STATUS  "build_dir         : ${build_dir}")
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "build_result      : ${build_result}")
         message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(WRITE ${build_dir}/stdout_build ${build_result})
-        file(WRITE ${build_dir}/stderr_build ${build_error})
+#        file(WRITE ${build_dir}/stdout_build ${build_result})
+#        file(WRITE ${build_dir}/stderr_build ${build_error})
     endif()
 
 endfunction()
