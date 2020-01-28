Corrected bug on size_t subtraction of force_overlap Added lines in quick_overview to show lowest variance

batch_scripts/quick_overview.py
@@ -50,6 +50,7 @@ for dirName, subdirList, fileList in os.walk(args.directory):
     iter      = []
     step      = []
     variance  = []
+    variancel = []
     ententrp  = []
     walltime  = []
     resets    = []
@@ -62,7 +63,7 @@ for dirName, subdirList, fileList in os.walk(args.directory):
 
 
     if not args.summary:
-        header = "{:<8} {:<6} {:<6} {:<6} {:>12} {:>12} {:>12} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format("Length", "Seed", "Iter","Step", "Variance","Ent.Entr.", "Time",
+        header = "{:<8} {:<6} {:<6} {:<6} {:>12} {:>12} {:>12} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format("Length", "Seed", "Iter","Step", "VarNow", "VarLow","Ent.Entr.", "Time",
                                                                                                            "Resets", "Stk", "Sat" ,"Con", "Suc", "Fin")
         print(header)
         if args.save:
@@ -106,6 +107,7 @@ for dirName, subdirList, fileList in os.walk(args.directory):
             iter           .append(h5file[table_path].get('sim_status')['iteration'][-1])
             step           .append(h5file[table_path].get('sim_status')['step'][-1])
             variance       .append(h5file[table_path].get('measurements')['energy_variance_per_site'][-1])
+            variancel       .append(h5file[table_path].get('measurements')['energy_variance_per_site_lowest'][-1])
             ententrp       .append(h5file[table_path].get('measurements')['entanglement_entropy_midchain'][-1])
             walltime       .append(h5file[table_path].get('sim_status')['wall_time'][-1])
             resets         .append(h5file[table_path].get('sim_status')['num_resets'][-1])
@@ -145,12 +147,13 @@ for dirName, subdirList, fileList in os.walk(args.directory):
 
 
             if not args.summary:
-                entry = "{:<8} {:<6} {:<6} {:<6} {:>12.4f} {:>12.4f} {:>12.4f} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
+                entry = "{:<8} {:<6} {:<6} {:<6} {:>12.4f} {:>12.4f} {:>12.4f} {:>12.4f} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
                     chainlen[-1],
                     seed[-1],
                     iter[-1],
                     step[-1],
                     np.log10(variance[-1]),
+                    np.log10(variancel[-1]),
                     ententrp[-1],
                     walltime[-1] / 60,
                     resets[-1],
@@ -167,15 +170,16 @@ for dirName, subdirList, fileList in os.walk(args.directory):
         except Exception as er:
             print("Could not read dataset. Reason: ", er)
             continue
-    header = "{:<8} {:<6} {:<6} {:<6} {:>12} {:>11} {:>12} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format("Length","Sims", "<iter>","<step>", "<log10 Var>","<Entgl>","<Time>",
+    header = "{:<8} {:<6} {:<6} {:<6} {:>12} {:>12} {:>11} {:>12} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format("Length","Sims", "<iter>","<step>", "<VarNow>","<VarLow>","<Entgl>","<Time>",
                                                                                                               "Resets","Stk", "Sat", "Con",
                                                                                                               "Suc", "Fin")
-    entry = "{:<8} {:<6} {:<6.1f} {:<6.1f} {:>12.4f} {:>11.4f} {:>12.3f} {:>8.1f} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
-        np.int(np.mean(chainlen)),
+    entry = "{:<8} {:<6} {:<6.1f} {:<6.1f} {:>12.4f} {:>12.4f} {:>11.4f} {:>12.3f} {:>8.1f} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
+        np.int(np.sum(chainlen)/len(chainlen)),
         len(seed),
         np.nanmean(iter),
         np.nanmean(step),
         np.nanmean(np.log10(variance)),
+        np.nanmean(np.log10(variancel)),
         np.mean(ententrp),
         np.mean(walltime) / 60,
         np.sum(resets),