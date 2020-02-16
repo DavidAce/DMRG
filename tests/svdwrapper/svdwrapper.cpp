24bcddedf773fbca424f295e439b8cfc737c8e75
DavidAce <aceituno@kth.se>
2020-02-16 12:47:06 +0100

    Speeding up svd tests

--- tests/svdwrapper/svdwrapper.cpp
+++ tests/svdwrapper/svdwrapper.cpp
@@ -10,8 +10,9 @@ int main(){
     namespace fs = tools::fs;
     using reciter = fs::recursive_directory_iterator;
     for (auto & item : reciter(std::string(TEST_DIR) + "/testmatrices")){
+        if(item.path().extension() != ".h5") continue;
         std::cout <<  "item: " << item << std::endl;
-        int logLevel = 2;
+        int logLevel = 0;
         h5pp::File file(item.path().string(), h5pp::AccessMode::READONLY, h5pp::CreateMode::OPEN, logLevel);
         Eigen::MatrixXcd matrix;
         file.readDataset(matrix,"svdmatrix");
@@ -21,7 +22,8 @@ int main(){
         auto [U2,S2,V2] = SVD.decompose(matrix);
 
         std::cout << "Abs diff S2-S1: " << (S2.array()-S1.array()).cwiseAbs().sum() << std::endl;
-
+//        h5pp::File file2(std::string(TEST_DIR) + "/testmatrices_new2/"+ item.path().filename().string(), h5pp::AccessMode::READWRITE, h5pp::CreateMode::TRUNCATE, logLevel);
+//        file2.writeDataset(matrix,"svdmatrix",H5D_CONTIGUOUS);
     }
 
     return 0;
