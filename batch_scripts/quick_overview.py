a899647e66eb03f63a4c11b454706df73cf62479
DavidAce <aceituno@kth.se>
2020-02-29 11:47:11 +0100

    Generate multiple states per realization by flipping random spins after each simulation
    * Added multistate support for quick overview

--- batch_scripts/quick_overview.py
+++ batch_scripts/quick_overview.py
@@ -85,104 +85,115 @@ for dirName, subdirList, fileList in os.walk(args.directory):
             print("Could not open file [", h5path, "] Reason: ", er)
             continue
 
-        table_path = ''
-        step_results = 0
-        step_journal = 0
+        # Collect the number of states present.
+        state_keys = ""
         try:
-            if 'xDMRG/journal/sim_status' in h5file:
-                step_array =  h5file['xDMRG/journal'].get('sim_status')['step']
-                if(len(step_array)>0):
-                    step_journal = step_array[-1]
-            if 'xDMRG/results/sim_status' in h5file:
-                step_array = h5file['xDMRG/results'].get('sim_status')['step']
-                if(len(step_array)>0):
-                    step_results = step_array[-1]
+            if 'xDMRG' in h5file:
+                state_keys = [str(x) for x in h5file['xDMRG'].keys() if 'state' in x]
         except Exception as err:
-            print("Could not read sim_status. Reason:", err)
-        if step_results == 0 and step_journal == 0:
-            continue
-        if step_journal >= step_results:
-            table_path = 'xDMRG/journal'
-        else:
-            table_path = 'xDMRG/results'
-
-        # print(table_path,step_journal, step_results)
-        try:
+            print("Could not read keys in h5file. Reason:", err)
+        entry = []
+        for state_key in state_keys:
+            table_path = ''
+            step_results = 0
+            step_journal = 0
+            prefix = 'xDMRG/' + state_key
             try:
-                finished.append(h5file['common/finOK'][-1])
-            except:
-                finished.append(0)
-            if(args.finished and finished[-1] == 0):
+                if prefix + '/journal/sim_status' in h5file:
+                    step_array =  h5file[prefix + '/journal'].get('sim_status')['step']
+                    if(len(step_array)>0):
+                        step_journal = step_array[-1]
+                if prefix + '/results/sim_status' in h5file:
+                    step_array = h5file[prefix + '/results'].get('sim_status')['step']
+                    if(len(step_array)>0):
+                        step_results = step_array[-1]
+            except Exception as err:
+                print("Could not read sim_status. Reason:", err)
+            if step_results == 0 and step_journal == 0:
                 continue
+            if step_journal >= step_results:
+                table_path = prefix + '/journal'
+            else:
+                table_path = prefix + '/results'
+
+            # print(table_path,step_journal, step_results)
+            try:
+                try:
+                    finished.append(h5file['common/finOK'][-1])
+                except:
+                    finished.append(0)
+                if(args.finished and finished[-1] == 0):
+                    continue
 
 
-            realization_name = h5path.replace('.h5', '')
-            chainlen       .append(h5file[table_path].get('measurements')['length'][-1])
-            seed           .append([int(x) for x in regex.findall(realization_name)][-1])
-            iter           .append(h5file[table_path].get('sim_status')['iteration'][-1])
-            step           .append(h5file[table_path].get('sim_status')['step'][-1])
-            variance       .append(h5file[table_path].get('measurements')['energy_variance_per_site'][-1])
-            #variancel       .append(h5file[table_path].get('measurements')['energy_variance_per_site_lowest'][-1])
-            variancel       .append(get_data(h5file[table_path].get('measurements'),'energy_variance_per_site_lowest'))
-            ententrp       .append(h5file[table_path].get('measurements')['entanglement_entropy_midchain'][-1])
-            walltime       .append(h5file[table_path].get('sim_status')['wall_time'][-1])
-            resets         .append(h5file[table_path].get('sim_status')['num_resets'][-1])
-            got_stuck      .append(h5file[table_path].get('sim_status')['simulation_has_got_stuck'][-1])
-            saturated      .append(h5file[table_path].get('sim_status')['simulation_has_saturated'][-1])
-            converged      .append(h5file[table_path].get('sim_status')['simulation_has_converged'][-1])
-            succeeded      .append(h5file[table_path].get('sim_status')['simulation_has_succeeded'][-1])
+                realization_name = h5path.replace('.h5', '')
+                chainlen       .append(h5file[table_path].get('measurements')['length'][-1])
+                seed           .append([int(x) for x in regex.findall(realization_name)][-1])
+                iter           .append(h5file[table_path].get('sim_status')['iteration'][-1])
+                step           .append(h5file[table_path].get('sim_status')['step'][-1])
+                variance       .append(h5file[table_path].get('measurements')['energy_variance_per_site'][-1])
+                #variancel       .append(h5file[table_path].get('measurements')['energy_variance_per_site_lowest'][-1])
+                variancel       .append(get_data(h5file[table_path].get('measurements'),'energy_variance_per_site_lowest'))
+                ententrp       .append(h5file[table_path].get('measurements')['entanglement_entropy_midchain'][-1])
+                walltime       .append(h5file[table_path].get('sim_status')['wall_time'][-1])
+                resets         .append(h5file[table_path].get('sim_status')['num_resets'][-1])
+                got_stuck      .append(h5file[table_path].get('sim_status')['simulation_has_got_stuck'][-1])
+                saturated      .append(h5file[table_path].get('sim_status')['simulation_has_saturated'][-1])
+                converged      .append(h5file[table_path].get('sim_status')['simulation_has_converged'][-1])
+                succeeded      .append(h5file[table_path].get('sim_status')['simulation_has_succeeded'][-1])
 
 
 
-            h5file.close()
 
-            style = ''
-            if finished[-1] == 1 and succeeded[-1] == 1:
-                style = colored.bg("green_4")
-            elif finished[-1] == 1:
-                if variance[-1] < 1e-10:
-                    style = colored.bg("dark_green_sea")
-                elif variance[-1] < 1e-8:
-                    style = colored.bg("dark_orange")
-                else:
-                    style = colored.bg("red_3b")
-            elif finished[-1] == 0 and got_stuck[-1] == 1:
-                style = colored.fg("red_3b")
-                if variance[-1] < 1e-10:
-                    style = colored.fg("dark_green_sea")
-                else:
-                    style = colored.fg("dark_orange")
-            elif finished[-1] == 0 and got_stuck[-1] == 0 and converged[-1] == 0:
+
                 style = ''
-            elif finished[-1] == 0 and got_stuck[-1] == 0 and converged[-1] == 1:
-                style = colored.fg("green_4")
+                if finished[-1] == 1 and succeeded[-1] == 1:
+                    style = colored.bg("green_4")
+                elif finished[-1] == 1:
+                    if variance[-1] < 1e-10:
+                        style = colored.bg("dark_green_sea")
+                    elif variance[-1] < 1e-8:
+                        style = colored.bg("dark_orange")
+                    else:
+                        style = colored.bg("red_3b")
+                elif finished[-1] == 0 and got_stuck[-1] == 1:
+                    style = colored.fg("red_3b")
+                    if variance[-1] < 1e-10:
+                        style = colored.fg("dark_green_sea")
+                    else:
+                        style = colored.fg("dark_orange")
+                elif finished[-1] == 0 and got_stuck[-1] == 0 and converged[-1] == 0:
+                    style = ''
+                elif finished[-1] == 0 and got_stuck[-1] == 0 and converged[-1] == 1:
+                    style = colored.fg("green_4")
 
 
 
-            if not args.summary:
-                entry = "{:<8} {:<6} {:<6} {:<6} {:>12.4f} {:>12.4f} {:>12.4f} {:>12.4f} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
-                    chainlen[-1],
-                    seed[-1],
-                    iter[-1],
-                    step[-1],
-                    np.log10(variance[-1]),
-                    np.log10(variancel[-1]),
-                    ententrp[-1],
-                    walltime[-1] / 60,
-                    resets[-1],
-                    got_stuck[-1],
-                    saturated[-1],
-                    converged[-1],
-                    succeeded[-1],
-                    finished[-1])
-                print(stylize(entry, style))
+                if not args.summary:
+                    entry.append("{:<8} {:<6} {:<6} {:<6} {:>12.4f} {:>12.4f} {:>12.4f} {:>12.4f} {:>8} {:>5} {:>5} {:>5} {:>5} {:>5}".format(
+                        chainlen[-1],
+                        seed[-1],
+                        iter[-1],
+                        step[-1],
+                        np.log10(variance[-1]),
+                        np.log10(variancel[-1]),
+                        ententrp[-1],
+                        walltime[-1] / 60,
+                        resets[-1],
+                        got_stuck[-1],
+                        saturated[-1],
+                        converged[-1],
+                        succeeded[-1],
+                        finished[-1]))
+                    print(stylize(entry[-1], style))
 
-                if args.save:
-                    file.write(entry + '\n')
+                    if args.save:
+                        file.write(entry[-1] + '\n')
 
-        except Exception as er:
-            print("Could not read dataset. Reason: ", er)
-            continue
+            except Exception as er:
+                print("Could not read dataset. Reason: ", er)
+                continue
+        h5file.close()
     if not fileList:
         continue
     if len(chainlen) == 0:
