03f9f7c4e0071b9e412036d7ed6368113918bdbf
DavidAce <aceituno@kth.se>
2020-03-03 12:58:01 +0100

    Generate multiple states per realization by flipping random spins after each simulation
    * Added multistate support for quick overview
    * Truncate less when randomizing into new state
    * Generate up to 8 states
    * Speed up of multitheta contraction
    * Apply sz pauli operator in randomization instead of sx
    
    
    Former-commit-id: f45bc8e81ade486f0c7744bae924030a0aedc099

--- batch_scripts/generate_input_sweep.py
+++ batch_scripts/generate_input_sweep.py
@@ -11,7 +11,7 @@ location    = "input"
 os.makedirs(location, exist_ok=True)
 
 
-sites        = np.array([16,20]) #np.linspace(16,36,6, dtype=int)
+sites        = np.array([28,36]) #np.linspace(16,36,6, dtype=int)
 # sites        = np.linspace(16,36,6, dtype=int)
 lambdas      = [0] # np.linspace(0,0.2,3)
 deltas       = [0] # np.linspace(-1.0,1.0,5)
