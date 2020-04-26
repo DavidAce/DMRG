27609b1fe9f5561f78b8faabf1d9c130f6314f2f
DavidAce <aceituno@kth.se>
2020-04-26 09:52:45 +0200

    New storage logic for resuming simulations
    Former-commit-id: 921d37fc6ab6a20c1a7341be7cc8aebb41528ddb

--- source/tools/common/io/h5table.cpp
+++ source/tools/common/io/h5table.cpp
@@ -8,26 +8,29 @@
 #include <h5pp/h5pp.h>
 #include <simulation/enums.h>
 
-void tools::common::io::h5table::write_sim_status(h5pp::File & h5ppFile, const std::string &path, const StorageLevel &storage_level, const class_simulation_status &sim_status) {
-    if(storage_level == StorageLevel::NONE) return;
-    log->trace("Appending simulation status table: {}...", path);
+void tools::common::io::h5table::write_sim_status(h5pp::File & h5ppFile, const std::string &prefix, const StorageLevel &storage_level, const class_simulation_status &sim_status) {
+    if(storage_level < StorageLevel::LIGHT) return;
+    std::string table_path = prefix + "/sim_status";
+    log->trace("Appending simulation status table: {}", table_path);
     h5pp_table_sim_status::register_table_type();
-    if(not h5ppFile.linkExists(path))
-        h5ppFile.createTable(h5pp_table_sim_status::h5_type, path, "simulation status");
-    h5ppFile.appendTableEntries(sim_status, path);
-    log->trace("Appending simulation status table: {}... OK", path);
+    if(not h5ppFile.linkExists(table_path))
+        h5ppFile.createTable(h5pp_table_sim_status::h5_type, table_path, "simulation status");
+    h5ppFile.appendTableEntries(sim_status, table_path);
 }
 
 
-void tools::common::io::h5table::write_profiling(h5pp::File & h5ppFile, const std::string &path, const StorageLevel &storage_level, const class_simulation_status &sim_status) {
-    log->trace("Appending profiling data to table: {}...", path);
+void tools::common::io::h5table::write_profiling(h5pp::File & h5ppFile, const std::string &prefix, const StorageLevel &storage_level, const class_simulation_status &sim_status) {
+    if(storage_level < StorageLevel::LIGHT) return;
+    std::string table_path = prefix + "/profiling";
+    log->trace("Appending profiling data to table: {}", table_path);
+
     h5pp_table_profiling::register_table_type();
-    if(not h5ppFile.linkExists(path))
-        h5ppFile.createTable(h5pp_table_profiling::h5_type, path, "profiling");
+    if(not h5ppFile.linkExists(table_path))
+        h5ppFile.createTable(h5pp_table_profiling::h5_type, table_path, "profiling");
 
     h5pp_table_profiling::table profiling_entry;
+    profiling_entry.iter            = sim_status.iter;
     profiling_entry.step            = sim_status.step;
-    profiling_entry.iteration       = sim_status.iteration;
     profiling_entry.position        = sim_status.position;
     profiling_entry.t_tot           = tools::common::profile::t_tot->get_age();
     profiling_entry.t_pre           = tools::common::profile::t_pre->get_measured_time();
@@ -37,7 +40,7 @@ void tools::common::io::h5table::write_profiling(h5pp::File & h5ppFile, const st
     profiling_entry.t_eig           = tools::common::profile::t_eig->get_measured_time();
     profiling_entry.t_svd           = tools::common::profile::t_svd->get_measured_time();
     profiling_entry.t_ham           = tools::common::profile::t_ham->get_measured_time();
-    profiling_entry.t_ham_sq        = tools::common::profile::t_ham_sq->get_measured_time();
+    profiling_entry.t_hsq           = tools::common::profile::t_hsq->get_measured_time();
     profiling_entry.t_mpo           = tools::common::profile::t_mpo->get_measured_time();
     profiling_entry.t_opt           = tools::common::profile::t_opt->get_measured_time();
     profiling_entry.t_evo           = tools::common::profile::t_evo->get_measured_time();
@@ -57,7 +60,26 @@ void tools::common::io::h5table::write_profiling(h5pp::File & h5ppFile, const st
     profiling_entry.t_vH2           = tools::common::profile::t_vH2->get_measured_time();
     profiling_entry.t_vH            = tools::common::profile::t_vH->get_measured_time();
     profiling_entry.t_op            = tools::common::profile::t_op->get_measured_time();
-    h5ppFile.appendTableEntries(profiling_entry, path);
-    tools::log->trace("Appending profiling data to table: {}... OK", path);
-
+    h5ppFile.appendTableEntries(profiling_entry, table_path);
 }
+
+void tools::common::io::h5table::write_mem_usage(h5pp::File & h5ppFile, const std::string &prefix, const StorageLevel &storage_level, const class_simulation_status &sim_status) {
+    if(storage_level < StorageLevel::LIGHT) return;
+    std::string table_path = prefix + "/mem_usage";
+    log->trace("Appending memory usage data to table: {}", table_path);
+
+    h5pp_table_memory_usage::register_table_type();
+    if(not h5ppFile.linkExists(table_path))
+        h5ppFile.createTable(h5pp_table_memory_usage::h5_type, table_path, "memory usage");
+
+    h5pp_table_memory_usage::table mem_usage_entry{};
+    mem_usage_entry.iter           = sim_status.iter;
+    mem_usage_entry.step           = sim_status.step;
+    mem_usage_entry.rss            = tools::common::profile::mem_rss_in_mb();
+    mem_usage_entry.hwm            = tools::common::profile::mem_hwm_in_mb();
+    mem_usage_entry.vm             = tools::common::profile::mem_vm_in_mb();
+    h5ppFile.appendTableEntries(mem_usage_entry, table_path);
+}
+
+
+
