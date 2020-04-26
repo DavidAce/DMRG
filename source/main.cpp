8e42a0620ff900c3704b17d924fdb1a02a45d23b
DavidAce <aceituno@kth.se>
2020-04-26 09:50:13 +0200

    Completely new parsing of CLI options and started to implement simulation resume logic
    Former-commit-id: eb101febec3c1ef284413ca9bc5d89a9ace6cab7

--- source/main.cpp
+++ source/main.cpp
@@ -8,139 +8,134 @@
 #include <iostream>
 #include <math/nmspc_random.h>
 #include <simulation/nmspc_settings.h>
-
+#include <tools/common/log.h>
 #ifdef _OPENMP
-#include <omp.h>
+    #include <omp.h>
 #endif
 #include <general/nmspc_tensor_omp.h>
 
 #ifdef OpenBLAS_AVAILABLE
-#include <cblas.h>
-#include <openblas_config.h>
+    #include <cblas.h>
+    #include <openblas_config.h>
 #endif
-
 
 #ifdef MKL_AVAILABLE
-#define MKL_Complex8 std::complex<float>
-#define MKL_Complex16 std::complex<double>
-#include <mkl_service.h>
-#include <mkl.h>
+    #define MKL_Complex8 std::complex<float>
+    #define MKL_Complex16 std::complex<double>
+    #include <mkl.h>
+    #include <mkl_service.h>
 #endif
 
-#include <thread>
 #include <getopt.h>
 #include <gitversion.h>
+#include <thread>
 
-
-void print_usage(){
-
-std::cout<<
-R"(
-==========  DMRG++  ============
-Usage                       : DMRG++ [-option <value>].
-NOTE                        : Order of argument matters. In particular, set seeds AFTER the input file.
--h                          : Help. Shows this text.
--c <existing .cfg filename> : Full or relative path of the config file that sets up the simulation (default = input.cfg)
--l <existing hdf5 filename> : Load from the given file (i.e., from a previous simulation).
--r <seed>                   : Positive number that seeds the random number generator (default = 1)
--s <state number>           : Positive number whose bitfield sets the initial product state. Negative is unused (default -1)
--t <num threads>            : Number of OpenMP threads
--o <output filename>        : Full or relative path to the output file (output). The seed number will be appended to this filename unless -x is passed.
--x                          : Do not append seed to the output filename.
-
-)";
-
-}
-
-
-#include <signal.h>
+#include <csignal>
+#include <simulation/class_dmrg_config.h>
 
 void signal_callback_handler(int signum) {
-    switch(signum){
-        case SIGTERM:  {std::cout  << "Caught SIGTERM" <<std::endl; break;}
-        case SIGKILL:  {std::cout  << "Caught SIGKILL" <<std::endl; break;}
-        case SIGINT :  {std::cout  << "Caught SIGINT"  <<std::endl; break;}
-        case SIGHUP :  {std::cout  << "Caught SIGHUP"  <<std::endl; break;}
-        case SIGQUIT : {std::cout  << "Caught SIGQUIT" <<std::endl; break;}
+    switch(signum) {
+        case SIGTERM: {
+            std::cout << "Caught SIGTERM" << std::endl;
+            break;
+        }
+        case SIGKILL: {
+            std::cout << "Caught SIGKILL" << std::endl;
+            break;
+        }
+        case SIGINT: {
+            std::cout << "Caught SIGINT" << std::endl;
+            break;
+        }
+        case SIGHUP: {
+            std::cout << "Caught SIGHUP" << std::endl;
+            break;
+        }
+        case SIGQUIT: {
+            std::cout << "Caught SIGQUIT" << std::endl;
+            break;
+        }
+        default: break;
     }
     std::cout << "Exiting" << std::endl << std::flush;
     std::quick_exit(signum);
 }
 
-std::string filename_append_number(const std::string_view filename,const long number){
-    if(number < 0) return std::string(filename);
-    auto log = Logger::setLogger("DMRG",0);
-    //Append the seed_model to the output filename
-    h5pp::fs::path oldFileName = filename;
-    h5pp::fs::path newFileName = filename;
-    newFileName.replace_filename(oldFileName.stem().string() + "_" + std::to_string(number) + oldFileName.extension().string() );
-    log->info("Appended number [{}] to filename: [{}]",number, newFileName.string());
-    return newFileName.string();
+void print_usage() {
+    std::cout <<
+        R"(
+==========  DMRG++  ============
+Usage                       : DMRG++ [-option <value>].
+-h                          : Help. Shows this text.
+-b <positive integer>       : Integer whose bitfield sets the initial product state. Negative is unused (default -1)
+-c <.cfg or .h5 filename>   : Full or relative path to a config file or hdf5 file from a previous simulation (which has a config file) (default = input.cfg)
+-s <seed>                   : Positive number that seeds the random number generator (default = 1)
+-t <num threads>            : Number of OpenMP threads
+-o <output filename base>   : Full or relative path to the output file (output). The seed number will be appended to this filename unless -x is passed.
+-v                          : Enables trace-level verbosity
+-x                          : Do not append seed to the output filename.
+
+)";
 }
 
-
+std::string filename_append_number(const std::string &filename, const long number) {
+    if(number < 0) return filename;
+    // Append the seed_model to the output filename
+    h5pp::fs::path oldFileName = filename;
+    h5pp::fs::path newFileName = filename;
+    if(oldFileName.stem().string().find(std::to_string(number)) != std::string::npos) return filename;
+    newFileName.replace_filename(oldFileName.stem().string() + "_" + std::to_string(number) + oldFileName.extension().string());
+    tools::log->info("Appended number [{}] to filename: [{}]", number, newFileName.string());
+    return newFileName.string();
+}
 
 /*!
     \brief  Main function. Sets simulation parameters and excecutes the desired algorithms.
     \return an integer 0 upon exit success
 */
 
-int main(int argc, char* argv[]) {
-    //Register termination codes and what to do in those cases
-    signal(SIGTERM , signal_callback_handler);
-    signal(SIGINT  , signal_callback_handler);
-    signal(SIGKILL , signal_callback_handler);
-    signal(SIGHUP  , signal_callback_handler);
-    signal(SIGQUIT , signal_callback_handler);
-
-    auto log = Logger::setLogger("DMRG",0);
+int main(int argc, char *argv[]) {
+    // Register termination codes and what to do in those cases
+    signal(SIGTERM, signal_callback_handler);
+    signal(SIGINT, signal_callback_handler);
+    signal(SIGKILL, signal_callback_handler);
+    signal(SIGHUP, signal_callback_handler);
+    signal(SIGQUIT, signal_callback_handler);
+    tools::log = Logger::setLogger("DMRG++ init", 0, true);
     using namespace tools;
     // print current Git status
-    log->info("Git branch      : {}",GIT::BRANCH);
-    log->info("    commit hash : {}",GIT::COMMIT_HASH);
-    log->info("    revision    : {}",GIT::REVISION);
+    tools::log->info("Git branch      : {}", GIT::BRANCH);
+    tools::log->info("    commit hash : {}", GIT::COMMIT_HASH);
+    tools::log->info("    revision    : {}", GIT::REVISION);
 
+    // Here we use getopt to parse CLI input
+    // Note that CLI input always override config-file values
+    // wherever they are found (config file, h5 file)
 
+    bool        append_seed = true;
+    std::string config;
+    std::string output;
+    long        verbosity   = -1;
+    long        seed        = -1;
+    long        bitfield    = -1;
+    long        num_threads = -1;
 
-    bool append_seed = true;
-    std::string config_filename;
-    std::string h5_load_filename;
-    while(true){
-        char opt = getopt(argc, argv, "hc:l:r:s:t:o:x");
-        if (opt == EOF) break;
-        if(optarg == nullptr) log->info("Parsing input argument: -{}",opt);
-        else                  log->info("Parsing input argument: -{} {}",opt,optarg);
-
-        switch(opt){
-            case 'c': config_filename = std::string(optarg); continue;
-            case 'l': h5_load_filename = std::string(optarg); continue;
-            case 'r': {
-                long seed = std::strtol(optarg,nullptr,10);
-                if(seed >= 0){
-                    log->info("Replacing model::seed {} -> {}", settings::model::seed, seed);
-                    settings::model::seed = seed;
-                }
-                continue;
-            }
-            case 's': {
-                long state_number =  std::strtol(optarg,nullptr,10);
-                if(state_number >= 0) {
-                    log->info("Replacing model::state_number {} -> {}", settings::model::state_number,state_number);
-                    settings::model::state_number = state_number;
-                }
-                continue;
-            }
-            case 't': {
-                int num_threads = std::strtol(optarg,nullptr,10);
-                if(num_threads > 0) {
-                    log->info("Setting OpenMP threads to {}", num_threads);
-                    settings::threading::num_threads = num_threads;
-                }
-                continue;
-            }
-            case 'o': settings::output::output_filename = std::string(optarg); continue;
+    while(true) {
+        char opt = static_cast<char>(getopt(argc, argv, "hb:c:s:t:o:vx"));
+        if(opt == EOF) break;
+        if(optarg == nullptr)
+            tools::log->info("Parsing input argument: -{}", opt);
+        else
+            tools::log->info("Parsing input argument: -{} {}", opt, optarg);
+        switch(opt) {
+            case 'b': bitfield = std::strtol(optarg, nullptr, 10); continue;
+            case 'c': config = std::string(optarg); continue;
+            case 's': seed = std::strtol(optarg, nullptr, 10); continue;
+            case 't': num_threads = std::strtol(optarg, nullptr, 10); continue;
+            case 'o': output = std::string(optarg); continue;
+            case 'v': verbosity = 0; continue;
             case 'x': append_seed = false; continue;
-            case ':': log->error("Option -{} needs a value", opt); break;
+            case ':': tools::log->error("Option -{} needs a value", opt); break;
             case 'h':
             case '?':
             default: print_usage(); exit(0);
@@ -148,119 +143,96 @@ int main(int argc, char* argv[]) {
         }
         break;
     }
+    /*
+     There may be multiple config files to consider:
+         1) Given from CLI (.cfg/.h5)
+         2) Inside the output file "<output>_<seed>.h5" if it already exists.
 
+     What should one do?
+     Simplest solution: Always ignore case 2)!
+     Taking 2) into account leads to very confusing policies.
 
-    /*! There are some ways this could go. This decision tree goes in order from A:
-        A1: hdf5 file not given: go to B
-        A2: hdf5 file given: Load hdf5 file config -> go to B
+     What are the implications?
+        - If CLI passes a .cfg file we have to override some of its settings:
+            - output
 
-        B1: config file not given: go to C if coming from A2, else use the default
-        B2: config file given: load given config -> go to C
-
-        C: generate output filename -> go to D
-
-        D1: output file exists: check option in settings::output::create_mode:
-            - case OPEN:        Load config and simulation state -> continue simulation
-            - case RENAME:      Rename output file -> go to D2
-            - case TRUNCATE:    Delete output file -> go to D2
-
-        D2: output file does not exist: Check if coming from A2
-            - Yes: load simulation state from given hdf5 file -> continue simulation
-            - No: start new simulation
-
-        NOTE that D is done separately inside of each algorithm (iDMRG, iTEBD... etc)
     */
 
-    // A: Try loading config from given hdf5 file
-    if(not h5_load_filename.empty()){
-        settings::input::h5_load_filename = h5_load_filename;
-        // A2: If we were given a previous simulation file it must be loaded first
-        h5pp::File h5file (settings::input::h5_load_filename, h5pp::AccessMode::READONLY,h5pp::CreateMode::OPEN);
-        settings::load_config_from_hdf5(h5file);
-    }
+    /*! It's important that we do things in this order:
+        A1: config file not given:  use the default input/input.cfg
+        A2: config file given with .cfg/h5 extension: load given config
 
-    //B: Try loading given config file.
+        B: Override settings with parameters given through CLI
+
+        C: generate output filename. If the seed is already on the filename, it is not appended again.
+    */
+
+    // B: Try loading given config file.
     //   Note that there is a default "input/input.cfg" if none was given
-    if(not config_filename.empty()){
-        settings::input::config_filename = config_filename;
-        class_config_reader indata(settings::input::config_filename);
-        if(indata.found_file){
-            // B2
-            settings::load_config_from_cfg(indata);
-        }else{
-            // Invalid file given
-            throw std::runtime_error(fmt::format("Could not find config file: {}", settings::input::config_filename));
-        }
-    }//else use default cfg
+    if(not config.empty()) {
+        class_dmrg_config dmrg_config(config);
+        if(dmrg_config.file_exists) {
+            dmrg_config.load();
+            settings::load_config(dmrg_config); // B2
+        } else
+            throw std::runtime_error(fmt::format("Could not find config file: {}", config)); // Invalid file given
+        settings::input::config_filename = config;
+    } // else use default cfg
 
+    // B: Override settings
+    if(seed >= 0) settings::input::seed = seed;
+    if(bitfield >= 0) settings::input::bitfield = bitfield;
+    if(num_threads >= 0) settings::threading::num_threads = num_threads;
+    if(not output.empty()) settings::output::output_filepath = output;
+    if(verbosity >= 0) settings::console::verbosity = verbosity;
+    tools::log = Logger::setLogger("DMRG++ init", settings::console::verbosity, settings::console::timestamp);
 
     // C: Generate the correct output filename based on given seeds
-    if (append_seed) {
-        settings::output::output_filename = filename_append_number(settings::output::output_filename, settings::model::seed);
-        settings::output::output_filename = filename_append_number(settings::output::output_filename, settings::model::state_number);
+    if(append_seed) {
+        settings::output::output_filepath = filename_append_number(settings::output::output_filepath, settings::input::seed);
+        settings::output::output_filepath = filename_append_number(settings::output::output_filepath, settings::input::bitfield);
     }
 
-    // D
-    if(h5pp::fs::exists(settings::output::output_filename)){
-        switch(settings::output::create_mode){
-            case h5pp::CreateMode::OPEN : {
-
-            }
-            case h5pp::CreateMode::RENAME : {
-
-            }
-            case h5pp::CreateMode::TRUNCATE : {
-
-            }
-        }
-
-    }else{
-
+// Set the number of threads to be used
+#ifdef _OPENMP
+    if(settings::threading::num_threads <= 0) {
+        settings::threading::num_threads = (int) std::thread::hardware_concurrency();
     }
 
-    //Set the number of threads to be used
+    omp_set_num_threads(settings::threading::num_threads);
+    Eigen::setNbThreads(settings::threading::num_threads);
+    tools::log->info("Using Eigen  with {} threads", Eigen::nbThreads());
+    tools::log->info("Using OpenMP with {} threads", omp_get_max_threads());
 
-    #ifdef _OPENMP
-        if(settings::threading::num_threads <= 0) { settings::threading::num_threads = (int)std::thread::hardware_concurrency(); }
-
-        omp_set_num_threads(settings::threading::num_threads);
-        Eigen::setNbThreads(settings::threading::num_threads);
-        log->info("Using Eigen  with {} threads",Eigen::nbThreads());
-        log->info("Using OpenMP with {} threads",omp_get_max_threads());
-
-        #ifdef OpenBLAS_AVAILABLE
-                openblas_set_num_threads(settings::threading::num_threads);
-                std::cout << OPENBLAS_VERSION
-                          << " compiled with parallel mode " << openblas_get_parallel()
-                          << " for target " << openblas_get_corename()
-                          << " with config " << openblas_get_config()
-                          << " with multithread threshold " << OPENBLAS_GEMM_MULTITHREAD_THRESHOLD
-                          << ". Running with " << openblas_get_num_threads() << " thread(s)" << std::endl;
-        #endif
-
-        #ifdef MKL_AVAILABLE
-            mkl_set_num_threads(settings::threading::num_threads);
-            log->info("Using Intel MKL with {} threads", mkl_get_max_threads());
-        #endif
-
+    #ifdef OpenBLAS_AVAILABLE
+    openblas_set_num_threads(settings::threading::num_threads);
+    tools::log->info("{} compiled with parallel mode {} for target {} with config {} | multithread threshold {} | running with {} threads", OPENBLAS_VERSION,
+                     openblas_get_parallel(), openblas_get_corename(), openblas_get_config(), OPENBLAS_GEMM_MULTITHREAD_THRESHOLD, openblas_get_num_threads());
+        //                std::cout << OPENBLAS_VERSION
+        //                          << " compiled with parallel mode " << openblas_get_parallel()
+        //                          << " for target " << openblas_get_corename()
+        //                          << " with config " << openblas_get_config()
+        //                          << " with multithread threshold " << OPENBLAS_GEMM_MULTITHREAD_THRESHOLD
+        //                          << ". Running with " << openblas_get_num_threads() << " thread(s)" << std::endl;
     #endif
 
+    #ifdef MKL_AVAILABLE
+    mkl_set_num_threads(settings::threading::num_threads);
+    tools::log->info("Using Intel MKL with {} threads", mkl_get_max_threads());
+    #endif
+
+#endif
 
     // Seed with random::device initially (This also takes care of srand used by Eigen)
-    // Note that we re-seed if a seed_model or seed_state is given also.
     // This is to make reproducible simulations
-    rn::seed(settings::model::seed);
+    rn::seed(settings::input::seed);
 
-
-    //Initialize the algorithm class
-    //This class stores simulation data automatically to a file specified in the config file
+    // Initialize the algorithm class
+    // This class stores simulation data automatically to a file specified in the config file
     class_algorithm_launcher launcher;
 
-    //Run the algorithms
+    // Run the algorithms
     launcher.run_algorithms();
 
     return 0;
 }
-
-
-
