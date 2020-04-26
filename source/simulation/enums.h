87e46266e4737af4c57e3881439ebe9fdcde3017
DavidAce <aceituno@kth.se>
2020-04-26 09:56:08 +0200

    * New file collision policy * Rename some nums to better capture their meaning
    Former-commit-id: e12e12ebf77914594d353f75eaafde56b016f4bd

--- source/simulation/enums.h
+++ source/simulation/enums.h
@@ -5,8 +5,9 @@
 #include <stdexcept>
 enum class SimulationType { iDMRG, fDMRG, xDMRG, iTEBD };
 enum class StorageLevel { NONE, LIGHT, NORMAL, FULL };
-enum class StorageReason {FINISH, CHI_UPDATE, JOURNAL};
+enum class StorageReason { JOURNAL,RESULTS, CHI_UPDATE,PROJECTION};
 enum class StopReason { SUCCEEDED, SATURATED, MAX_ITERS, MAX_RESET, RANDOMIZE,NONE };
+enum class FileCollisionPolicy {RESUME,BACKUP,RENAME,REPLACE};
 enum class PerturbMode {
     PERCENTAGE,                // J_ptb = couplingPtb * J_rnd
     ABSOLUTE,                  // J_ptb = couplingPtb
@@ -14,8 +15,10 @@ enum class PerturbMode {
     UNIFORM_RANDOM_ABSOLUTE,   // J_ptb = std::random_uniform(-couplingPtb, couplingPtb)
 };
 
+
+
 template<typename T >
-constexpr auto enum2str(T & item) {
+constexpr auto enum2str(const T & item) {
     if constexpr (std::is_same_v<T,SimulationType>){
         if (item == SimulationType::iDMRG) return "iDMRG";
         if (item == SimulationType::fDMRG) return "fDMRG";
@@ -31,15 +34,16 @@ constexpr auto enum2str(T & item) {
         if (item == StopReason::NONE)       return "NONE";
     }
     if constexpr (std::is_same_v<T,StorageLevel>) {
-        if (item == StorageLevel::NONE)     return "NONE";
-        if (item == StorageLevel::LIGHT)    return "LIGHT";
-        if (item == StorageLevel::NORMAL)   return "NORMAL";
-        if (item == StorageLevel::FULL)     return "FULL";
+        if (item == StorageLevel::NONE)   return "NONE";
+        if (item == StorageLevel::LIGHT)  return "LIGHT";
+        if (item == StorageLevel::NORMAL) return "NORMAL";
+        if (item == StorageLevel::FULL)   return "FULL";
     }
     if constexpr (std::is_same_v<T,StorageReason>) {
-        if (item == StorageReason::FINISH)      return "FINISH";
-        if (item == StorageReason::CHI_UPDATE)  return "CHI_UPDATE";
         if (item == StorageReason::JOURNAL)     return "JOURNAL";
+        if (item == StorageReason::RESULTS)     return "RESULTS";
+        if (item == StorageReason::CHI_UPDATE)  return "CHI_UPDATE";
+        if (item == StorageReason::PROJECTION)  return "PROJECTION";
     }
     if constexpr (std::is_same_v<T,PerturbMode>){
         if (item == PerturbMode::PERCENTAGE               ) return "PERCENTAGE";
@@ -47,14 +51,20 @@ constexpr auto enum2str(T & item) {
         if (item == PerturbMode::UNIFORM_RANDOM_PERCENTAGE) return "UNIFORM_RANDOM_PERCENTAGE";
         if (item == PerturbMode::UNIFORM_RANDOM_ABSOLUTE  ) return "UNIFORM_RANDOM_ABSOLUTE";
     }
-    if constexpr (std::is_same_v<T,h5pp::AccessMode>){
-        if (item == h5pp::AccessMode::READWRITE) return "READWRITE";
-        if (item == h5pp::AccessMode::READONLY) return "READONLY";
+    if constexpr (std::is_same_v<T, FileCollisionPolicy>){
+        if (item == FileCollisionPolicy::RESUME) return "RESUME";
+        if (item == FileCollisionPolicy::RENAME) return "RENAME";
+        if (item == FileCollisionPolicy::BACKUP) return "BACKUP";
+        if (item == FileCollisionPolicy::REPLACE) return "REPLACE";
     }
-    if constexpr (std::is_same_v<T,h5pp::CreateMode>){
-        if (item == h5pp::CreateMode::OPEN) return "OPEN";
-        if (item == h5pp::CreateMode::TRUNCATE) return "TRUNCATE";
-        if (item == h5pp::CreateMode::RENAME) return "RENAME";
+
+    if constexpr (std::is_same_v<T,h5pp::FilePermission>){
+        if (item == h5pp::FilePermission::READONLY)         return "READONLY";
+        if (item == h5pp::FilePermission::COLLISION_FAIL)   return "COLLISION_FAIL";
+        if (item == h5pp::FilePermission::RENAME)           return "RENAME";
+        if (item == h5pp::FilePermission::READWRITE)        return "READWRITE";
+        if (item == h5pp::FilePermission::BACKUP)           return "BACKUP";
+        if (item == h5pp::FilePermission::REPLACE)          return "REPLACE";
     }
 
     throw std::runtime_error("Given invalid enum item");
@@ -83,9 +93,10 @@ constexpr auto str2enum(std::string_view item) {
         if (item == "FULL"   ) return StorageLevel::FULL;
     }
     if constexpr (std::is_same_v<T,StorageReason>) {
-        if (item == "FINISH")      return StorageReason::FINISH;
-        if (item == "CHI_UPDATE")  return StorageReason::CHI_UPDATE;
         if (item == "JOURNAL")     return StorageReason::JOURNAL;
+        if (item == "RESULTS")     return StorageReason::RESULTS;
+        if (item == "CHI_UPDATE")  return StorageReason::CHI_UPDATE;
+        if (item == "PROJECTION")  return StorageReason::PROJECTION;
     }
     if constexpr (std::is_same_v<T,PerturbMode>){
         if (item == "PERCENTAGE"                ) return  PerturbMode::PERCENTAGE;
@@ -93,14 +104,20 @@ constexpr auto str2enum(std::string_view item) {
         if (item == "UNIFORM_RANDOM_PERCENTAGE" ) return  PerturbMode::UNIFORM_RANDOM_PERCENTAGE;
         if (item == "UNIFORM_RANDOM_ABSOLUTE"   ) return  PerturbMode::UNIFORM_RANDOM_ABSOLUTE;
     }
-    if constexpr (std::is_same_v<T,h5pp::AccessMode>){
-        if (item == "READWRITE") return  h5pp::AccessMode::READWRITE;
-        if (item == "READONLY")  return  h5pp::AccessMode::READONLY ;
+    if constexpr (std::is_same_v<T, FileCollisionPolicy>){
+        if (item == "RESUME") return FileCollisionPolicy::RESUME;
+        if (item == "RENAME") return FileCollisionPolicy::RENAME;
+        if (item == "BACKUP") return FileCollisionPolicy::BACKUP;
+        if (item == "REPLACE") return FileCollisionPolicy::REPLACE;
     }
-    if constexpr (std::is_same_v<T,h5pp::CreateMode>){
-        if (item == "OPEN")     return h5pp::CreateMode::OPEN;
-        if (item == "TRUNCATE") return h5pp::CreateMode::TRUNCATE;
-        if (item == "RENAME")   return h5pp::CreateMode::RENAME;
+    if constexpr (std::is_same_v<T,h5pp::FilePermission>){
+        if (item == "READONLY")         return  h5pp::FilePermission::READONLY;
+        if (item == "COLLISION_FAIL")   return  h5pp::FilePermission::COLLISION_FAIL;
+        if (item == "RENAME")           return  h5pp::FilePermission::RENAME;
+        if (item == "READWRITE")        return  h5pp::FilePermission::READWRITE;
+        if (item == "BACKUP")           return  h5pp::FilePermission::BACKUP;
+        if (item == "REPLACE")          return  h5pp::FilePermission::REPLACE;
     }
-    throw std::runtime_error("Given invalid string item");
+
+    throw std::runtime_error("Given invalid string item: " + std::string(item));
 }
