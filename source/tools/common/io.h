312cc08f349b7709f78373bebee16b22498b6bca
DavidAce <aceituno@kth.se>
2020-04-26 09:50:59 +0200

    * Honor naming convention, path -> prefix * Implementing resume
    Former-commit-id: d835e8bee7cb0cc49be94946c4aa91ed3f0be2f4

--- source/tools/common/io.h
+++ source/tools/common/io.h
@@ -1,14 +1,12 @@
 #pragma once
+#include <map>
 #include <string>
-
 class class_simulation_status;
 enum class StorageLevel;
 
-namespace h5pp{
+namespace h5pp {
     class File;
 }
-
-
 
 namespace tools::common::io {
     /* clang-format off */
@@ -17,23 +15,43 @@ namespace tools::common::io {
     }
 
     namespace h5table{
-        extern void write_sim_status   (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_simulation_status &sim_status);
-        extern void write_profiling    (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_simulation_status &sim_status);
+        extern void write_sim_status   (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status &sim_status);
+        extern void write_profiling    (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status &sim_status);
+        extern void write_mem_usage    (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status &sim_status);
     }
 
-    namespace h5restore{
-        extern class_simulation_status load_sim_status_from_hdf5(const h5pp::File &h5ppFile, const std::string & path);
-        extern void                    load_profiling_from_hdf5 (const h5pp::File &h5ppFile, const std::string & path);
+    namespace h5attr{
+//        extern void write_prefix_meta (h5pp::File & h5ppFile, const std::string & prefix, const std::string & sim_name, const std::string &sim_tag, const StorageLevel & storage_level );
+        extern void write_prefix_meta (h5pp::File & h5ppFile, const std::string & prefix, const std::string & sim_name, const std::string &sim_tag, const std::string & model_type, const StorageLevel & storage_level, size_t iter, size_t step, size_t position );
+    }
+
+
+    namespace h5resume{
+        extern std::string             find_resumable_state     (const h5pp::File & h5ppFile, const std::string & sim_name, const std::string & search = "");
+        extern void                    load_sim_status_from_hdf5(const h5pp::File & h5ppFile, const std::string & prefix,class_simulation_status & sim_status);
+        extern void                    load_profiling_from_hdf5 (const h5pp::File & h5ppFile, const std::string & prefix);
     }
 
     namespace h5tmp{
-        extern std::string set_tmp_prefix(const std::string & output_filename);
-        extern std::string unset_tmp_prefix(const std::string & output_filename);
-        extern void copy_from_tmp(const std::string & output_filename);
-        extern void copy_file(const std::string & src, const std::string & tgt);
-        extern void create_directory(const std::string & dir);
-        extern void remove_from_temp(const std::string & output_filename);
+        namespace internal{
+            struct pathpair {
+             std::string original_path;
+             std::string temporary_path;
+            };
 
+            inline std::map<std::string,pathpair> file_register;
+            extern const pathpair & register_paths(const std::string & filepath);
+            extern const pathpair & get_paths(const std::string & filepath);
+            extern std::string get_tmp_dir();
+        }
+        extern const std::string & get_temporary_filepath(const std::string & filepath);
+        extern const std::string & get_original_filepath(const std::string & filepath);
+        extern void register_new_file (const std::string & filepath);
+        extern void copy_from_tmp(const std::string & filepath);
+        extern void copy_into_tmp(const std::string & filepath);
+        extern void copy_file(const std::string & src, const std::string & tgt);
+        extern void create_directory(const std::string & path);
+        extern void remove_from_tmp(const std::string & filepath);
     }
     /* clang-format off */
 }
--- source/tools/finite/io.h
+++ source/tools/finite/io.h
@@ -12,24 +12,27 @@ namespace h5pp {
 namespace tools::finite::io {
     /* clang-format off */
     namespace h5dset{
-        extern void write_all_state            (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_bond_matrices        (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_bond_matrix          (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_full_mps             (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_full_mpo             (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_model                (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
-        extern void write_array_measurements   (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_state_finite & state);
+        extern void write_all                  (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_state_finite & state);
+        extern void write_mps                  (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_state_finite & state);
+        extern void write_mpo                  (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_state_finite & state);
+        extern void write_array_measurements   (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_state_finite & state);
     }
 
     namespace h5table{
-        extern void write_measurements         (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_simulation_status & sim_status, const class_state_finite & state);
-        extern void write_sim_status           (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_simulation_status & sim_status);
-        extern void write_profiling            (h5pp::File & h5ppFile, const std::string & path, const StorageLevel & storage_level, const class_simulation_status & sim_status);
+        extern void write_model                (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_state_finite & state);
+        extern void write_measurements         (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status & sim_status, const class_state_finite & state);
+        extern void write_sim_status           (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status & sim_status);
+        extern void write_profiling            (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status & sim_status);
+        extern void write_mem_usage            (h5pp::File & h5ppFile, const std::string & prefix, const StorageLevel & storage_level, const class_simulation_status & sim_status);
+
     }
 
-    namespace h5restore{
-        extern void load_from_hdf5                      (const h5pp::File & h5ppFile, const std::string & path, class_simulation_status & sim_status,  class_state_finite & state);
-        extern class_state_finite load_state_from_hdf5  (const h5pp::File & h5ppFile, const std::string & path);
+    namespace h5resume{
+        extern void load_all (const h5pp::File & h5ppFile, const std::string & prefix, class_simulation_status & sim_status,  class_state_finite & state);
+        extern void load_mps (const h5pp::File & h5ppFile, const std::string & prefix,class_state_finite & state);
+        extern void load_mpo (const h5pp::File & h5ppFile, const std::string & prefix,class_state_finite & state);
     }
+
+
     /* clang-format on */
 }
