e3a74c179301ae5eea2bdb09107853b2a2fa2ab9
DavidAce <aceituno@kth.se>
2020-04-26 09:48:40 +0200

    Secure initialization working better with simulation resume
    Former-commit-id: 0e3e5ce6f6f609786b9022633eef92dcab2f4705

--- source/tools/finite/mpo.h
+++ source/tools/finite/mpo.h
@@ -2,9 +2,10 @@
 #include <simulation/enums.h>
 #include <string>
 
+/* clang-format off */
 class class_state_finite;
 namespace tools::finite::mpo {
-    extern void initialize                 (class_state_finite & state, size_t length, const std::string &model_type);
+    extern void initialize                 (class_state_finite & state,const std::string &model_type, size_t num_sites, size_t position);
     extern void randomize                  (class_state_finite & state);
     extern void perturb_hamiltonian        (class_state_finite & state, double coupling_ptb, double field_ptb, PerturbMode PerturbMode);
     extern void damp_hamiltonian           (class_state_finite & state, double coupling_damp, double field_damp);
@@ -12,3 +13,4 @@ namespace tools::finite::mpo {
     extern void reduce_mpo_energy_multi    (class_state_finite & state);
     extern void reduce_mpo_energy_2site    (class_state_finite & state);
 }
+/* clang-format on */
