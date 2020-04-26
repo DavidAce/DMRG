e79edce2afc9e4c6a1a89a7df593ad5d7da4a1ca
DavidAce <aceituno@kth.se>
2020-04-26 10:04:49 +0200

    Started using externalproject_add for arpack-ng and arpack++

--- cmake-modules/Fetch_arpack++.cmake
+++ cmake-modules/Fetch_arpack++.cmake
@@ -8,26 +8,17 @@ if (TARGET arpack::arpack++)
     message(STATUS "Arpack++ found")
 elseif(NOT ${DMRG_DOWNLOAD_METHOD} MATCHES "none")
     message(STATUS "Arpack++ will be installed into ${CMAKE_BINARY_DIR}/dmrg-deps-install/arpack++ on first build.")
-    include(ExternalProject)
-    ExternalProject_Add(external_ARPACK++
-            URL         https://github.com/m-reuter/arpackpp/archive/2.3.0.tar.gz
-            URL_MD5     1b09e35b6c44e118003922643b99978a
-            PREFIX      ${CMAKE_BINARY_DIR}/dmrg-deps-build/arpack++
-            INSTALL_DIR ${CMAKE_BINARY_DIR}/dmrg-deps-install/arpack++
-            UPDATE_COMMAND ""
-            TEST_COMMAND ""
-            INSTALL_COMMAND ""
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND
-            ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/include/arpack++
-            DEPENDS lapacke::lapacke arpack::arpack gfortran::gfortran
-            )
 
-    ExternalProject_Get_Property(external_ARPACK++ INSTALL_DIR)
-    add_library(arpack::arpack++ INTERFACE IMPORTED)
-    target_link_libraries(arpack::arpack++ INTERFACE lapacke::lapacke arpack::arpack)
-    target_include_directories(arpack::arpack++ SYSTEM INTERFACE ${INSTALL_DIR}/include)
-    add_dependencies(arpack::arpack++ external_ARPACK++)
+    include(${PROJECT_SOURCE_DIR}/cmake-modules/BuildDependency.cmake)
+    build_dependency(arpack++ "${CMAKE_INSTALL_PREFIX}/arpack++" "")
+    include(cmake-modules/FindArpack++.cmake)
+    find_Arpackpp()
+    if (TARGET arpack::arpack++)
+        message(STATUS "Successfully installed Arpack++")
+    else()
+        message(FATAL_ERROR "arpack-ng could not be downloaded.")
+    endif()
+
 else()
     message(FATAL_ERROR "Dependency Arpack++ not found and DMRG_DOWNLOAD_METHOD = ${DMRG_DOWNLOAD_METHOD}")
 endif()
