7a01bf4385ce11236639374c72aba0fb82c716d8
DavidAce <aceituno@kth.se>
2020-04-26 09:47:58 +0200

    Better macros from h5pp

--- source/io/nmspc_filesystem.h
+++ source/io/nmspc_filesystem.h
@@ -1,24 +1,15 @@
-//
-// Created by david on 2019-12-09.
-//
-
-#ifndef DMRG_NMSPC_FILESYSTEM_H
-#define DMRG_NMSPC_FILESYSTEM_H
-
-// Include filesystem or experimental/filesystem
-#if __has_include(<filesystem>)
+#pragma once
+#if __has_include(<ghc/filesystem.hpp>)
+// Last resort
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#elif __has_include(<filesystem>)
 #include <filesystem>
-    namespace tools{
-        namespace fs =  std::filesystem;
-    }
+#include <utility>
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
-namespace tools {
-    namespace fs = std::experimental::filesystem;
-}
+namespace fs = std::experimental::filesystem;
 #else
-#error Could not find <filesystem> or <experimental/filesystem>
+    #error Could not find includes: <filesystem> or <experimental/filesystem> <ghc/filesystem>
 #endif
-
-
-#endif //DMRG_NMSPC_FILESYSTEM_H
