98b3da49311d34f519c16fa95943f9ec676c0c80
DavidAce <aceituno@kth.se>
2020-05-02 11:18:37 +0200

    Hints and paths seem to have different search logic

--- cmake-modules/Fetch_arpack-ng.cmake
+++ cmake-modules/Fetch_arpack-ng.cmake
@@ -1,4 +1,3 @@
-include(GNUInstallDirs)
 if(DMRG_DOWNLOAD_METHOD MATCHES "find|fetch")
     foreach (tgt blas::blas;lapack::lapack;gfortran::gfortran)
         if(NOT TARGET ${tgt})
@@ -17,8 +16,7 @@ if(NOT TARGET arpack::arpack AND DMRG_DOWNLOAD_METHOD MATCHES "find|fetch")
     unset(arpack_ng_LIBRARIES)
     unset(arpack_ng_INCLUDE_DIRS)
     find_package(arpack-ng
-            HINTS ${CMAKE_INSTALL_PREFIX}
-            PATH_SUFFIXES arpack-ng/${CMAKE_INSTALL_LIBDIR}/cmake
+            PATHS ${CMAKE_INSTALL_PREFIX}
             NO_CMAKE_PACKAGE_REGISTRY)
     if(arpack_ng_LIBRARIES AND arpack_ng_INCLUDE_DIRS)
         message(STATUS "Found arpack-ng")
@@ -33,7 +31,6 @@ if (NOT TARGET arpack::arpack AND DMRG_DOWNLOAD_METHOD STREQUAL "find")
     message(STATUS "Looking for arpack-ng in system")
     find_library(arpack_ng_LIBRARIES
             NAMES arpack
-            PATH_SUFFIXES arpack-ng arpack arpack/lib arpack-ng/${CMAKE_INSTALL_LIBDIR}
             REQUIRED
             )
     if(NOT arpack_ng_LIBRARIES)
@@ -78,8 +75,7 @@ if(NOT TARGET arpack::arpack AND DMRG_DOWNLOAD_METHOD MATCHES "fetch")
     build_dependency(arpack-ng "${CMAKE_INSTALL_PREFIX}" "${ARPACK_CMAKE_OPTIONS}")
 
     find_package(arpack-ng
-            HINTS ${CMAKE_INSTALL_PREFIX}
-            PATH_SUFFIXES arpack-ng/${CMAKE_INSTALL_LIBDIR}/cmake
+            PATHS ${CMAKE_INSTALL_PREFIX}
             REQUIRED)
     if(arpack_ng_LIBRARIES AND arpack_ng_INCLUDE_DIRS)
         message(STATUS "Successfully installed arpack-ng")
