4d04f348e665d333c82f53a68f91487b6a75782f
DavidAce <aceituno@kth.se>
2020-05-02 14:49:46 +0200

    Replacing all the hints by using `CMAKE_PREFIX_PATH` instead
    Former-commit-id: 5194ae49681dadfcd041572a2578afdafe9ea42f

--- build.sh
+++ build.sh
@@ -250,6 +250,7 @@ if [ -z "$dry_run" ] ;then
           -DDMRG_ENABLE_TESTS:BOOL=$enable_tests \
           -DDMRG_ENABLE_OPENMP=$enable_openmp \
           -DDMRG_ENABLE_MKL=$enable_mkl \
+          --debug-find
            -G "$generator" \
            ../../
     exit_code=$?
