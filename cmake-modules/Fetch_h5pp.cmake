fb5374a9d3c3ea0d7d5ed3f28cc70f38d0630c53
DavidAce <aceituno@kth.se>
2020-05-03 13:56:30 +0200

    Fix finding dependencies
    Former-commit-id: 52f8614b0ba685c3b64053aeef96412e3b7b8d62

--- cmake-modules/Fetch_h5pp.cmake
+++ cmake-modules/Fetch_h5pp.cmake
@@ -6,9 +6,7 @@ if(NOT TARGET h5pp::h5pp AND DMRG_DOWNLOAD_METHOD STREQUAL "find")
 endif()
 
 if(NOT TARGET h5pp::h5pp AND DMRG_DOWNLOAD_METHOD MATCHES "fetch")
-    find_package(h5pp 1.7.0
-            HINTS ${CMAKE_INSTALL_PREFIX}
-            PATH_SUFFIXES h5pp
+    find_package(h5pp 1.7.1
             NO_CMAKE_PACKAGE_REGISTRY)
     if(h5pp_FOUND AND TARGET h5pp::h5pp)
         message(STATUS "Found h5pp")
@@ -19,15 +17,15 @@ if(NOT TARGET h5pp::h5pp AND DMRG_DOWNLOAD_METHOD MATCHES "fetch")
     message(STATUS "h5pp will be installed into ${CMAKE_INSTALL_PREFIX}")
     include(${PROJECT_SOURCE_DIR}/cmake-modules/BuildDependency.cmake)
     if(TARGET Eigen3::Eigen)
-        get_target_property(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
+        get_target_property(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_SYSTEM_INCLUDE_DIRECTORIES)
         list(APPEND H5PP_CMAKE_OPTIONS  -DEIGEN3_INCLUDE_DIR:PATH=${CMAKE_INSTALL_PREFIX}/Eigen3/include/eigen3)
     endif()
-    list(APPEND H5PP_CMAKE_OPTIONS  -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH})
+    list(APPEND H5PP_CMAKE_OPTIONS  -DH5PP_DIRECTORY_HINTS:PATH=${CMAKE_INSTALL_PREFIX})
     list(APPEND H5PP_CMAKE_OPTIONS  -DH5PP_DOWNLOAD_METHOD:BOOL=${DMRG_DOWNLOAD_METHOD})
     list(APPEND H5PP_CMAKE_OPTIONS  -DH5PP_PREFER_CONDA_LIBS:BOOL=${DMRG_PREFER_CONDA_LIBS})
     build_dependency(h5pp "${CMAKE_INSTALL_PREFIX}" "${H5PP_CMAKE_OPTIONS}")
-
-    find_package(h5pp 1.7.0 NO_CMAKE_PACKAGE_REGISTRY)
+    find_package(h5pp 1.7.1
+            NO_CMAKE_PACKAGE_REGISTRY)
     if(h5pp_FOUND AND TARGET h5pp::h5pp)
         message(STATUS "h5pp installed successfully")
     else()
