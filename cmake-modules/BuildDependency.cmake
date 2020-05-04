46e07b74da99a17e4b08fbe21c1b69e4e7b01664
DavidAce <aceituno@kth.se>
2020-05-04 10:34:16 +0200

    Make sure to avoid conda on static builds for glog
    Former-commit-id: a696fd1b8130ced3e2be7a999ac3057d8ed544e4

--- cmake-modules/BuildDependency.cmake
+++ cmake-modules/BuildDependency.cmake
@@ -4,10 +4,13 @@ function(build_dependency dep_name install_dir extra_flags)
         set(install_dir ${install_dir}/${dep_name})
         mark_as_advanced(install_dir)
     endif()
+    include(cmake-modules/GetNumThreads.cmake)
+    get_num_threads(num_threads)
 
     execute_process( COMMAND  ${CMAKE_COMMAND} -E make_directory ${build_dir})
     execute_process(
             COMMAND  ${CMAKE_COMMAND}
+            --parallel ${num_threads}
             -DCMAKE_INSTALL_PREFIX:PATH=${install_dir}
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
@@ -17,46 +20,47 @@ function(build_dependency dep_name install_dir extra_flags)
             ${PROJECT_SOURCE_DIR}/cmake-modules/external_${dep_name}
             WORKING_DIRECTORY ${build_dir}
             RESULT_VARIABLE config_result
-            ERROR_VARIABLE  config_error
+            #ERROR_VARIABLE  config_error
     )
-    if(${config_result})
+    if(config_result)
         message(STATUS "Got non-zero exit code while configuring ${dep_name}")
         message(STATUS  "build_dir         : ${build_dir}")
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "config_result     : ${config_result}")
-        message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(APPEND ${build_dir}/stdout ${config_result})
-        file(APPEND ${build_dir}/stderr ${config_error})
-        if(DMRG_PRINT_INFO)
-            message(STATUS "Contents of stdout: \n  ${config_result} \n")
-            message(STATUS "Contents of stderr: \n  ${config_error}  \n")
-        endif()
+        #message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
+        #file(APPEND ${build_dir}/stdout ${config_result})
+        #file(APPEND ${build_dir}/stderr ${config_error})
+        #if(DMRG_PRINT_INFO OR CMAKE_VERBOSE_MAKEFILE OR DMRG_PRINT_CHECKS)
+        #    message(STATUS "Contents of stdout: \n  ${config_result} \n")
+        #    message(STATUS "Contents of stderr: \n  ${config_error}  \n")
+        #endif()
+        message(FATAL_ERROR "Failed to configure ${dep_name}")
     endif()
 
 
-    include(cmake-modules/GetNumThreads.cmake)
-    get_num_threads(num_threads)
+
     set(ENV{CMAKE_BUILD_PARALLEL_LEVEL} ${num_threads})
     execute_process(COMMAND  ${CMAKE_COMMAND} --build . --parallel ${num_threads}
             WORKING_DIRECTORY "${build_dir}"
             RESULT_VARIABLE build_result
-            ERROR_VARIABLE  build_error
+            #ERROR_VARIABLE  build_error
             )
 
-    if(${build_result})
+    if(build_result)
         message(STATUS "Got non-zero exit code while building ${dep_name}")
         message(STATUS  "build_dir         : ${build_dir}")
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "build_result      : ${build_result}")
-        message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(APPEND ${build_dir}/stdout ${build_result})
-        file(APPEND ${build_dir}/stderr ${build_error})
-        if(DMRG_PRINT_INFO)
-            message(STATUS "Contents of stdout: \n  ${build_result} \n")
-            message(STATUS "Contents of stderr: \n  ${build_error} \n")
-        endif()
+#        message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
+#        file(APPEND ${build_dir}/stdout ${build_result})
+#        file(APPEND ${build_dir}/stderr ${build_error})
+#        if(DMRG_PRINT_INFO OR CMAKE_VERBOSE_MAKEFILE OR DMRG_PRINT_CHECKS)
+#            message(STATUS "Contents of stdout: \n  ${build_result} \n")
+#            message(STATUS "Contents of stderr: \n  ${build_error} \n")
+#        endif()
+        message(FATAL_ERROR "Failed to build ${dep_name}")
     endif()
 
     # Copy the install manifest if it exists
