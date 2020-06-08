efa0e1436f262f06ef29b68e58c56b15d456f245
DavidAce <aceituno@kth.se>
2020-06-08 15:55:11 +0200

    Rewriting tests with catch2
    Former-commit-id: 41051645839dc1829851e2bf5d6e64e09017b7e9

--- tests/svd.cpp
+++ tests/svd.cpp
@@ -1,29 +1,27 @@
+#define CATCH_CONFIG_MAIN
 
-
-
+#include "catch.hpp"
 #include <Eigen/Core>
 #include <h5pp/h5pp.h>
-#include <io/nmspc_filesystem.h>
-#include <math/class_svd_wrapper.h>
+#include <math/svd.h>
 
-int main(){
-    using reciter = fs::recursive_directory_iterator;
-    for (auto & item : reciter(std::string(TEST_MATRIX_DIR))){
-        if(item.path().filename().string().find("svdmatrix") == std::string::npos) continue;
-        if(item.path().extension() != ".h5") continue;
-        std::cout <<  "item: " << item << std::endl;
-        int logLevel = 1;
-        h5pp::File file(item.path().string(), h5pp::FilePermission::READONLY, logLevel);
-        Eigen::MatrixXcd matrix;
-        file.readDataset(matrix,"svdmatrix");
-        class_SVD SVD;
-        auto [U1,S1,V1] = SVD.decompose(matrix);
-        SVD.use_lapacke = true;
-        auto [U2,S2,V2] = SVD.decompose(matrix);
-
-        std::cout << "Abs diff S2-S1: " << (S2.array()-S1.array()).cwiseAbs().sum() << std::endl;
+TEST_CASE("Singular value decomposition in Eigen and Lapacke", "[svd]") {
+    SECTION("Test all svd decompositions") {
+        using reciter = h5pp::fs::recursive_directory_iterator;
+        svd::solver svd;
+        for(auto &item : reciter(std::string(TEST_MATRIX_DIR))) {
+            if(item.path().filename().string().find("svdmatrix") == std::string::npos) continue;
+            if(item.path().extension() != ".h5") continue;
+            size_t     logLevel = 2;
+            h5pp::File file(item.path().string(), h5pp::FilePermission::READONLY, logLevel);
+            auto       matrix = file.readDataset<Eigen::MatrixXcd>("svdmatrix");
+            svd.use_lapacke   = false;
+            auto [U1, S1, V1] = svd.decompose(matrix);
+            svd.use_lapacke   = true;
+            auto [U2, S2, V2] = svd.decompose(matrix);
+            double difference = std::log10((S2.array() - S1.array()).cwiseAbs().sum());
+            fmt::print("{:<32} diff {:.24f}\n", item.path().filename().string(), difference);
+            REQUIRE(difference < -12);
+        }
     }
-
-    return 0;
-
 }
