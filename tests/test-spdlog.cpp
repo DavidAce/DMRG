2fa9eed8d471c5f71ad4926eab60b6afa2deb2c5
DavidAce <aceituno@kth.se>
2020-06-23 18:55:45 +0200

    Updated h5pp

--- tests/test-spdlog.cpp
+++ tests/test-spdlog.cpp
@@ -1,6 +1,9 @@
 #include <h5pp/details/h5ppFormat.h>
 
+#include <h5pp/details/h5ppLogger.h>
+
 int main (){
     fmt::print("Hello world");
-
+    std::runtime_error test ("");
+    std::runtime_error::operator=(test);
 }
