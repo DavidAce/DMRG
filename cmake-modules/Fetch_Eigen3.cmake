6fb21b79dd46c5b1e56cdfa3b0a1c3b24bd6fc49
DavidAce <aceituno@kth.se>
2020-06-30 11:00:51 +0200

    Fixed application of compile definition EIGEN_MAX_ALIGN_BYTES=16

--- cmake-modules/Fetch_Eigen3.cmake
+++ cmake-modules/Fetch_Eigen3.cmake
@@ -30,19 +30,3 @@ if(NOT TARGET Eigen3::Eigen AND DMRG_DOWNLOAD_METHOD MATCHES "find|fetch")
 endif()
 
 
-if(TARGET Eigen3::Eigen)
-
-    # AVX aligns 32 bytes (AVX512 aligns 64 bytes).
-    # When running on Tetralith, with march=native, there can be alignment mismatch
-    # in ceres which results in a segfault on free memory.
-    # Something like "double free or corruption ..."
-    #   * EIGEN_MAX_ALIGN_BYTES=16 works on Tetralith
-    cmake_host_system_information(RESULT _host_name  QUERY HOSTNAME)
-    if(_host_name MATCHES "tetralith|triolith")
-        message(STATUS "Applying special Eigen compile definitions for Tetralith: EIGEN_MAX_ALIGN_BYTES=16")
-        #target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_MALLOC_ALREADY_ALIGNED=0) # May work to fix CERES segfaults!!!
-        target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_MAX_ALIGN_BYTES=16)
-    endif()
-
-endif()
-
