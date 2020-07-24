cbed3ab0ef35d87f99c6ad92c77f92bceb9d30d2
DavidAce <aceituno@kth.se>
2020-07-24 19:30:36 +0200

    Find openblas properly
    Former-commit-id: 1df0a649b42bc4c8bc3a69ee6c08088b8ddc8338

--- cmake-modules/Fetch_OpenBLAS.cmake
+++ cmake-modules/Fetch_OpenBLAS.cmake
@@ -5,7 +5,7 @@ if(TARGET mkl::mkl)
 endif()
 
 
-if(NOT TARGET openblas::openblas AND DMRG_DOWNLOAD_METHOD MATCHES "find|fetch")
+if(NOT TARGET openblas::openblas AND DMRG_DOWNLOAD_METHOD MATCHES "find")
     set(OpenBLAS_FIND_VERBOSE ON)
     find_package(OpenBLAS)
 endif()
@@ -19,14 +19,6 @@ if(NOT TARGET openblas::openblas AND DMRG_DOWNLOAD_METHOD MATCHES "fetch")
         set(OPENBLAS_ENABLE_OPENMP 0) # OpenMP doesnt work on static builds with OpenBLAS
     endif()
     include(cmake-modules/getExpandedTarget.cmake)
-    #expand_target_libs(gfortran::gfortran GFORTRAN_LIBS)
-    #list(FILTER GFORTRAN_LIBS INCLUDE REGEX "gfortran|quadmath")
-    #list(JOIN GFORTRAN_LIBS " " GFORTRAN_LIBS_SPACE_SEP_STRING)
-    #list(GET  GFORTRAN_LIBS 0 GFORTRAN_LIB)
-    #get_filename_component(GFORTRAN_PATH ${GFORTRAN_LIB} DIRECTORY)
-
-    #set(LDFLAGS "-L${GFORTRAN_PATH} ${GFORTRAN_LIBS_SPACE_SEP_STRING}")
-    #set(FFLAGS  "-O3 -Wno-maybe-uninitialized -Wno-conversion -Wno-unused-but-set-variable -Wno-unused-variable")
     list(APPEND OPENBLAS_CMAKE_OPTIONS  -DTARGET:STRING=${OPENBLAS_MARCH})
     list(APPEND OPENBLAS_CMAKE_OPTIONS  -DUSE_THREAD:BOOL=${OPENBLAS_MULTITHREADED})
     list(APPEND OPENBLAS_CMAKE_OPTIONS  -DBUILD_RELAPACK:BOOL=OFF)
