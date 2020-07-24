d95c753714ff5b306ed0488a0932d3e28139a125
DavidAce <aceituno@kth.se>
2020-07-24 19:59:51 +0200

    Remove extra declarations
    Former-commit-id: 1eaa94011dfe472eb4a67f439accea1d8b243bbc

--- source/tools/common/moments.h
+++ source/tools/common/moments.h
@@ -15,14 +15,14 @@ namespace tools::common::moments{
     using T4 = Eigen::Tensor<Scalar, 4>;
     template<typename T>
     using TM = Eigen::TensorMap<T>;
-    extern template double tools::common::moments::first(const T3<real> &, const T4<real> &, const T3<real> &, const T3<real> &);
+//    extern template double tools::common::moments::first(const T3<real> &, const T4<real> &, const T3<real> &, const T3<real> &);
     extern template double tools::common::moments::first(const T3<cplx> &, const T4<cplx> &, const T3<cplx> &, const T3<cplx> &);
-    extern template double tools::common::moments::first(const TM<T3<real>> &, const T4<real> &, const T3<real> &, const T3<real> &);
-    extern template double tools::common::moments::first(const TM<T3<cplx>> &, const T4<cplx> &, const T3<cplx> &, const T3<cplx> &);
-    extern template double tools::common::moments::second(const T3<real> &, const T4<real> &, const T4<real> &, const T4<real> &);
+//    extern template double tools::common::moments::first(const TM<T3<real>> &, const T4<real> &, const T3<real> &, const T3<real> &);
+//    extern template double tools::common::moments::first(const TM<T3<cplx>> &, const T4<cplx> &, const T3<cplx> &, const T3<cplx> &);
+//    extern template double tools::common::moments::second(const T3<real> &, const T4<real> &, const T4<real> &, const T4<real> &);
     extern template double tools::common::moments::second(const T3<cplx> &, const T4<cplx> &, const T4<cplx> &, const T4<cplx> &);
-    extern template double tools::common::moments::second(const TM<T3<real>> &, const T4<real> &, const T4<real> &, const T4<real> &);
-    extern template double tools::common::moments::second(const TM<T3<cplx>> &, const T4<cplx> &, const T4<cplx> &, const T4<cplx> &);
+//    extern template double tools::common::moments::second(const TM<T3<real>> &, const T4<real> &, const T4<real> &, const T4<real> &);
+//    extern template double tools::common::moments::second(const TM<T3<cplx>> &, const T4<cplx> &, const T4<cplx> &, const T4<cplx> &);
 
 
 }
