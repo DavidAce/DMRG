56fcb53906acdc54011dff4fbd5db72d162697f7
DavidAce <aceituno@kth.se>
2020-07-24 18:46:11 +0200

    Fixing include complex
    Former-commit-id: e6bb73d3744b679fb444bd0ba6a5e1c5979bfee8

--- cmake-modules/CheckLapackeCompiles.cmake
+++ cmake-modules/CheckLapackeCompiles.cmake
@@ -20,8 +20,16 @@ function(check_lapacke_compiles TARGETS LIBS INCS OPTS DEFS)
     include(CheckCXXSourceCompiles)
     check_cxx_source_compiles("
         #include <complex>
-        #ifdef MKL_AVAILABLE
+        #ifndef lapack_complex_float
+            #define lapack_complex_float  std::complex<float>
+        #endif
+        #ifndef lapack_complex_double
+            #define lapack_complex_double std::complex<double>
+        #endif
+        #if __has_include(<mkl_lapacke.h>)
         #include <mkl_lapacke.h>
+        #elif __has_include(<openblas/lapacke.h>)
+        #include <openblas/lapacke.h>
         #else
         #include <lapacke.h>
         #endif
