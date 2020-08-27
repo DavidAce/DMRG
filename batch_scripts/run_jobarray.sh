b1ae2347d2bfcfac5a04d443569ee7512fe252fa
DavidAce <aceituno@kth.se>
2020-08-27 18:23:08 +0200

    Preparing batch run 110
    * Testing job arrays

--- batch_scripts/run_jobarray.sh
+++ batch_scripts/run_jobarray.sh
@@ -41,27 +41,29 @@ fi
 mkdir -p logs
 num_cols=$(awk '{print NF}' $simfile | head -n 1)
 arg_line=$(tail -n+$SLURM_ARRAY_TASK_ID $simfile | head -1)
+config_file=$(echo $arg_line | cut -d " " -f1)
+config_base=$(basename $config_file .cfg)
+model_seed=$(echo $arg_line | cut -d " " -f2)
 
-
-echo "Running job $SLURM_ARRAY_JOB_ID, iter $SLURM_ARRAY_TASK_ID at $HOSTNAME with inputfile $simfile"
-echo "CPUS ON  NODE: $SLURM_CPUS_ON_NODE"
-echo "CPUS PER NODE: $SLURM_JOB_CPUS_PER_NODE"
-echo "CPUS PER TASK: $SLURM_CPUS_PER_TASK"
-echo "MEM PER CPU  : $SLURM_MEM_PER_CPU"
-echo "MEM PER NODE : $SLURM_MEM_PER_NODE"
-
+echo "HOSTNAME          : $HOSTNAME"
+echo "CPUS ON  NODE     : $SLURM_CPUS_ON_NODE"
+echo "CPUS PER NODE     : $SLURM_JOB_CPUS_PER_NODE"
+echo "CPUS PER TASK     : $SLURM_CPUS_PER_TASK"
+echo "MEM PER CPU       : $SLURM_MEM_PER_CPU"
+echo "MEM PER NODE      : $SLURM_MEM_PER_NODE"
+echo "ARRAY JOB ID      : $SLURM_ARRAY_JOB_ID"
+echo "ARRAY TASK ID     : $SLURM_ARRAY_TASK_ID"
+echo "INPUT FILE        : $simfile"
+echo "CONFIG FILE       : $config_file"
+echo "SEED              : $model_seed"
 
 if [ "$num_cols" -eq 2 ]; then
-    config=$(echo $arg_line | cut -d " " -f1)
-    model_seed=$(echo $arg_line | cut -d " " -f2)
-    echo "Executing    : $exec -c $config -s $model_seed"
-    $exec -c $config -s $model_seed
+    echo "EXEC LINE         : $exec -c $config_file -s $model_seed &> logs/$config_base/$model_seed.out"
+    $exec -c $config_file -s $model_seed &> logs/$config_base/$model_seed.out
 elif [ "$num_cols" -eq 3 ]; then
-    config=$(echo $arg_line | cut -d " " -f1)
-    model_seed=$(echo $arg_line | cut -d " " -f2)
+    echo "EXEC LINE         : $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out"
     bit_field=$(echo $arg_line | cut -d " " -f3)
-    echo "Executing    : $exec -c $config -s $model_seed -b $bit_field"
-    $exec -c $config -s $model_seed -b $bit_field
+    $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out
 else
     echo "Case not implemented"
     exit 1
