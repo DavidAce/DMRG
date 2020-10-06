e77678de2aae3f4209d885da51b793c5cb7b479d
DavidAce <aceituno@kth.se>
2020-10-06 22:40:42 +0200

    Preparing batch run for l=0.01 larger sizes
    Former-commit-id: 40a4c154f20ecc43a5f47dad0501ecb21bcb460e

--- batch_scripts/generate_input_sweep.py
+++ batch_scripts/generate_input_sweep.py
@@ -11,7 +11,7 @@ location    = "input"
 os.makedirs(location, exist_ok=True)
 
 
-sites        = np.array([16,20,24]) #np.linspace(16,36,6, dtype=int)
+sites        = np.array([28,32,36]) #np.linspace(16,36,6, dtype=int)
 # sites        = np.linspace(16,36,6, dtype=int)
 lambdas      = [0.01] # np.linspace(0,0.2,3)
 deltas       = [0] # np.linspace(-1.0,1.0,5)
@@ -22,19 +22,24 @@ num_total = 0
 settings = []
 input_filenames = []
 print("Generating", len(sites) * len(lambdas) * len(J_mean) * len(h_mean) , "input files")
-for num_L in sites:
-    for num_l in range(len(lambdas)):
-        for num_j in range(len(J_mean)):
-            for num_h in range(len(h_mean)):
+for num_L,val_L in enumerate(sites):
+    for num_l,val_l in enumerate(lambdas):
+        for num_j,val_j in enumerate(J_mean):
+            for num_h,val_h in enumerate(h_mean):
                 os.makedirs(location, exist_ok=True)
-                input_filename = location + '/' + basename + '_L'+ str(num_L) + '_l' + str(num_l) + '_J'+ str(num_j) + '_h'+ str(num_h) + '.cfg'
+                str_L = str(val_L)
+                str_l = "{:.3f}".format(val_l)
+                str_j = "{:.3f}".format(val_j)
+                str_h = "{:.3f}".format(val_h)
+
+                input_filename = location + '/' + basename + '_L'+ str_L + '_l' + str_l + '_J'+ str_j + '_h'+ str_h + '.cfg'
                 settings = {
-                    "output::output_filepath"            : 'output/L_'+ str(num_L) + '/l_'+str(num_l) + '/J_' +str(num_j) + '/h_'+ str(num_h)+ '/' + basename + '.h5',
+                    "output::output_filepath"            : 'output/L_'+ str_L + '/l_'+ str_l + '/J_' + str_j + '/h_'+ str_h + '/' + basename + '.h5',
                     "threading::num_threads"             : "1",
-                    "model::model_size"                  : str(num_L),
-                    "model::ising_sdual::J_mean"         : "{:.2f}".format(J_mean[num_j]),
-                    "model::ising_sdual::h_mean"         : "{:.2f}".format(h_mean[num_h]),
-                    "model::ising_sdual::lambda"         : "{:.2f}".format(lambdas[num_l]),
+                    "model::model_size"                  : str_L,
+                    "model::ising_sdual::J_mean"         : str_j,
+                    "model::ising_sdual::h_mean"         : str_h,
+                    "model::ising_sdual::lambda"         : str_l,
                     "model::ising_sdual::J_stdv"         : "1.0",
                     "model::ising_sdual::h_stdv"         : "1.0",
                     "xdmrg::chi_lim_max"                 : "512",
@@ -43,7 +48,6 @@ for num_L in sites:
                 num_total = num_total + 1
                 generate_input_file(settings, input_filename, template_filename)
 
-# generate_input_files(settings, input_filenames, template_filename,location)
 
 
 
