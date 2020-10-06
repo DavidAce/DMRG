6eb5009ab149b682d56bf63cb48f980b6c7071bb
DavidAce <aceituno@kth.se>
2020-10-06 22:10:28 +0200

    Added support for measuring lap times
    Former-commit-id: 9c09b95fbb6fda470c1ad09ecf865687d1890976

--- source/general/class_tic_toc.h
+++ source/general/class_tic_toc.h
@@ -12,9 +12,11 @@ class class_tic_toc {
     using hresclock = std::chrono::high_resolution_clock;
     hresclock::time_point tic_timepoint;
     hresclock::time_point toc_timepoint;
+    hresclock::time_point lap_timepoint;
     hresclock::time_point start_timepoint;
     hresclock::duration   delta_time    = std::chrono::high_resolution_clock::duration::zero();
     hresclock::duration   measured_time = std::chrono::high_resolution_clock::duration::zero();
+    hresclock::duration   lap_time;
 
     std::string name            = "";
     bool        enable          = true; // Whether we are profiling or not.
@@ -30,7 +32,8 @@ class class_tic_toc {
 
     void                      set_properties(bool on_off, int prec, std::string output_text);
     void                      set_label(std::string output_text);
-    void                      set_time(double measured_time);
+    void                      set_measured_time(double measured_time);
+    void                      start_lap();
     void                      print_age() const;
     void                      print_measured_time() const;
     void                      print_last_time_interval() const;
@@ -38,11 +41,14 @@ class class_tic_toc {
     [[nodiscard]] std::string get_name() const;
     [[nodiscard]] double      get_age() const;
     [[nodiscard]] double      get_measured_time() const;
-    [[nodiscard]] double      get_last_time_interval() const;
+    [[nodiscard]] double      get_last_interval() const;
+    [[nodiscard]] double      restart_lap();
+    [[nodiscard]] double      get_lap() const;
+
     [[nodiscard]] std::string string(double tgt = std::numeric_limits<double>::quiet_NaN(), double cmp = std::numeric_limits<double>::quiet_NaN()) const;
     [[nodiscard]] std::string string_age() const;
     [[nodiscard]] std::string string_measured_time() const;
-    [[nodiscard]] std::string string_last_time_interval() const;
+    [[nodiscard]] std::string string_last_interval() const;
     [[nodiscard]] std::string string_measured_time_w_percent(double cmp = std::numeric_limits<double>::quiet_NaN()) const;
 
     void reset();
@@ -50,7 +56,7 @@ class class_tic_toc {
     class_tic_toc &      operator=(double other_time_in_seconds);
     class_tic_toc &      operator+=(double other_time_in_seconds);
     class_tic_toc &      operator-=(double other_time_in_seconds);
-    class_tic_toc &      operator+=(const class_tic_toc & rhs);
-    class_tic_toc &      operator-=(const class_tic_toc & rhs);
+    class_tic_toc &      operator+=(const class_tic_toc &rhs);
+    class_tic_toc &      operator-=(const class_tic_toc &rhs);
     friend std::ostream &operator<<(std::ostream &, const class_tic_toc &);
 };
