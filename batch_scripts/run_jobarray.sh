f3f85ebda55fc6d46ec8c13dae1184a42e2859b3
DavidAce <aceituno@kth.se>
2020-10-20 11:40:47 +0200

    Preparing batch run to test if random entangled initial state works now
    Former-commit-id: 9361496e7b991d733e22068f76df59bf397d9f79

--- batch_scripts/run_jobarray.sh
+++ batch_scripts/run_jobarray.sh
@@ -3,8 +3,6 @@
 #SBATCH --kill-on-invalid-dep=yes
 #SBATCH --output=logs/%x-%A_%a.out
 #SBATCH --error=logs/%x-%A_%a.err
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
 #!#SBATCH --ntasks-per-core=1
 
 
@@ -14,18 +12,20 @@ usage() {
 
 Usage                               : $PROGNAME [-options] with the following options:
 -h                                  : Help. Shows this text.
+-d                                  : Dry run
 -e <executable>                     : Path to executable (default = "")
--f <simfile>                        : Path to simulation file, two columns formatted as [configfile seed] (default = "")
+-f <jobfile>                        : Path to simulation file, two columns formatted as [configfile seed] (default = "")
 -o <output logfile>                 : Path to GNU parallel logfile (default = "")
 EOF
   exit 1
 }
 
-while getopts he:f:o: o; do
+while getopts hde:f:o: o; do
     case $o in
         (h) usage ;;
+        (d) dryrun="ON";;
         (e) exec=$OPTARG;;
-        (f) simfile=$OPTARG;;
+        (f) jobfile=$OPTARG;;
         (o) outfile=$OPTARG;;
         (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
         (*) usage ;;
@@ -33,13 +33,13 @@ while getopts he:f:o: o; do
 done
 
 
-if [ ! -f $simfile ]; then
-    echo "Simfile is not a valid file: $simfile"
+if [ ! -f $jobfile ]; then
+    echo "job file is not a valid file: $jobfile"
     exit 1
 fi
 
-num_cols=$(awk '{print NF}' $simfile | head -n 1)
-arg_line=$(tail -n+$SLURM_ARRAY_TASK_ID $simfile | head -1)
+num_cols=$(awk '{print NF}' $jobfile | head -n 1)
+arg_line=$(tail -n+$SLURM_ARRAY_TASK_ID $jobfile | head -1)
 config_file=$(echo $arg_line | cut -d " " -f1)
 config_base=$(basename $config_file .cfg)
 model_seed=$(echo $arg_line | cut -d " " -f2)
@@ -54,17 +54,22 @@ echo "MEM PER CPU       : $SLURM_MEM_PER_CPU"
 echo "MEM PER NODE      : $SLURM_MEM_PER_NODE"
 echo "ARRAY JOB ID      : $SLURM_ARRAY_JOB_ID"
 echo "ARRAY TASK ID     : $SLURM_ARRAY_TASK_ID"
-echo "INPUT FILE        : $simfile"
+echo "JOB FILE          : $jobfile"
 echo "CONFIG FILE       : $config_file"
 echo "SEED              : $model_seed"
 
+
 if [ "$num_cols" -eq 2 ]; then
     echo "EXEC LINE         : $exec -c $config_file -s $model_seed &> logs/$config_base/$model_seed.out"
-    $exec -c $config_file -s $model_seed &> logs/$config_base/$model_seed.out
+    if [ -z  "$dryrun" ];then
+      $exec -c $config_file -s $model_seed &> logs/$config_base/$model_seed.out
+    fi
 elif [ "$num_cols" -eq 3 ]; then
-    echo "EXEC LINE         : $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out"
     bit_field=$(echo $arg_line | cut -d " " -f3)
-    $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out
+    echo "EXEC LINE         : $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out"
+    if [ -z  "$dryrun" ];then
+      $exec -c $config_file -s $model_seed -b $bit_field &> logs/$config_base/$model_seed_$bit_field.out
+    fi
 else
     echo "Case not implemented"
     exit 1
