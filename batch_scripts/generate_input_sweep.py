b5069754f4206ae44c876546e2c8518ca98f9f3e
DavidAce <aceituno@kth.se>
2020-10-20 13:47:09 +0200

    Started exploring delta and using delta in path instead of J and h

--- batch_scripts/generate_input_sweep.py
+++ batch_scripts/generate_input_sweep.py
@@ -1,6 +1,6 @@
 from generate_inputs.src.generate_inputs import *
 import numpy as np
-
+from fractions import Fraction
 
 # Using the input_template.cfg, make num_copies new files enumerated as 0....num_copies,
 # while replacing the fields stated in find_replace.
@@ -11,42 +11,54 @@ location    = "input"
 os.makedirs(location, exist_ok=True)
 
 
-sites        = np.array([16,24]) #np.linspace(16,36,6, dtype=int)
-# sites        = np.linspace(16,36,6, dtype=int)
-lambdas      = [0,0.01] # np.linspace(0,0.2,3)
-deltas       = [0] # np.linspace(-1.0,1.0,5)
-J_mean       = np.array([1])
-h_mean       = J_mean - deltas
-# h_log_mean   = np.flipud(np.arange(-1, 1.5, 0.5)+1)
+sites        = np.array([16,24])
+lambdas      = [0.005,0.010, 0.015,0.020]
+deltas       = [-0.1, -0.05, 0.0, 0.05, 0.1]
+
 num_total = 0
 settings = []
 input_filenames = []
-print("Generating", len(sites) * len(lambdas) * len(J_mean) * len(h_mean) , "input files")
+
+def delta(J,h):
+    return np.log(J) - np.log(h)
+
+def undelta(delta):
+    exp_delta = np.exp(delta)
+    fr = Fraction(exp_delta).limit_denominator()
+    maxJh = np.max([fr.numerator, fr.denominator])
+    J = round(fr.numerator/maxJh,6)
+    h = round(fr.denominator/maxJh,6)
+    return J, h
+
+
+print("Generating", len(sites) * len(lambdas) * len(deltas), "input files")
 for num_L,val_L in enumerate(sites):
     for num_l,val_l in enumerate(lambdas):
-        for num_j,val_j in enumerate(J_mean):
-            for num_h,val_h in enumerate(h_mean):
-                os.makedirs(location, exist_ok=True)
-                str_L = str(val_L)
-                str_l = "{:.4f}".format(val_l)
-                str_j = "{:.4f}".format(val_j)
-                str_h = "{:.4f}".format(val_h)
+        for num_d,val_d in enumerate(deltas):
+            val_j,val_h = undelta(val_d)
+            os.makedirs(location, exist_ok=True)
+            str_L = str(val_L)
+            str_d = "{:+.4f}".format(val_d)
+            str_l = "{:.4f}".format(val_l)
+            str_j = "{:.6f}".format(val_j)
+            str_h = "{:.6f}".format(val_h)
 
-                input_filename = location + '/' + basename + '_L'+ str_L + '_l' + str_l + '_J'+ str_j + '_h'+ str_h + '.cfg'
-                settings = {
-                    "output::output_filepath"            : 'output/L_'+ str_L + '/l_'+ str_l + '/J_' + str_j + '/h_'+ str_h + '/' + basename + '.h5',
-                    "threading::num_threads"             : "2",
-                    "model::model_size"                  : str_L,
-                    "model::ising_sdual::J_mean"         : str_j,
-                    "model::ising_sdual::h_mean"         : str_h,
-                    "model::ising_sdual::lambda"         : str_l,
-                    "model::ising_sdual::J_stdv"         : "1.0",
-                    "model::ising_sdual::h_stdv"         : "1.0",
-                    "xdmrg::chi_lim_max"                 : "512",
-                    "xdmrg::max_states "                 : "2",
-                }
-                num_total = num_total + 1
-                generate_input_file(settings, input_filename, template_filename)
+            input_filename = location + '/' + basename + '_L'+ str_L + '_l' + str_l + '_d'+ str_d + '.cfg'
+            settings = {
+                "output::output_filepath"            : 'output/L_'+ str_L + '/l_'+ str_l + '/d_' + str_d + '/' + basename + '.h5',
+                "threading::num_threads"             : "2",
+                "model::model_size"                  : str_L,
+                "model::ising_sdual::lambda"         : str_l,
+                "model::ising_sdual::J_mean"         : str_j,
+                "model::ising_sdual::h_mean"         : str_h,
+                "model::ising_sdual::J_stdv"         : "1.0",
+                "model::ising_sdual::h_stdv"         : "1.0",
+                "xdmrg::chi_lim_max"                 : "512",
+                "xdmrg::max_states "                 : "2",
+            }
+            num_total = num_total + 1
+            print(input_filename, "L:", str_L,"l:",str_l, "d:", "{:+6.4f}".format(val_d),"j/h:", undelta(val_d))
+            generate_input_file(settings, input_filename, template_filename)
 
 
 
