48dbb033fc7c79e39f31b5b6f5f48c9e8a7ba2e4
DavidAce <aceituno@kth.se>
2020-11-04 13:39:01 +0100

    Start using delta instead of J_mean and h_mean to reduce input parameters

--- batch_scripts/generate_input_sweep.py
+++ batch_scripts/generate_input_sweep.py
@@ -11,9 +11,14 @@ location    = "input"
 os.makedirs(location, exist_ok=True)
 
 
-sites        = np.array([16,20,24])
-lambdas      = [0.000,0.005,0.010, 0.015,0.020]
-deltas       = [-0.1, -0.05, 0.0, 0.05, 0.1]
+sites        = np.array([16,24])
+lambdas      = [0.000]
+deltas       = [0.000]
+
+
+# sites        = np.array([16,20,24])
+# lambdas      = [0.000,0.005,0.010, 0.015,0.020]
+# deltas       = [-0.1, -0.05, 0.0, 0.05, 0.1]
 
 num_total = 0
 settings = []
@@ -23,13 +28,10 @@ def delta(J,h):
     return np.log(J) - np.log(h)
 
 def undelta(delta):
-    exp_delta = np.exp(delta)
-    fr = Fraction(exp_delta).limit_denominator()
-    maxJh = np.max([fr.numerator, fr.denominator])
-    J = round(fr.numerator/maxJh,6)
-    h = round(fr.denominator/maxJh,6)
-    return J, h
-
+    if(delta > 0):
+        return 1.0, np.exp(-delta)
+    else:
+        return np.exp(delta),1.0
 
 print("Generating", len(sites) * len(lambdas) * len(deltas), "input files")
 for num_L,val_L in enumerate(sites):
@@ -46,18 +48,17 @@ for num_L,val_L in enumerate(sites):
             input_filename = location + '/' + basename + '_L'+ str_L + '_l' + str_l + '_d'+ str_d + '.cfg'
             settings = {
                 "output::output_filepath"            : 'output/L_'+ str_L + '/l_'+ str_l + '/d_' + str_d + '/' + basename + '.h5',
-                "threading::num_threads"             : "2",
+                "threading::num_threads"             : "1",
                 "model::model_size"                  : str_L,
+                "model::ising_sdual::delta"          : str_d,
                 "model::ising_sdual::lambda"         : str_l,
-                "model::ising_sdual::J_mean"         : str_j,
-                "model::ising_sdual::h_mean"         : str_h,
                 "model::ising_sdual::J_stdv"         : "1.0",
                 "model::ising_sdual::h_stdv"         : "1.0",
                 "xdmrg::chi_lim_max"                 : "512",
-                "xdmrg::max_states "                 : "2",
+                "xdmrg::max_states "                 : "4",
             }
             num_total = num_total + 1
-            print(input_filename, "L:", str_L,"l:",str_l, "d:", "{:+6.4f}".format(val_d),"j/h:", undelta(val_d))
+            print(input_filename, "L:", str_L,"l:",str_l, "d:", str_d,"j:", str_j, "h:",str_h)
             generate_input_file(settings, input_filename, template_filename)
 
 
