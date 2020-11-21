01442930c18a1a7fc1987c5c1ea5935419d66a5e
DavidAce <aceituno@kth.se>
2020-11-21 10:19:20 +0100

    Remove use of max_saturation_iters

--- source/algorithms/class_algorithm_finite.h
+++ source/algorithms/class_algorithm_finite.h
@@ -29,7 +29,7 @@ class class_algorithm_finite : public class_algorithm_base {
     // Control behavior when stuck
     size_t              max_stuck_iters      = 4;     /*!< If stuck for this many sweeps -> stop. */
     size_t              min_saturation_iters = 1;     /*!< If both var and ent saturated  this long -> got_stuck: true */
-    size_t              max_saturation_iters = 10;    /*!< If either var or ent saturated this long -> got_stuck: true */
+    size_t              max_saturation_iters = 10;    /*!< If either var or ent saturated this long -> got_stuck: true Requires var and ent stuck for > 0 */
 
     bool                has_projected        = false; /*!< True if projection has already been tried */
     bool                has_damped           = false; /*!< True if damping of hamiltonian parameters is ongoing */
