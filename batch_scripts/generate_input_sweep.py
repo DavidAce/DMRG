b02fabb4964ddac6c62d300844ebf2896baebbf7
DavidAce <aceituno@kth.se>
2020-12-02 16:11:56 +0100

    Preparing batch run
    Former-commit-id: 7e29aa8d2bbae781d680bf5a61e0cc9cc4dfadb7

--- batch_scripts/generate_input_sweep.py
+++ batch_scripts/generate_input_sweep.py
@@ -13,8 +13,8 @@ location    = "input"
 
 sites               = np.array([16,24])
 lambdas             = [0.000]
-deltas              = [0.000]
-initial_state       = ["RANDOM_ENTANGLED_STATE"]
+deltas              = [-0.10, -0.05,0.00, 0.05, 0.10]
+initial_state       = ["RANDOM_PRODUCT_STATE"]
 multisite_max_sites = [6]
 output_prefix       = "output"
 
@@ -60,7 +60,7 @@ for val_L,val_l, val_d, init, multi in  product(sites,lambdas,deltas,initial_sta
     input_filename = location + extra_prefix + '/' + basename + '_L'+ str_L + '_l' + str_l + '_d'+ str_d + '.cfg'
     settings = {
         "output::output_filepath"            : output_prefix + extra_prefix + '/L_'+ str_L + '/l_'+ str_l + '/d_' + str_d + '/' + basename + '.h5',
-        "threading::num_threads"             : "1",
+        "threading::num_threads"             : "2",
         "console::verbosity"                 : "2",
         "model::model_size"                  : str_L,
         "model::ising_sdual::delta"          : str_d,
@@ -76,33 +76,3 @@ for val_L,val_l, val_d, init, multi in  product(sites,lambdas,deltas,initial_sta
     num_total = num_total + 1
     print(input_filename, "L:", str_L,"l:",str_l, "d:", str_d,"j:", str_j, "h:",str_h)
     generate_input_file(settings, input_filename, template_filename)
-#
-# for num_L,val_L in enumerate(sites):
-#     for num_l,val_l in enumerate(lambdas):
-#         for num_d,val_d in enumerate(deltas):
-#             val_j,val_h = undelta(val_d)
-#             os.makedirs(location, exist_ok=True)
-#             str_L = str(val_L)
-#             str_d = "{:+.4f}".format(val_d)
-#             str_l = "{:.4f}".format(val_l)
-#             str_j = "{:.6f}".format(val_j)
-#             str_h = "{:.6f}".format(val_h)
-#
-#             input_filename = location + '/' + basename + '_L'+ str_L + '_l' + str_l + '_d'+ str_d + '.cfg'
-#             settings = {
-#                 "output::output_filepath"            : output_prefix + '/L_'+ str_L + '/l_'+ str_l + '/d_' + str_d + '/' + basename + '.h5',
-#                 "threading::num_threads"             : "1",
-#                 "console::verbosity"                 : "2",
-#                 "model::model_size"                  : str_L,
-#                 "model::ising_sdual::delta"          : str_d,
-#                 "model::ising_sdual::lambda"         : str_l,
-#                 "model::ising_sdual::J_stdv"         : "1.0",
-#                 "model::ising_sdual::h_stdv"         : "1.0",
-#                 "xdmrg::chi_lim_max"                 : "512",
-#                 "xdmrg::max_states"                  : "8",
-#                 "xdmrg::overlap_iters"               : "2",
-#             }
-#             num_total = num_total + 1
-#             print(input_filename, "L:", str_L,"l:",str_l, "d:", str_d,"j:", str_j, "h:",str_h)
-#             generate_input_file(settings, input_filename, template_filename)
-#
