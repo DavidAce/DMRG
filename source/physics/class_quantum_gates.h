5b4213b8f5a62ed41e84499e4dc847d78d22c397
DavidAce <aceituno@kth.se>
2021-01-05 16:02:32 +0100

    Improved exponential for flbit
    * Handle larger time steps by exponentiating diagonal elements independently with mod-2pi trick

--- source/physics/class_quantum_gates.h
+++ source/physics/class_quantum_gates.h
@@ -10,14 +10,18 @@ namespace qm {
     using cplx = std::complex<double>;
     using real = double;
 
-    struct Gate {
+    class Gate {
+        private:
+        Eigen::Tensor<Scalar,2> exp_internal(const Eigen::Tensor<Scalar,2> & op_, Scalar alpha) const;
+        mutable std::optional<Eigen::Tensor<Scalar,2>> adj = std::nullopt;
+        public:
         Eigen::Tensor<Scalar,2> op;
         std::vector<size_t> pos;
         Gate(const Eigen::Tensor<Scalar,2>  & op_, const std::vector<size_t> & pos_);
-        Gate(const Eigen::Tensor<Scalar,2>  & op_, const std::vector<size_t> & pos_, Scalar delta);
-        void exp_inplace(Scalar delta);
-        [[nodiscard]] Gate exp(Scalar delta) const;
+        Gate(const Eigen::Tensor<Scalar,2>  & op_, const std::vector<size_t> & pos_, Scalar alpha);
+        void exp_inplace(Scalar alpha);
+        [[nodiscard]] Gate exp(Scalar alpha) const;
         [[nodiscard]] bool isUnitary(double prec = 1e-12) const;
-        [[nodiscard]] Gate adjoint() const;
+        [[nodiscard]] Eigen::Tensor<Scalar,2>& adjoint() const;
     };
 }
