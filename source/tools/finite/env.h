6064c66cbcf8e20a57aa2bfc5ceb5a63ed17f723
DavidAce <aceituno@kth.se>
2021-01-10 20:04:23 +0100

    Assert edges in const mode
    Former-commit-id: fe3fe3534bfd3872d9ea268117b0c9ee3b9fd68f

--- source/tools/finite/env.h
+++ source/tools/finite/env.h
@@ -6,9 +6,9 @@ class class_edges_finite;
 
 namespace tools::finite::env{
 //    extern void rebuild_all_edges(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
-    extern void assert_edges(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
-    extern void assert_edges_var(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
-    extern void assert_edges_ene(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
+    extern void assert_edges(const class_state_finite & state, const class_model_finite & model, const class_edges_finite & edges);
+    extern void assert_edges_var(const class_state_finite & state, const class_model_finite & model, const class_edges_finite & edges);
+    extern void assert_edges_ene(const class_state_finite & state, const class_model_finite & model, const class_edges_finite & edges);
     extern void rebuild_edges(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
     extern void rebuild_edges_var(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
     extern void rebuild_edges_ene(const class_state_finite & state, const class_model_finite & model, class_edges_finite & edges);
