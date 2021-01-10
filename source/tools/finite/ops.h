be38aa50ea65d99aa6a1a730f113ac2ddd11a175
DavidAce <aceituno@kth.se>
2021-01-10 18:35:37 +0100

    Use [[nodiscard]] to avoid future bugs

--- source/tools/finite/ops.h
+++ source/tools/finite/ops.h
@@ -11,12 +11,12 @@ namespace tools::finite::ops {
     extern void apply_mpos                    (class_state_finite & state, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
     extern void  project_to_nearest_sector    (class_state_finite & state, const std::string &sector);
     extern void  project_to_sector            (class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
-    extern class_state_finite
+    [[nodiscard]] extern class_state_finite
         get_projection_to_sector              (const class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
-    extern class_state_finite
+    [[nodiscard]] extern class_state_finite
         get_projection_to_nearest_sector      (const class_state_finite & state, const std::string &sector);
-    extern double overlap                     (const class_state_finite & state1, const class_state_finite & state2);
-    extern double expectation_value           (const class_state_finite & state1, const class_state_finite & state2, const std::list<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
-    extern double exp_sq_value                (const class_state_finite & state1, const class_state_finite & state2, const std::list<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,4> & Ledge, const Eigen::Tensor<Scalar,4> & Redge);
+     [[nodiscard]] extern double overlap                     (const class_state_finite & state1, const class_state_finite & state2);
+     [[nodiscard]] extern double expectation_value           (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
+     [[nodiscard]] extern double exp_sq_value                (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,4> & Ledge, const Eigen::Tensor<Scalar,4> & Redge);
     /* clang-format on */
 }
