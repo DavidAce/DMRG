24f6bcf82cba63929bc7c6c32dda4c22ab1dfa8c
DavidAce <aceituno@kth.se>
2021-01-10 18:35:37 +0100

    Use [[nodiscard]] to avoid future bugs

--- source/tools/finite/measure.h
+++ source/tools/finite/measure.h
@@ -20,58 +20,60 @@ namespace tools::finite::measure{
     extern void do_all_measurements(const class_tensors_finite & tensors);
     extern void do_all_measurements(const class_state_finite & state);
 
-    extern size_t length                                      (const class_tensors_finite & tensors);
-    extern size_t length                                      (const class_state_finite & state);
-    extern size_t length                                      (const class_model_finite & model);
-    extern size_t length                                      (const class_edges_finite & edges);
-    extern long   bond_dimension_current                      (const class_state_finite & state);
-    extern long   bond_dimension_midchain                     (const class_state_finite & state);
-    extern std::vector<long> bond_dimensions_merged           (const class_state_finite & state);
-    extern std::vector<long> bond_dimensions                  (const class_state_finite & state);
-    extern double norm                                        (const class_state_finite & state);
-//    extern double norm_fast                                   (const class_state_finite & state);
+    [[nodiscard]] extern size_t length                                      (const class_tensors_finite & tensors);
+    [[nodiscard]] extern size_t length                                      (const class_state_finite & state);
+    [[nodiscard]] extern size_t length                                      (const class_model_finite & model);
+    [[nodiscard]] extern size_t length                                      (const class_edges_finite & edges);
+    [[nodiscard]] extern long   bond_dimension_current                      (const class_state_finite & state);
+    [[nodiscard]] extern long   bond_dimension_midchain                     (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<long> bond_dimensions_merged           (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<long> bond_dimensions                  (const class_state_finite & state);
+    [[nodiscard]] extern double norm                                        (const class_state_finite & state);
+//  [[nodiscard]]  extern double norm_fast                                   (const class_state_finite & state);
 
 
-    extern double spin_component                              (const class_state_finite & state, const Eigen::Matrix2cd &paulimatrix);
-    extern double spin_component                              (const class_state_finite & state, const std::string & axis);
-    extern Eigen::Tensor<Scalar,1> mps_wavefn                 (const class_state_finite & state);
-    extern double entanglement_entropy_current                (const class_state_finite & state);
-    extern double entanglement_entropy_midchain               (const class_state_finite & state);
-    extern std::vector<double> entanglement_entropies         (const class_state_finite & state);
-    extern std::vector<double> renyi_entropies                (const class_state_finite & state, double q);
-    extern std::array<double,3> spin_components               (const class_state_finite & state);
-    extern std::vector<double> truncation_errors              (const class_state_finite & state);
-    extern std::vector<double> truncation_errors_active       (const class_state_finite & state);
+    [[nodiscard]] extern double spin_component                              (const class_state_finite & state, const Eigen::Matrix2cd &paulimatrix);
+    [[nodiscard]] extern double spin_component                              (const class_state_finite & state, const std::string & axis);
+    [[nodiscard]] extern Eigen::Tensor<Scalar,1> mps_wavefn                 (const class_state_finite & state);
+    [[nodiscard]] extern double entanglement_entropy_current                (const class_state_finite & state);
+    [[nodiscard]] extern double entanglement_entropy_midchain               (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<double> entanglement_entropies         (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<double> renyi_entropies                (const class_state_finite & state, double q);
+    [[nodiscard]] extern std::array<double,3> spin_components               (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<double> truncation_errors              (const class_state_finite & state);
+    [[nodiscard]] extern std::vector<double> truncation_errors_active       (const class_state_finite & state);
 
 
     template<typename state_or_mps_type>
-    double energy_minus_energy_reduced             (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy_minus_energy_reduced             (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
     template<typename state_or_mps_type>
-    double energy                                  (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy                                  (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
     template<typename state_or_mps_type>
-    double energy_per_site                         (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy_per_site                         (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
     template<typename state_or_mps_type>
-    double energy_variance                         (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy_variance                         (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
     template<typename state_or_mps_type>
-    double energy_variance_per_site                (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy_variance_per_site                (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, tensors_measure_finite * measurements = nullptr);
 
     template<typename state_or_mps_type>
-    double energy_normalized                       (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, double energy_minimum, double energy_maximum, tensors_measure_finite * measurements = nullptr);
+    [[nodiscard]] double energy_normalized                       (const state_or_mps_type & state, const class_model_finite & model, const class_edges_finite & edges, double energy_minimum, double energy_maximum, tensors_measure_finite * measurements = nullptr);
 
 
-    extern double energy_minus_energy_reduced(const class_tensors_finite & tensors);
-    extern double energy(const class_tensors_finite & tensors);
-    extern double energy_per_site(const class_tensors_finite & tensors);
-    extern double energy_variance(const class_tensors_finite & tensors);
-    extern double energy_variance_per_site(const class_tensors_finite & tensors);
-    extern double energy_normalized(const class_tensors_finite & tensors, double energy_minimum, double energy_maximum);
+    [[nodiscard]] extern double energy_reduced(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_per_site_reduced(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_minus_energy_reduced(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_per_site(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_variance(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_variance_per_site(const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_normalized(const class_tensors_finite & tensors, double energy_minimum, double energy_maximum);
 
-    extern double energy_minus_energy_reduced(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
-    extern double energy(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
-    extern double energy_per_site(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
-    extern double energy_variance(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
-    extern double energy_variance_per_site(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
-    extern double energy_normalized(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors, double energy_minimum, double energy_maximum);
+    [[nodiscard]] extern double energy_minus_energy_reduced(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_per_site(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_variance(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_variance_per_site(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors);
+    [[nodiscard]] extern double energy_normalized(const Eigen::Tensor<Scalar,3> &mps, const class_tensors_finite & tensors, double energy_minimum, double energy_maximum);
 }
 
 /* clang-format on */
