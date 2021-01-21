09392bf57c8c345d880997192a6d67fb2f38520e
DavidAce <aceituno@kth.se>
2021-01-21 18:01:01 +0100

    Moved fmt and spdlog from tools to io
    Former-commit-id: d7aa7c61aff8963f676a41a8aa6e1bbbf45698d4

--- source/tools/common/log.h
+++ source/tools/common/log.h
@@ -1,16 +1,6 @@
 #pragma once
-#if __has_include(<spdlog/logger.h>)
-    #include <spdlog/logger.h>
-    #if !defined(SPDLOG_COMPILED_LIB)
-        #pragma message "SPDLOG_COMPILED_LIB is not defined. Define it and link libspdlog to get shorter compilation"
-    #endif
-    #if defined(FMT_HEADER_ONLY)
-        #pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
-    #endif
-#else
-    #error "Could not find spdlog headers"
-#endif
-
+#include <io/fmt.h>
+#include <io/spdlog.h>
 namespace tools {
     inline std::shared_ptr<spdlog::logger> log;
     namespace Logger {
