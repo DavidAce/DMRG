67c142c4b8f37c7c50656db3d477de20762efc44
DavidAce <aceituno@kth.se>
2021-01-31 08:42:57 +0100

    * Try projecting less often * Try more sites in multisite * Try larger bond dimension * Try converging slightly longer
    Former-commit-id: 8989297b106d22821a4c0c00581ca9beff0b88d4

--- source/algorithms/class_algorithm_finite.h
+++ source/algorithms/class_algorithm_finite.h
@@ -23,7 +23,7 @@ class class_algorithm_finite : public class_algorithm_base {
                                       * Therefore, this number is only incremented when randomizing the current state*/
 
     // Control behavior when stuck
-    size_t max_stuck_iters      = 4;  /*!< If stuck for this many sweeps -> stop. */
+    size_t max_stuck_iters      = 5;  /*!< If stuck for this many sweeps -> stop. */
     size_t min_saturation_iters = 1;  /*!< If both var and ent saturated  this long -> got_stuck: true */
     size_t max_saturation_iters = 10; /*!< If either var or ent saturated this long -> got_stuck: true Requires var and ent stuck for > 0 */
 
