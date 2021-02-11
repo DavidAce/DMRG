985ee4db15fb88d6740776d275b1c4ba16fccd91
DavidAce <aceituno@kth.se>
2021-02-11 07:21:14 +0100

    Refactoring/rename of tensor/state/mps
    Former-commit-id: 2dbdac0a6db122f69e3ba0b920a0f2587e7ccab6

--- source/tools/finite/opt-internal/ceres_direct.cpp
+++ source/tools/finite/opt-internal/ceres_direct.cpp
@@ -15,52 +15,52 @@
 #include <tools/finite/measure.h>
 #include <tools/finite/opt-internal/opt-internal.h>
 #include <tools/finite/opt-internal/report.h>
-#include <tools/finite/opt_state.h>
-tools::finite::opt::opt_state tools::finite::opt::internal::ceres_direct_optimization(const class_tensors_finite &tensors, const class_algorithm_status &status,
+#include <tools/finite/opt_mps.h>
+tools::finite::opt::opt_mps tools::finite::opt::internal::ceres_direct_optimization(const class_tensors_finite &tensors, const class_algorithm_status &status,
                                                                                       OptType optType, OptMode optMode, OptSpace optSpace) {
     std::vector<size_t> sites(tensors.active_sites.begin(), tensors.active_sites.end());
-    opt_state           initial_tensor("current state", tensors.state->get_multisite_mps(), sites,
-                             tools::finite::measure::energy(tensors) - tensors.model->get_energy_reduced(), // Eigval
-                             tensors.model->get_energy_reduced(),                                           // Energy reduced for full system
-                             tools::finite::measure::energy_variance(tensors),
-                             1.0, // Overlap
-                             tensors.get_length());
+    opt_mps             initial_state("current state", tensors.state->get_multisite_mps(), sites,
+                            tools::finite::measure::energy(tensors) - tensors.model->get_energy_reduced(), // Eigval
+                            tensors.model->get_energy_reduced(),                                           // Energy reduced for full system
+                            tools::finite::measure::energy_variance(tensors),
+                            1.0, // Overlap
+                            tensors.get_length());
 
-    return ceres_direct_optimization(tensors, initial_tensor, status, optType, optMode, optSpace);
+    return ceres_direct_optimization(tensors, initial_state, status, optType, optMode, optSpace);
 }
 
-tools::finite::opt::opt_state tools::finite::opt::internal::ceres_direct_optimization(const class_tensors_finite &tensors, const opt_state &initial_tensor,
+tools::finite::opt::opt_mps tools::finite::opt::internal::ceres_direct_optimization(const class_tensors_finite &tensors, const opt_mps &initial_mps,
                                                                                       const class_algorithm_status &status, OptType optType, OptMode optMode,
                                                                                       OptSpace optSpace) {
     tools::log->trace("Optimizing in DIRECT mode");
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir"]->tic();
 
-    reports::bfgs_add_entry("Direct", "init", initial_tensor);
-    const auto &current_tensor = tensors.state->get_multisite_mps();
-    const auto  current_vector = Eigen::Map<const Eigen::VectorXcd>(current_tensor.data(), current_tensor.size());
-    auto        options        = internal::ceres_default_options;
-    auto        summary        = ceres::GradientProblemSolver::Summary();
-    opt_state   optimized_tensor;
-    optimized_tensor.set_name(initial_tensor.get_name());
-    optimized_tensor.set_sites(initial_tensor.get_sites());
-    optimized_tensor.set_length(initial_tensor.get_length());
-    optimized_tensor.set_energy_reduced(initial_tensor.get_energy_reduced());
+
+    reports::bfgs_add_entry("Direct", "init", initial_mps);
+    const auto &current_mps = tensors.state->get_multisite_mps();
+    const auto  current_map = Eigen::Map<const Eigen::VectorXcd>(current_mps.data(), current_mps.size());
+
+    auto      options = internal::ceres_default_options;
+    auto      summary = ceres::GradientProblemSolver::Summary();
+    opt_mps   optimized_mps;
+    optimized_mps.set_name(initial_mps.get_name());
+    optimized_mps.set_sites(initial_mps.get_sites());
+    optimized_mps.set_length(initial_mps.get_length());
+    optimized_mps.set_energy_reduced(initial_mps.get_energy_reduced());
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_bfgs"]->tic();
 
     switch(optType) {
         case OptType::CPLX: {
             // Copy the initial guess and operate directly on it
-            optimized_tensor.set_tensor(initial_tensor.get_tensor());
+            optimized_mps.set_tensor(initial_mps.get_tensor());
             auto *            functor = new ceres_direct_functor<std::complex<double>>(tensors, status);
             CustomLogCallback ceres_logger(*functor);
             options.callbacks.emplace_back(&ceres_logger);
             ceres::GradientProblem problem(functor);
             tools::log->trace("Running LBFGS direct cplx");
-            ceres::Solve(options, problem, optimized_tensor.get_vector_cplx_as_2xreal().data(), &summary);
+            ceres::Solve(options, problem, optimized_mps.get_vector_cplx_as_2xreal().data(), &summary);
             // Copy the results from the functor
-            optimized_tensor.set_counter(functor->get_count());
-            optimized_tensor.set_energy(functor->get_energy());
-            optimized_tensor.set_variance(functor->get_variance());
+            optimized_mps.set_counter(functor->get_count());
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH2"] += *functor->t_vH2;
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH2v"] += *functor->t_vH2v;
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH"] += *functor->t_vH;
@@ -70,18 +70,16 @@ tools::finite::opt::opt_state tools::finite::opt::internal::ceres_direct_optimiz
         }
         case OptType::REAL: {
             // Here we make a temporary
-            auto              initial_tensor_real = initial_tensor.get_vector_cplx_as_1xreal();
-            auto *            functor             = new ceres_direct_functor<double>(tensors, status);
+            auto              initial_state_real = initial_mps.get_vector_cplx_as_1xreal();
+            auto *            functor            = new ceres_direct_functor<double>(tensors, status);
             CustomLogCallback ceres_logger(*functor);
             options.callbacks.emplace_back(&ceres_logger);
             ceres::GradientProblem problem(functor);
             tools::log->trace("Running LBFGS direct real");
-            ceres::Solve(options, problem, initial_tensor_real.data(), &summary);
+            ceres::Solve(options, problem, initial_state_real.data(), &summary);
             // Copy the results from the functor
-            optimized_tensor.set_counter(functor->get_count());
-            optimized_tensor.set_tensor_real(initial_tensor_real.data(), initial_tensor.get_tensor().dimensions());
-            optimized_tensor.set_energy(functor->get_energy());
-            optimized_tensor.set_variance(functor->get_variance());
+            optimized_mps.set_counter(functor->get_count());
+            optimized_mps.set_tensor_real(initial_state_real.data(), initial_mps.get_tensor().dimensions());
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH2"] += *functor->t_vH2;
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH2v"] += *functor->t_vH2v;
             *tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_vH"] += *functor->t_vH;
@@ -91,10 +89,12 @@ tools::finite::opt::opt_state tools::finite::opt::internal::ceres_direct_optimiz
         }
     }
     // Copy and set the rest of the tensor metadata
-    optimized_tensor.normalize();
-    optimized_tensor.set_iter(summary.iterations.size());
-    optimized_tensor.set_time(summary.total_time_in_seconds);
-    optimized_tensor.set_overlap(std::abs(current_vector.dot(optimized_tensor.get_vector())));
+    optimized_mps.normalize();
+    optimized_mps.set_energy(tools::finite::measure::energy(optimized_mps.get_tensor(), tensors));
+    optimized_mps.set_variance(tools::finite::measure::energy_variance(optimized_mps.get_tensor(), tensors));
+    optimized_mps.set_overlap(std::abs(current_map.dot(optimized_mps.get_vector())));
+    optimized_mps.set_iter(summary.iterations.size());
+    optimized_mps.set_time(summary.total_time_in_seconds);
 
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir_bfgs"]->toc();
     reports::time_add_dir_entry();
@@ -103,9 +103,9 @@ tools::finite::opt::opt_state tools::finite::opt::internal::ceres_direct_optimiz
     double sec = std::fmod(std::fmod(summary.total_time_in_seconds, 3600), 60);
     tools::log->debug("Finished LBFGS in {:0<2}:{:0<2}:{:0<.1f} seconds and {} iters. Exit status: {}. Message: {}", hrs, min, sec, summary.iterations.size(),
                       ceres::TerminationTypeToString(summary.termination_type), summary.message.c_str());
-    reports::bfgs_add_entry("Direct", "opt", optimized_tensor);
+    reports::bfgs_add_entry("Direct", "opt", optimized_mps);
 
     tools::log->trace("Returning theta from optimization mode {} space {}", optMode, optSpace);
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_dir"]->toc();
-    return optimized_tensor;
+    return optimized_mps;
 }
--- source/tools/finite/opt-internal/ceres_subspace_hamiltonian.cpp
+++ source/tools/finite/opt-internal/ceres_subspace_hamiltonian.cpp
@@ -13,7 +13,7 @@
 #include <tools/common/log.h>
 #include <tools/common/prof.h>
 #include <tools/finite/opt-internal/opt-internal.h>
-#include <tools/finite/opt_state.h>
+#include <tools/finite/opt_mps.h>
 using Scalar = std::complex<double>;
 using real   = double;
 using cplx   = std::complex<double>;
@@ -59,57 +59,90 @@ MatrixType<T> tools::finite::opt::internal::get_multisite_hamiltonian_matrix(con
 template MatrixType<real> tools::finite::opt::internal::get_multisite_hamiltonian_matrix(const class_model_finite &model, const class_edges_finite &edges);
 template MatrixType<cplx> tools::finite::opt::internal::get_multisite_hamiltonian_matrix(const class_model_finite &model, const class_edges_finite &edges);
 
+
+template<typename T>
+MatrixType<T> tools::finite::opt::internal::get_multisite_hamiltonian_squared_matrix(const class_model_finite &model, const class_edges_finite &edges) {
+    const auto &mpo2 = model.get_multisite_mpo_squared();
+    const auto &env2 = edges.get_multisite_var_blk();
+    tools::log->trace("Contracting multisite hamiltonian");
+    tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_sub_ham"]->tic();
+
+    long dim0 = mpo2.dimension(2);
+    long dim1 = env2.L.dimension(0);
+    long dim2 = env2.R.dimension(0);
+
+    Eigen::Tensor<std::complex<double>, 6> ham2(dim0, dim1, dim2, dim0, dim1, dim2);
+    ham2.device(Textra::omp::getDevice()) =
+        env2.L.contract(mpo2, Textra::idx({2}, {0})).contract(env2.R, Textra::idx({2}, {2})).shuffle(Textra::array6{2, 0, 4, 3, 1, 5});
+
+    auto cols = ham2.dimension(0) * ham2.dimension(1) * ham2.dimension(2);
+    auto rows = ham2.dimension(3) * ham2.dimension(4) * ham2.dimension(5);
+    long size = dim0 * dim1 * dim2;
+    if(rows != size) throw std::runtime_error(fmt::format("Mismatch in multisite hamiltonian rows (dim0*dim1*dim2) and size: {} != {}", cols, size));
+    if(cols != size) throw std::runtime_error(fmt::format("Mismatch in multisite hamiltonian cols (dim3*dim4*dim5) and size: {} != {}", rows, size));
+
+    auto   ham2_map         = Eigen::Map<Eigen::MatrixXcd>(ham2.data(), rows, cols);
+    double non_hermiticity = (ham2_map - ham2_map.adjoint()).cwiseAbs().sum() / static_cast<double>(ham2_map.size());
+    double sparcity        = static_cast<double>((ham2_map.array().cwiseAbs2() != 0.0).count()) / static_cast<double>(ham2_map.size());
+
+    if(non_hermiticity > 1e-12) throw std::runtime_error(fmt::format("multisite hamiltonian squared is not hermitian: {:.16f}", non_hermiticity));
+    if(non_hermiticity > 1e-14) tools::log->warn("multisite hamiltonian squared is slightly non-hermitian: {:.16f}", non_hermiticity);
+    if(ham2_map.hasNaN()) throw std::runtime_error("multisite hamiltonian squared has NaN's!");
+    tools::log->trace("multisite hamiltonian squared nonzeros: {:.8f} %", sparcity * 100);
+    tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_sub_ham"]->toc();
+    if constexpr(std::is_same_v<T, double>) {
+        return Eigen::Map<Eigen::MatrixXcd>(ham2.data(), rows, cols).real().transpose().selfadjointView<Eigen::Lower>();
+    } else {
+        return Eigen::Map<Eigen::MatrixXcd>(ham2.data(), rows, cols).transpose().selfadjointView<Eigen::Lower>();
+    }
+}
+template MatrixType<real> tools::finite::opt::internal::get_multisite_hamiltonian_squared_matrix(const class_model_finite &model, const class_edges_finite &edges);
+template MatrixType<cplx> tools::finite::opt::internal::get_multisite_hamiltonian_squared_matrix(const class_model_finite &model, const class_edges_finite &edges);
+
+
+
 template<typename T>
 MatrixType<T> tools::finite::opt::internal::get_multisite_hamiltonian_squared_subspace_matrix(const class_model_finite &model, const class_edges_finite &edges,
-                                                                                              const std::vector<opt_state> &candidate_list) {
+                                                                                              const std::vector<opt_mps> &candidate_list) {
     // First, make sure every candidate is actually a basis vector, otherwise this computation would turn difficult if we have to skip rows and columns
     for(const auto &candidate : candidate_list)
         if(not candidate.is_basis_vector)
             throw std::runtime_error("One candidate is not a basis vector. When constructing a hamiltonian subspace matrix, make sure the candidates are all "
                                      "eigenvectors/basis vectors");
 
-    const auto &mpo = model.get_multisite_mpo_squared();
-    const auto &env = edges.get_multisite_var_blk();
+    const auto &mpo2 = model.get_multisite_mpo_squared();
+    const auto &env2 = edges.get_multisite_var_blk();
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_sub_hsq"]->tic();
     tools::log->trace("Contracting subspace hamiltonian squared new");
-    long dim0   = mpo.dimension(2);
-    long dim1   = env.L.dimension(0);
-    long dim2   = env.R.dimension(0);
+    long dim0   = mpo2.dimension(2);
+    long dim1   = env2.L.dimension(0);
+    long dim2   = env2.R.dimension(0);
     long eignum = static_cast<long>(candidate_list.size()); // Number of eigenvectors
 
     Eigen::Tensor<Scalar, 0> H2_ij;
-    Eigen::Tensor<Scalar, 3> Hv(dim0, dim1, dim2);
-    Eigen::MatrixXcd         H2(eignum, eignum);
+    Eigen::Tensor<Scalar, 3> H2_mps(dim0, dim1, dim2); // The local hamiltonian multiplied by mps at column j.
+    Eigen::MatrixXcd         H2_sub(eignum, eignum); // The local hamiltonian projected to the subspace (spanned by candidate_list)
 
     for(auto col = 0; col < eignum; col++) {
-        const auto &theta_j = std::next(candidate_list.begin(), col)->get_tensor();
-        tools::common::contraction::matrix_vector_product(Hv, theta_j, mpo, env.L, env.R);
+        const auto &mps_j = std::next(candidate_list.begin(), col)->get_tensor();
+        tools::common::contraction::matrix_vector_product(H2_mps, mps_j, mpo2, env2.L, env2.R);
         for(auto row = col; row < eignum; row++) {
-            const auto &theta_i                    = std::next(candidate_list.begin(), row)->get_tensor();
-            H2_ij.device(Textra::omp::getDevice()) = theta_i.conjugate().contract(Hv, Textra::idx({0, 1, 2}, {0, 1, 2}));
-            H2(row, col)                           = H2_ij(0);
+            const auto &mps_i                    = std::next(candidate_list.begin(), row)->get_tensor();
+            H2_ij.device(Textra::omp::getDevice()) = mps_i.conjugate().contract(H2_mps, Textra::idx({0, 1, 2}, {0, 1, 2}));
+            H2_sub.selfadjointView<Eigen::Lower>().coeff(row, col) = H2_ij(0);
         }
     }
-
-    H2                     = H2.selfadjointView<Eigen::Lower>();
-    double non_hermiticity = (H2 - H2.adjoint()).cwiseAbs().sum() / static_cast<double>(H2.size());
-    double sparcity        = static_cast<double>((H2.array().cwiseAbs2() != 0.0).count()) / static_cast<double>(H2.size());
-    if(H2.rows() != H2.cols()) throw std::runtime_error(fmt::format("Hamiltonian tensor is not square: rows [{}] | cols [{}]", H2.rows(), H2.cols()));
-    if(non_hermiticity > 1e-12) throw std::runtime_error(fmt::format("subspace hamiltonian squared is not hermitian: {:.16f}", non_hermiticity));
-    if(non_hermiticity > 1e-14) tools::log->warn("subspace hamiltonian squared is slightly non-hermitian: {:.16f}", non_hermiticity);
-    if(H2.hasNaN()) throw std::runtime_error("subspace hamiltonian squared has NaN's!");
-    tools::log->trace("multisite hamiltonian squared nonzeros: {:.8f} %", sparcity * 100);
     tools::common::profile::prof[AlgorithmType::xDMRG]["t_opt_sub_hsq"]->toc();
     if constexpr(std::is_same_v<T, double>)
-        return H2.real();
+        return H2_sub.real().template selfadjointView<Eigen::Lower>();
     else
-        return H2;
+        return H2_sub.selfadjointView<Eigen::Lower>();
 }
 
 // Explicit instantiations
 template MatrixType<real> tools::finite::opt::internal::get_multisite_hamiltonian_squared_subspace_matrix(const class_model_finite &    model,
                                                                                                           const class_edges_finite &    edges,
-                                                                                                          const std::vector<opt_state> &candidate_list);
+                                                                                                          const std::vector<opt_mps> &candidate_list);
 template MatrixType<cplx> tools::finite::opt::internal::get_multisite_hamiltonian_squared_subspace_matrix(const class_model_finite &    model,
                                                                                                           const class_edges_finite &    edges,
-                                                                                                          const std::vector<opt_state> &candidate_list);
+                                                                                                          const std::vector<opt_mps> &candidate_list);
