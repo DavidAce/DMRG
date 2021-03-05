34e42172648cd7ef4192804a82d248f09513ea83
DavidAce <aceituno@kth.se>
2021-03-05 12:35:33 +0100

    Preparing tetralith xdmrg batch with more svd precision and bfgs precision
    Former-commit-id: e642c880a55fc477ece6c8a0274d8015879f18db

--- build.sh
+++ build.sh
@@ -239,8 +239,8 @@ fi
 if [[ "$HOSTNAME" == *"tetralith"* ]];then
     echo "Running on tetralith"
     if [ -z "$no_module" ]; then
-        module load buildenv-gcc/.9.3.0-bare
-        module load CMake/3.19.2
+        module load foss/2020b
+#        module load CMake/3.19.2
         if [ "$enable_mkl" = "ON" ] ; then
             export MKLROOT=/software/sse/easybuild/prefix/software/imkl/2019.1.144-iimpi-2019a/mkl
             export EBROOTIMKL=/software/sse/easybuild/prefix/software/imkl/2019.1.144-iimpi-2019a
