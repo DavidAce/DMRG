9664d5a6dae06443693a281367128616b9384ba6
DavidAce <aceituno@kth.se>
2021-03-09 11:07:23 +0100

    Faster detection of saturation
    Former-commit-id: 52b1cadae39e6c3c67da633a47f4b8d66cd5c060

--- source/algorithms/class_algorithm_finite.h
+++ source/algorithms/class_algorithm_finite.h
@@ -23,9 +23,9 @@ class class_algorithm_finite : public class_algorithm_base {
                                       * Therefore, this number is only incremented when randomizing the current state*/
 
     // Control behavior when stuck
-    size_t max_stuck_iters      = 10; //  5;  /*!< If stuck for this many sweeps -> stop. */
-    size_t min_saturation_iters = 4;  // 1;  /*!< If both var and ent saturated  this long -> got_stuck: true */
-    size_t max_saturation_iters = 20; // 10; /*!< If either var or ent saturated this long -> got_stuck: true Requires var and ent stuck for > 0 */
+    size_t max_stuck_iters      = 10;  //  5;  /*!< If stuck for this many sweeps -> stop. */
+    size_t min_saturation_iters = 2;   // 1;  /*!< If both var and ent saturated  this long -> got_stuck: true */
+    size_t max_saturation_iters = 20;  // 10; /*!< If either var or ent saturated this long -> got_stuck: true Requires var and ent stuck for > 0 */
 
     bool                    has_projected        = false;        /*!< True if projection has already been tried */
     bool                    has_damped           = false;        /*!< True if damping of hamiltonian parameters is ongoing */
