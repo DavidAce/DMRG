8ced0f1c8b04950455987a26a20e08dd8221c507
DavidAce <aceituno@kth.se>
2021-04-16 15:12:44 +0200

    clang-format

--- source/tools/finite/ops.h
+++ source/tools/finite/ops.h
@@ -7,16 +7,17 @@ class class_mpo_site;
 namespace tools::finite::ops {
     using Scalar = std::complex<double>;
     /* clang-format off */
-    extern void apply_mpo                     (class_state_finite & state, const Eigen::Tensor<Scalar,4> & mpo, const Eigen::Tensor<Scalar,3> &Ledge, const Eigen::Tensor<Scalar,3> & Redge);
-    extern void apply_mpos                    (class_state_finite & state, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
-    extern void  project_to_nearest_sector    (class_state_finite & state, const std::string &sector);
-    extern void  project_to_sector            (class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
-    [[nodiscard]] extern class_state_finite
-        get_projection_to_sector              (const class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
-    [[nodiscard]] extern class_state_finite
-        get_projection_to_nearest_sector      (const class_state_finite & state, const std::string &sector);
-     [[nodiscard]] extern double overlap                     (const class_state_finite & state1, const class_state_finite & state2);
-     [[nodiscard]] extern double expectation_value           (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
-     [[nodiscard]] extern double exp_sq_value                (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,4> & Ledge, const Eigen::Tensor<Scalar,4> & Redge);
+    extern void apply_mpo                                   (class_state_finite & state, const Eigen::Tensor<Scalar,4> & mpo, const Eigen::Tensor<Scalar,3> &Ledge, const Eigen::Tensor<Scalar,3> & Redge);
+    extern void apply_mpos                                  (class_state_finite & state, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
+    extern void project_to_nearest_sector                   (class_state_finite & state, const std::string &sector);
+    extern void project_to_sector                           (class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
+    [[nodiscard]] extern
+    class_state_finite get_projection_to_sector             (const class_state_finite & state, const Eigen::MatrixXcd & paulimatrix, int sign);
+    [[nodiscard]] extern
+    class_state_finite get_projection_to_nearest_sector     (const class_state_finite & state, const std::string & sector);
+
+    [[nodiscard]] extern double overlap                     (const class_state_finite & state1, const class_state_finite & state2);
+    [[nodiscard]] extern double expectation_value           (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,3> & Ledge, const Eigen::Tensor<Scalar,3> & Redge);
+    [[nodiscard]] extern double exp_sq_value                (const class_state_finite & state1, const class_state_finite & state2, const std::vector<Eigen::Tensor<Scalar,4>> & mpos, const Eigen::Tensor<Scalar,4> & Ledge, const Eigen::Tensor<Scalar,4> & Redge);
     /* clang-format on */
 }
