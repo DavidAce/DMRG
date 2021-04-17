228e06b3eaf9060535681079b56aeef5988f9827
DavidAce <aceituno@kth.se>
2021-04-17 18:18:18 +0200

    More accurate re-normalization of results from arpack
    More accurate re-normalization of results from arpack

--- source/tools/finite/opt-internal/ground_state.cpp
+++ source/tools/finite/opt-internal/ground_state.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <config/nmspc_settings.h>
+#include <general/nmspc_tensor_extra.h>
 #include <math/eig.h>
 #include <math/eig/arpack_solver/matrix_product_hamiltonian.h>
 #include <tensors/class_tensors_finite.h>
@@ -33,16 +34,20 @@ Eigen::Tensor<class_tensors_finite::Scalar, 3> tools::finite::opt::internal::gro
     MatrixProductHamiltonian<Scalar> matrix(env.L.data(), env.R.data(), mpo.data(), shape_mps, shape_mpo);
     tools::log->trace("Defining eigenvalue solver");
     eig::solver solver;
+
+    if(tensors.measurements.energy_variance)
+        solver.config.eigThreshold = std::clamp( 1e-2 * tensors.measurements.energy_variance.value(), 1e-16 , settings::precision::eig_threshold);
+
     // Since we use reduced energy mpo's, we set a sigma shift = 1.0 to move the smallest eigenvalue away from 0, which
     // would otherwise cause trouble for Arpack. This equates to subtracting sigma * identity from the bottom corner of the mpo.
     // The resulting eigenvalue will be shifted by the same amount, but the eigenvector will be the same, and that's what we keep.
     tools::log->trace("Finding ground state");
     try{
         solver.eigs(matrix, nev, ncv, ritz, eig::Form::SYMM, eig::Side::R, 1.0, eig::Shinv::OFF, eig::Vecs::ON, eig::Dephase::OFF);
-        return eig::view::get_eigvec<Scalar>(solver.result, shape_mps, 0);
+        return Textra::asNormalized(eig::view::get_eigvec<Scalar>(solver.result, shape_mps, 0));
     }catch(const std::exception & ex){
         tools::log->warn("Arpack failed. shape_mps {} | shape_mpo {} | sites {}", shape_mps, shape_mpo, tensors.active_sites);
-        return tensors.get_multisite_mps();
+        return Textra::asNormalized(tensors.get_multisite_mps());
     }
 
 }
