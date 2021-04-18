479202b286a5ced36e0adf6b981bce6917046d4c
DavidAce <aceituno@kth.se>
2021-04-18 17:45:23 +0200

    Re-introduce the slope condition (again) when detecting saturation

--- source/algorithms/class_algorithm_base.h
+++ source/algorithms/class_algorithm_base.h
@@ -64,7 +64,7 @@ class class_algorithm_base {
         std::vector<double> Y_stn; // The "normalized" scale invariant version of Y_std
         //        std::vector<double> Y_log; // Normalized values to check saturation. Let y = -log10(Y_vec). Then Y_log = y/y.back()
         //        std::vector<double> Y_ste; // The "moving" standard error of Y_log. (std from x -> end, moving x towards end)
-        //        std::vector<double> Y_slp; // The "moving" slope of Y_log. (slope from x -> end, moving x towards end)
+        std::vector<double> Y_slp; // The "moving" slope of Y_log. (slope from x -> end, moving x towards end)
     };
     size_t count_convergence(const std::vector<double> & Y_vec, double threshold, size_t start_idx = 0);
     SaturationReport check_saturation(const std::vector<double> &Y_vec, double sensitivity);
