6441e2752bf3b0c070c5600daf21fc46f5f206e7
DavidAce <aceituno@kth.se>
2021-05-18 10:39:12 +0200

    Add pretty typename printer in for eig::sfinae

--- source/math/eig/sfinae.h
+++ source/math/eig/sfinae.h
@@ -8,4 +8,26 @@ namespace eig::sfinae {
     struct has_RawEigenvaluesImag<T, std::void_t<decltype(std::declval<T>().size())>> : public std::true_type {};
     template<typename T>
     inline constexpr bool has_RawEigenvaluesImag_v = has_RawEigenvaluesImag<T>::value;
+
+    template<typename T>
+    constexpr auto type_name() {
+        std::string_view name, prefix, suffix;
+#ifdef __clang__
+        name   = __PRETTY_FUNCTION__;
+        prefix = "auto eig::sfinae::type_name() [T = ";
+        suffix = "]";
+#elif defined(__GNUC__)
+        name   = __PRETTY_FUNCTION__;
+        prefix = "constexpr auto eig::sfinae::type_name() [with T = ";
+        suffix = "]";
+#elif defined(_MSC_VER)
+        name   = __FUNCSIG__;
+        prefix = "auto __cdecl eig::sfinae::type_name<";
+        suffix = ">(void)";
+#endif
+        name.remove_prefix(prefix.size());
+        name.remove_suffix(suffix.size());
+        return name;
+    }
+
 }
