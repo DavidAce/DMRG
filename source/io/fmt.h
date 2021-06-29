8896549d87991ddf57f2c02e6bc43c974f2052b3
DavidAce <aceituno@kth.se>
2021-06-29 13:58:40 +0200

    Update include logic for spdlog and fmt
    Former-commit-id: fa8fd3b4f11a6cb4f03c6227417aea318ea9c56b

--- source/io/fmt.h
+++ source/io/fmt.h
@@ -1,28 +1,33 @@
 #pragma once
 
-#if defined(SPDLOG_FMT_EXTERNAL) && \
-    __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
-#include <fmt/core.h>
-#include <fmt/format.h>
-#include <fmt/ostream.h>
-#include <fmt/ranges.h>
-#elif !defined(SPDLOG_FMT_EXTERNAL) && \
-    __has_include(<spdlog/fmt/fmt.h>) && __has_include(<spdlog/fmt/bundled/ranges.h>) &&  __has_include(<spdlog/fmt/bundled/ostream.h>)
-#include <spdlog/fmt/bundled/ostream.h>
-    #include <spdlog/fmt/bundled/ranges.h>
+#if !defined(SPDLOG_COMPILED_LIB)
+    #if !defined(SPDLOG_HEADER_ONLY)
+        #define SPDLOG_HEADER_ONLY
+    #endif
+#endif
+
+#if __has_include(<spdlog/fmt/fmt.h>)
+    // Spdlog will include the bundled fmt unless SPDLOG_FMT_EXTERNAL is defined, in which case <fmt/core.h> gets included instead
+    // If SPDLOG_HEADER_ONLY is defined this will cause FMT_HEADER_ONLY to also get defined
     #include <spdlog/fmt/fmt.h>
-    #if defined(FMT_HEADER_ONLY)
-        #pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
+    #if defined(SPDLOG_FMT_EXTERNAL)
+        #include <fmt/compile.h>
+        #include <fmt/ostream.h>
+        #include <fmt/ranges.h>
+    #else
+        #include <spdlog/fmt/bundled/compile.h>
+        #include <spdlog/fmt/bundled/ostream.h>
+        #include <spdlog/fmt/bundled/ranges.h>
     #endif
 #elif __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
-// Check if there are already fmt headers installed independently from Spdlog
-// Note that in this case the user hasn't enabled Spdlog for h5pp, so the build hasn't linked any compiled FMT libraries
-// To avoid undefined references we coult opt in to the header-only mode of FMT.
-// Note that this check should be skipped if using conan. Then, SPDLOG_FMT_EXTERNAL is defined
+    #if defined(SPDLOG_HEADER_ONLY)
+    // Since spdlog is header-only, let's assume fmt is as well
+    // We do this because we have no way of knowing if this is getting linked to libfmt
+        #define FMT_HEADER_ONLY
+    #endif
+    #include <fmt/compile.h>
     #include <fmt/core.h>
     #include <fmt/format.h>
     #include <fmt/ostream.h>
     #include <fmt/ranges.h>
-#else
-    #error "Could not find fmt headers"
 #endif
