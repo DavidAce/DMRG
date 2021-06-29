bfb87f3868afc2bfdb776d3a7008d52dc0ae840c
DavidAce <aceituno@kth.se>
2021-06-29 13:58:40 +0200

    Update include logic for spdlog and fmt

--- source/io/spdlog.h
+++ source/io/spdlog.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "fmt.h"
-#if __has_include(<spdlog/logger.h>)
-#include <spdlog/logger.h>
-#if !defined(SPDLOG_COMPILED_LIB)
-#pragma message "SPDLOG_COMPILED_LIB is not defined. Define it and link libspdlog to get shorter compilation"
-#endif
-#if defined(FMT_HEADER_ONLY)
-#pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
-#endif
-#else
-#error "Could not find spdlog headers"
+
+#if __has_include(<spdlog/spdlog.h>) && __has_include(<spdlog/sinks/stdout_color_sinks.h>)
+    #include <spdlog/sinks/stdout_color_sinks.h>
+    #include <spdlog/spdlog.h>
+
+    #if !defined(SPDLOG_COMPILED_LIB)
+        #pragma message "SPDLOG_COMPILED_LIB is not defined. Define it and link libspdlog to get shorter compilation"
+    #endif
+
+    #if defined(FMT_HEADER_ONLY)
+        #pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
+    #endif
 #endif
