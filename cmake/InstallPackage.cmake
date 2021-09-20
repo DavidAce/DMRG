a687cff89fedb35e4dccbc3159a2522f14b1dd40
DavidAce <aceituno@kth.se>
2021-09-20 17:45:13 +0200

    Updated InstallPackage.cmake script with changes from h5pp
    Former-commit-id: d6db65af74146c152d0b9ddea2bd0c574e9eabe2

--- cmake/InstallPackage.cmake
+++ cmake/InstallPackage.cmake
@@ -50,10 +50,10 @@ function(install_package pkg_name)
     endif()
 
     # Further parsing / override defaults
-    set(pkg_build_dir ${PKG_BUILD_DIR}/${pkg_name})
-    set(pkg_install_dir   ${PKG_INSTALL_DIR})
+    set(pkg_build_dir   ${PKG_BUILD_DIR}/${pkg_name})
+    set(pkg_install_dir ${PKG_INSTALL_DIR})
     set(pkg_target_name ${pkg_name}::${pkg_name})
-    set(pkg_find_name ${pkg_name})
+    set(pkg_find_name   ${pkg_name})
     if(PKG_INSTALL_PREFIX_PKGNAME)
         set(pkg_install_dir ${pkg_install_dir}/${pkg_name})
     endif()
@@ -102,11 +102,11 @@ function(install_package pkg_name)
         message(FATAL_ERROR "Could not install ${pkg_name}: dependencies missing [${PKG_MISSING_TARGET}]")
     endif()
 
-    # We set variables here that allows us to find packages with CMAKE_PREFIX_PATH
-    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${pkg_install_dir}" CACHE INTERNAL "Paths for find_package lookup")
+    # Append to CMAKE_PREFIX_PATH so we can find the packages later
+    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${pkg_install_dir};${PKG_INSTALL_DIR}" CACHE INTERNAL "Paths for find_package lookup" FORCE)
     list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
 
-    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" CACHE STRING "" FORCE)
+    # Try finding config files before modules
     set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
 
     if(PKG_LIBRARY_NAMES)
@@ -125,22 +125,26 @@ function(install_package pkg_name)
             find_package(${pkg_find_name} ${PKG_VERSION} ${COMPONENTS} ${PKG_COMPONENTS} ${QUIET})
         endif()
     elseif(PKG_MODULE)
-        find_package(${pkg_find_name} ${PKG_VERSION} ${COMPONENTS} ${PKG_COMPONENTS} ${QUIET})
+        if(NOT ${pkg_find_name}_FOUND)
+            find_package(${pkg_find_name} ${PKG_VERSION} ${COMPONENTS} ${PKG_COMPONENTS} ${QUIET})
+        endif()
     else()
-        find_package(${pkg_find_name} ${PKG_VERSION}
-                HINTS ${PKG_HINTS}
-                PATHS ${PKG_PATHS}
-                PATH_SUFFIXES ${PKG_PATH_SUFFIXES}
-                ${COMPONENTS} ${PKG_COMPONENTS}
-                ${CONFIG} ${QUIET}
-                # These lets us ignore system packages
-                NO_SYSTEM_ENVIRONMENT_PATH #5
-                NO_CMAKE_PACKAGE_REGISTRY #6
-                NO_CMAKE_SYSTEM_PATH #7
-                NO_CMAKE_SYSTEM_PACKAGE_REGISTRY #8
-                )
+        if(NOT ${pkg_find_name}_FOUND)
+            find_package(${pkg_find_name} ${PKG_VERSION}
+                    HINTS ${PKG_HINTS}
+                    PATHS ${PKG_PATHS}
+                    PATH_SUFFIXES ${PKG_PATH_SUFFIXES}
+                    ${COMPONENTS} ${PKG_COMPONENTS}
+                    ${CONFIG} ${QUIET}
+                    # These lets us ignore system packages
+                    NO_SYSTEM_ENVIRONMENT_PATH #5
+                    NO_CMAKE_PACKAGE_REGISTRY #6
+                    NO_CMAKE_SYSTEM_PATH #7
+                    NO_CMAKE_SYSTEM_PACKAGE_REGISTRY #8
+                    )
+        endif()
     endif()
-    # Check if the package was found
+    # Set _FOUND variables for alternate names and components
     if(NOT ${pkg_name}_FOUND)
         if(${pkg_find_name}_FOUND)
             set(${pkg_name}_FOUND TRUE)
@@ -154,7 +158,7 @@ function(install_package pkg_name)
             endforeach()
         endif()
     endif()
-
+    # Check if the package was found
     if(${pkg_name}_FOUND)
         if(PKG_DEPENDS)
             target_link_libraries(${pkg_target_name} INTERFACE ${PKG_DEPENDS})
