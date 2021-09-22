07aaf919e5855faf4d6d37fd1d5ac7408800ba5e
DavidAce <aceituno@kth.se>
2021-09-22 09:01:07 +0200

    Start testing rclone instead of rsync
    Former-commit-id: d4ca92f454bb637479be5a97cc61bf7502c07756

---
+++ batch_scripts/rclone_results.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+PROGNAME=$0
+
+usage() {
+  cat << EOF >&2
+
+Usage              : $PROGNAME [-f <input_file>] [-h] [-m <mode>] [-t <target>]
+
+-a <address>       : Target machine IP address (default = thinkstation.duckdns.org)
+-d                 : Performs a dry run.
+-h                 : Help. Shows this text.
+-p <target prefix> : Prefix at destination (default = /mnt/Barracuda/Projects/mbl_transition)
+-s <source dir>    : Source relative to current dir (default = .)
+-t <target dir>    : Target directory (default = tmp)
+-u <user>          : User at target machine (default = david)
+-L                 : Follow symbolic links
+EOF
+  exit 1
+}
+default_adr="thinkstation.duckdns.org"
+default_pfx="/mnt/Barracuda/Projects/mbl_transition"
+default_src="."
+default_tgt="tmp"
+default_usr="david"
+max_threads=4
+dry_run=""
+follow_sym=""
+while getopts a:dhn:p:s:t:u:L o; do
+      case $o in
+        (a) default_adr=$OPTARG;;
+        (d) dry_run="--dry-run";;
+        (h) usage ;;
+        (n) max_threads=$OPTARG;;
+        (p) default_pfx=$OPTARG;;
+        (s) default_src=$OPTARG;;
+        (t) default_tgt=$OPTARG;;
+        (u) default_usr=$OPTARG;;
+        (L) follow_sym="-L" ;;
+        (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1 ;;
+        (?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+        (*) usage ;;
+      esac
+done
+
+#if [ $OPTIND -eq 1 ]; then echo "No flags were passed"; usage ;exit 1; fi
+
+
+rclone copy $default_src ThinkStation:${default_pfx}/${default_tgt} $follow_sym $dry_run --progress --update --multi-thread-streams=$max_threads
