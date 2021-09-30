e8dc895eeb42841471f1832ec80b4e0e24332361
DavidAce <aceituno@kth.se>
2021-09-30 19:47:20 +0200

    account for missing jobids
    Former-commit-id: d465135c110f5ec5bcce824826afd6c4f698fffd

--- batch_scripts/gather_failed.py
+++ batch_scripts/gather_failed.py
@@ -22,6 +22,10 @@ args = parser.parse_args()
 if args.outdir:
     Path(args.outdir).mkdir(parents=True, exist_ok=True)
 
+def getlines(file, linelist):
+  return [x for i, x in enumerate(file) if i in map(int,linelist)]
+
+
 # Gather job-id's that may contain failed jobs. Remember, 1 job-id can correspond to a job-array
 # which may contain many simulations (seeds)
 joblist = []
@@ -36,9 +40,9 @@ jobitem = {
 with open("{}/{}".format(args.outdir, args.failfile), "w") as output:
     sacct_command = ["sacct","-X", "--parsable2", "--noheader", '--format=jobid,jobidraw,jobname,exitcode,state']
     if args.logscan:
-        sacct_command.append("--state=completed,running,failed,timeout,deadline,node_fail")
+        sacct_command.append("--state=failed,timeout,deadline,node_fail,completed")
     else:
-        sacct_command.append("--state=failed,timeout,resizing,deadline,node_fail")
+        sacct_command.append("--state=failed,timeout,deadline,node_fail")
 
     sacct_command.extend(["-u", args.user])
     if args.jobname:
@@ -77,19 +81,43 @@ for jobitem in joblist:
 
     # Found a logfile that may contain a failed simulation (or not, if doing logscan)
     with open(jobitem['outfile'], "r") as log, open("{}/{}".format(args.outdir,args.resfile), "a") as resfile:
-        cfgline = ''
+        cfgline = None
+        sequence = None
+        jobfile = None
+        success = set([])
+        jobid   = None
         for line in log:
-            if not line.startswith(('CONFIG', 'JOB', 'EXIT')):
+            if not line.startswith(('JOB ID', 'JOB FILE', 'EXIT', 'TASK ID SEQUENCE')):
                 continue
             keyval = [item.strip() for item in line.split(':',1)]
-            if keyval[0] == 'CONFIG LINE' or keyval[0] == 'JOB FILE LINE(S)':
-                cfgline = keyval[1]
-            if keyval[0] == "EXIT CODE" and keyval[1] != "0":
-                if len(cfgline) == 0:
-                    raise Exception('cfgline is empty')
-                print("Found failed simulation | exit {:>3} | {}".format(keyval[1],cfgline))
-                resfile.write("{}\n".format(cfgline))
-                count = count + 1
+            if keyval[0] == 'JOB FILE':
+                jobfile = keyval[1] # Contains the array jobarray chunk file.
+            if keyval[0] == 'TASK ID SEQUENCE':
+                sequence = set(map(int,keyval[1].split(' '))
+            if keyval[0] == 'JOB ID':
+                jobid = int(keyval[1])
+            if keyval[0] == "EXIT CODE" and keyval[1] == "0":
+                success.add(jobid)
+
+        # We can now compare the ids in the task id sequence that should have run,
+        # to the ones that actually suceeded.
+        failed = sequence - success
+
+        # We can now extract the config lines that should have executed
+        cfglines = getlines(open(jobfile,"r"), failed)
+        for cfgline in cfglines:
+            print("Found failed simulation | exit {:>3} | {}".format(keyval[1],cfgline))
+            resfile.write("{}\n".format(cfgline))
+            count = count + 1
+#
+#             if keyval[0] == 'CONFIG LINE' or keyval[0] == 'JOB FILE LINE(S)':
+#                 cfgline = keyval[1]
+#             if keyval[0] == "EXIT CODE" and keyval[1] != "0":
+#                 if not cfgline:
+#                     raise Exception('cfgline is empty')
+#                 print("Found failed simulation | exit {:>3} | {}".format(keyval[1],cfgline))
+#                 resfile.write("{}\n".format(cfgline))
+#                 count = count + 1
 
 print('Found {} jobids'.format(len(joblist)))
 print('Found {} failed simulations'.format(count))
