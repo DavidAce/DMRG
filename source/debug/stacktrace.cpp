a9113ac304718edeef74bed078fc393e0cda7cc1
DavidAce <aceituno@kth.se>
2021-10-19 01:44:11 +0200

    Improved signal handling/logging when exiting
    Former-commit-id: 009b06588d737a7f2359c8207f48fd54b2b68262

--- source/debug/stacktrace.cpp
+++ source/debug/stacktrace.cpp
@@ -3,42 +3,48 @@
 #include <cxxabi.h>
 #include <stdexcept>
 
-void debug::signal_callback_handler(int signum) {
-    switch(signum) {
+void debug::signal_callback_handler(int status) {
+    debug::print_stack_trace();
+    switch(status) {
         case SIGTERM: {
-            std::fprintf(stderr, "Caught SIGTERM\n");
+            std::fprintf(stderr, "Exit SIGTERM: %d\n", status);
             break;
         }
         case SIGKILL: {
-            std::fprintf(stderr, "Caught SIGKILL\n");
+            std::fprintf(stderr, "Exit SIGKILL: %d\n", status);
+            std::quick_exit(status); // Does not call destructors
             break;
         }
         case SIGINT: {
-            std::fprintf(stderr, "Caught SIGINT\n");
+            std::fprintf(stderr, "Exit SIGINT: %d\n", status);
             break;
         }
         case SIGHUP: {
-            std::fprintf(stderr, "Caught SIGHUP\n");
+            std::fprintf(stderr, "Exit SIGHUP: %d\n", status);
             break;
         }
         case SIGQUIT: {
-            std::fprintf(stderr, "Caught SIGQUIT\n");
+            std::fprintf(stderr, "Exit SIGQUIT: %d\n", status);
             break;
         }
         case SIGABRT: {
-            std::fprintf(stderr, "Caught SIGABRT\n");
+            std::fprintf(stderr, "Exit SIGABRT: %d\n", status);
             break;
         }
         case SIGSEGV: {
-            std::fprintf(stderr, "Caught SIGSEGV\n");
+            std::fprintf(stderr, "Exit SIGSEGV: %d\n", status);
+            std::quick_exit(status); // Does not call destructors
             break;
         }
-        default: break;
+        default: {
+            std::fprintf(stderr, "Exit %d\n", status);
+            break;
+        }
     }
-    debug::print_stack_trace();
-    std::fprintf(stderr, "Exiting\n");
-    std::quick_exit(signum);
+
+    std::exit(status);
 }
+
 
 void debug::register_callbacks() {
     signal(SIGTERM, signal_callback_handler);
@@ -48,6 +54,41 @@ void debug::register_callbacks() {
     signal(SIGQUIT, signal_callback_handler);
     signal(SIGABRT, signal_callback_handler);
     signal(SIGSEGV, signal_callback_handler);
+
+    /* ISO C99 signals.  */
+    signal(SIGINT, signal_callback_handler);  //	 2	/* Interactive attention signal.  */
+    signal(SIGILL, signal_callback_handler);  //	 4	/* Illegal instruction.  */
+    signal(SIGABRT, signal_callback_handler); //	 6	/* Abnormal termination.  */
+    signal(SIGFPE, signal_callback_handler);  //	 8	/* Erroneous arithmetic operation.  */
+    signal(SIGSEGV, signal_callback_handler); //	 11	/* Invalid access to storage.  */
+    signal(SIGTERM, signal_callback_handler); //	 15	/* Termination request.  */
+
+    /* Historical signals specified by POSIX. */
+    signal(SIGHUP, signal_callback_handler);  //	 1	/* Hangup.  */
+    signal(SIGQUIT, signal_callback_handler); //	 3	/* Quit.  */
+    signal(SIGTRAP, signal_callback_handler); //	 5	/* Trace/breakpoint trap.  */
+    signal(SIGKILL, signal_callback_handler); //	 9	/* Killed.  */
+    signal(SIGBUS, signal_callback_handler);  //	 10	/* Bus error.  */
+    signal(SIGSYS, signal_callback_handler);  //	 12	/* Bad system call.  */
+    signal(SIGPIPE, signal_callback_handler); //	 13	/* Broken pipe.  */
+    signal(SIGALRM, signal_callback_handler); //	 14	/* Alarm clock.  */
+
+    /* New(er) POSIX signals (1003.1-2008, 1003.1-2013).  */
+    signal(SIGURG, signal_callback_handler);    //   16 /* Urgent data is available at a socket.  */
+    signal(SIGSTOP, signal_callback_handler);   //   17 /* Stop, unblockable.  */
+    signal(SIGTSTP, signal_callback_handler);   //   18 /* Keyboard stop.  */
+    signal(SIGCONT, signal_callback_handler);   //   19 /* Continue.  */
+    signal(SIGCHLD, signal_callback_handler);   //   20 /* Child terminated or stopped.  */
+    signal(SIGTTIN, signal_callback_handler);   //   21 /* Background read from control terminal.  */
+    signal(SIGTTOU, signal_callback_handler);   //   22 /* Background write to control terminal.  */
+    signal(SIGPOLL, signal_callback_handler);   //   23 /* Pollable event occurred (System V).  */
+    signal(SIGXCPU, signal_callback_handler);   //   24 /* CPU time limit exceeded.  */
+    signal(SIGXFSZ, signal_callback_handler);   //   25 /* File size limit exceeded.  */
+    signal(SIGVTALRM, signal_callback_handler); //   26 /* Virtual timer expired.  */
+    signal(SIGPROF, signal_callback_handler);   //   27 /* Profiling timer expired.  */
+    signal(SIGUSR1, signal_callback_handler);   //   30 /* User-defined signal 1.  */
+    signal(SIGUSR2, signal_callback_handler);   //   31 /* User-defined signal 2.  */
+    signal(137, signal_callback_handler);   //   137 /* LLDB?  */
 }
 
 #if __has_include(<libunwind.h>) && defined(DMRG_HAS_UNWIND)
@@ -62,6 +103,7 @@ void debug::register_callbacks() {
     #include <memory>
     #include <unistd.h>
     #include <vector>
+
 std::string getexepath() {
     static char result[PATH_MAX];
     ssize_t     count = readlink("/proc/self/exe", result, PATH_MAX);
