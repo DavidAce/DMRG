e10d1823a404bb5c0f5ff73f146a39df6f5318ed
DavidAce <aceituno@kth.se>
2021-12-15 17:40:28 +0100

    Handle on/off in build.py

--- build.py
+++ build.py
@@ -12,9 +12,9 @@ import psutil
 def str2bool(v):
     if isinstance(v, bool):
             return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', 'on'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ('no', 'false', 'f', 'n', '0', 'off'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
