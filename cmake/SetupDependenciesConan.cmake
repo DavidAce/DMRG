9e38203484a4301819893b0de18dbb10890dd50c
DavidAce <aceituno@kth.se>
2022-06-21 20:30:40 +0200

    Formatting

--- cmake/SetupDependenciesConan.cmake
+++ cmake/SetupDependenciesConan.cmake
@@ -1,8 +1,8 @@
 
-if(DMRG_PACKAGE_MANAGER MATCHES "conan")
+if (DMRG_PACKAGE_MANAGER MATCHES "conan")
 
     #  Make sure we use DMRG's own find modules
-    list(INSERT CMAKE_MODULE_PATH 0  ${PROJECT_SOURCE_DIR}/cmake)
+    list(INSERT CMAKE_MODULE_PATH 0 ${PROJECT_SOURCE_DIR}/cmake)
     find_package(Threads REQUIRED)
     find_package(OpenMP COMPONENTS CXX REQUIRED)
     find_package(Fortran REQUIRED)
@@ -11,7 +11,7 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
     ##############################################################################
     ###  Optional Intel MKL support. Uses OpenBLAS as fall-back                ###
     ##############################################################################
-    if(DMRG_ENABLE_MKL)
+    if (DMRG_ENABLE_MKL)
         find_package(MKL COMPONENTS blas lapack gf gnu_thread lp64 REQUIRED)  # MKL - Intel's math Kernel Library, use the BLAS implementation in Eigen and Arpack. Includes lapack.
         add_library(lapacke::lapacke ALIAS mkl::mkl) # Lapacke is included
 
@@ -27,13 +27,13 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
                 OPTIONS arpack-ng:blas_libraries=${MKL_LIBRARIES}
                 OPTIONS arpack-ng:lapack_libraries=${MKL_LIBRARIES}
                 )
-    else()
-        if(OPENBLAS_DYNAMIC_ARCH)
+    else ()
+        if (OPENBLAS_DYNAMIC_ARCH)
             list(APPEND DMRG_CONAN_OPTIONS OPTIONS openblas:dynamic_arch=True)
-        else()
+        else ()
             list(APPEND DMRG_CONAN_OPTIONS OPTIONS openblas:dynamic_arch=False)
-        endif()
-    endif()
+        endif ()
+    endif ()
 
 
     ##################################################################
@@ -47,24 +47,24 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
 
 
     # Download cmake-conan integrator
-    if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan/conan.cmake")
+    if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan/conan.cmake")
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
         file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
                 "${CMAKE_BINARY_DIR}/conan/conan.cmake"
                 EXPECTED_HASH MD5=81d5eab13a49f43527e35a90bfac6960
                 TLS_VERIFY ON)
-    endif()
+    endif ()
     include(${CMAKE_BINARY_DIR}/conan/conan.cmake)
 
-    if(BUILD_SHARED_LIBS)
+    if (BUILD_SHARED_LIBS)
         list(APPEND DMRG_CONAN_OPTIONS OPTIONS "*:shared=True")
-    else()
+    else ()
         list(APPEND DMRG_CONAN_OPTIONS OPTIONS "*:shared=False")
-    endif()
+    endif ()
 
-    if(CMAKE_BUILD_TYPE MATCHES "Debug")
+    if (CMAKE_BUILD_TYPE MATCHES "Debug")
         list(APPEND DMRG_CONAN_OPTIONS OPTIONS "ceres-solver:use_glog=False")
-    endif()
+    endif ()
 
     conan_add_remote(CONAN_COMMAND ${CONAN_COMMAND} NAME conan-dmrg INDEX 0 URL https://thinkstation.duckdns.org/artifactory/api/conan/conan-dmrg)
     conan_cmake_autodetect(CONAN_AUTODETECT)
@@ -87,9 +87,9 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
     ##################################################################
     ### Find all the things!                                       ###
     ##################################################################
-    if(NOT CONAN_CMAKE_SILENT_OUTPUT)
+    if (NOT CONAN_CMAKE_SILENT_OUTPUT)
         set(CONAN_CMAKE_SILENT_OUTPUT OFF) # Default is off
-    endif()
+    endif ()
     list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/conan)
     list(PREPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}/conan)
     # Use CONFIG to avoid MODULE mode. This is recommended for the cmake_find_package_multi generator
@@ -112,15 +112,7 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
         add_library(LAPACK::LAPACK ALIAS OpenBLAS::OpenBLAS)
         add_library(lapacke::lapacke ALIAS OpenBLAS::OpenBLAS)
     endif ()
-    if (TARGET eigen::eigen AND NOT TARGET Eigen3::Eigen)
-        add_library(Eigen3::Eigen ALIAS eigen::eigen)
-    endif ()
-    if (TARGET ceres-solver::ceres-solver AND NOT TARGET Ceres::Ceres)
-        add_library(Ceres::ceres ALIAS ceres-solver::ceres-solver)
-    endif ()
-    if (TARGET backward-cpp::backward-cpp AND NOT TARGET Backward::Backward)
-        add_library(Backward::Backward ALIAS backward-cpp::backward-cpp)
-    endif ()
+
     target_link_libraries(dmrg-deps INTERFACE
             CLI11::CLI11
             h5pp::h5pp
@@ -129,29 +121,5 @@ if(DMRG_PACKAGE_MANAGER MATCHES "conan")
             Ceres::ceres
             Backward::Backward
             BLAS::BLAS
-            backward-cpp::backward-cpp
             )
-
-    if (TARGET libunwind::libunwind)
-        target_compile_definitions(dmrg-deps INTERFACE DMRG_HAS_UNWIND=1)
-        target_link_libraries(dmrg-deps INTERFACE libunwind::libunwind)
-    endif ()
-
-    # Configure Eigen
-    if (TARGET eigen::eigen)
-        target_compile_definitions(eigen::eigen INTERFACE EIGEN_USE_THREADS)
-        if (TARGET BLAS::BLAS)
-            target_link_libraries(eigen::eigen INTERFACE BLAS::BLAS)
-            target_compile_definitions(eigen::eigen INTERFACE EIGEN_USE_BLAS)
-            target_compile_definitions(eigen::eigen INTERFACE EIGEN_USE_LAPACKE_STRICT)
-            if (TARGET mkl::mkl)
-                message(STATUS "Eigen will use MKL")
-                target_compile_definitions(eigen::eigen INTERFACE EIGEN_USE_MKL_ALL)
-            else ()
-                message(STATUS "Eigen will use OpenBLAS")
-            endif ()
-        endif ()
-    else ()
-        message(FATAL_ERROR "Target not defined: eigen::eigen")
-    endif ()
-endif()
+endif ()
