234b585083e23e5e47ea9aabbd797c6c707c0075
DavidAce <aceituno@kth.se>
2022-06-21 20:30:06 +0200

    Fixed issues linking to backward, glog and libunwind

--- cmake/SetupDependenciesCMake.cmake
+++ cmake/SetupDependenciesCMake.cmake
@@ -1,7 +1,6 @@
 
 if (DMRG_PACKAGE_MANAGER STREQUAL "cmake")
     include(cmake/InstallPackage.cmake)
-    include(cmake/PrintTargetInfo.cmake)
     list(APPEND h5pp_ARGS -DEigen3_ROOT:PATH=${DMRG_DEPS_INSTALL_DIR})
     list(APPEND h5pp_ARGS -DH5PP_PACKAGE_MANAGER:STRING=cmake)
     list(APPEND h5pp_ARGS -DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE_MAKEFILE})
@@ -61,19 +60,17 @@ if (DMRG_PACKAGE_MANAGER STREQUAL "cmake")
     # Starting from here there should definitely be blas library that includes lapacke
     # Lapacke is needed by arpack++, included in MKL or OpenBLAS
     find_package(Lapacke REQUIRED)
-    print_target_info(BLAS::BLAS "| ")
+
     # Iterative Eigenvalue solver for a few eigenvalues/eigenvectors using Arnoldi method.
     install_package(arpack-ng VERSION 3.8.0
             TARGET_NAME ARPACK::ARPACK
             DEPENDS BLAS::BLAS LAPACK::LAPACK gfortran::gfortran)
-    print_target_info(ARPACK::ARPACK "| ")
 
     # C++ frontend for arpack-ng. Custom find module.
     install_package(arpack++
             TARGET_NAME arpack++::arpack++
             DEPENDS ARPACK::ARPACK lapacke::lapacke gfortran::gfortran
             MODULE CHECK)
-    print_target_info(arpack++::arpack++ "| ")
 
     # Eigen3 numerical library (needed by ceres and h5pp)
     install_package(Eigen3 VERSION 3.4 TARGET_NAME Eigen3::Eigen)
@@ -81,20 +78,26 @@ if (DMRG_PACKAGE_MANAGER STREQUAL "cmake")
     # cli11 for parsing cli arguments
     install_package(cli11 VERSION 2.1.1 TARGET_NAME CLI11::CLI11 FIND_NAME CLI11)
 
-    # Backward for printing pretty stack traces
-    install_package(Backward)
 
     # h5pp for writing to file binary in format
     install_package(h5pp VERSION 1.10.0 CMAKE_ARGS ${h5pp_ARGS})
+
+    # Backward for printing pretty stack traces
+    install_package(Backward MODULE)
 
     # Google Flags library needed by ceres-solver
     install_package(gflags VERSION 2.2.2 COMPONENTS ${GFLAGS_COMPONENTS} ${GFLAGS_ITEMS})
 
     # Google logging library needed by ceres-solver
-    install_package(glog VERSION 0.5 CMAKE_ARGS -Dgflags_ROOT:PATH=${DMRG_DEPS_INSTALL_DIR} CHECK)
+    install_package(glog VERSION 0.6
+            #            DEPENDS libunwind::libunwind
+            CMAKE_ARGS
+            -Dgflags_ROOT:PATH=${DMRG_DEPS_INSTALL_DIR}
+            #                -DUnwind_ROOT:PATH=${DMRG_DEPS_INSTALL_DIR}
+            CHECK)
 
     # ceres-solver (for L-BFGS routine)
-    install_package(Ceres VERSION 2.0
+    install_package(Ceres VERSION 2.1.0
             TARGET_NAME Ceres::ceres
             DEPENDS gflags glog::glog
             CMAKE_ARGS ${Ceres_ARGS}
@@ -110,35 +113,4 @@ if (DMRG_PACKAGE_MANAGER STREQUAL "cmake")
             BLAS::BLAS
             Backward::Backward
             )
-
-    if (TARGET unwind::unwind)
-        target_compile_definitions(dmrg-deps INTERFACE DMRG_HAS_UNWIND=1)
-        target_link_libraries(dmrg-depsINTERFACE unwind::unwind)
-        target_link_libraries(Ceres::ceres INTERFACE unwind::unwind)
-        if (TARGET glog::glog)
-            target_link_libraries(glog::glog INTERFACE unwind::unwind)
-        endif ()
-    endif ()
-
-    # Configure Eigen
-    if (TARGET Eigen3::Eigen)
-        target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_USE_THREADS)
-        get_target_property(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
-        target_include_directories(Eigen3::Eigen SYSTEM INTERFACE ${EIGEN3_INCLUDE_DIR})
-        if (TARGET mkl::mkl)
-            message(STATUS "Eigen3 will use MKL")
-            target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_USE_MKL_ALL)
-            target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_USE_LAPACKE_STRICT)
-            target_link_libraries(Eigen3::Eigen INTERFACE mkl::mkl)
-        elseif (TARGET OpenBLAS::OpenBLAS)
-            message(STATUS "Eigen3 will use OpenBLAS")
-            target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_USE_BLAS)
-            target_compile_definitions(Eigen3::Eigen INTERFACE EIGEN_USE_LAPACKE_STRICT)
-            target_link_libraries(Eigen3::Eigen INTERFACE OpenBLAS::OpenBLAS)
-        endif ()
-    else ()
-        message(FATAL_ERROR "Target not defined: Eigen3::Eigen")
-    endif ()
-
-
 endif ()
--- source/debug/stacktrace.cpp
+++ source/debug/stacktrace.cpp
@@ -1,10 +1,7 @@
 #include "stacktrace.h"
-#include <climits>
 #include <csignal>
 #include <cstdio>
 #include <cstdlib>
-#include <cxxabi.h>
-#include <stdexcept>
 
 void debug::signal_callback_handler(int status) {
     switch(status) {
@@ -114,97 +111,6 @@ void debug::print_stack_trace() {
     backward::Printer p;
     p.print(st);
 }
-
-#elif __has_include(<libunwind.h>) && defined(DMRG_HAS_UNWIND)
-
-    #define UNW_LOCAL_ONLY
-    #include <array>
-    #include <climits>
-    #include <cstdio>
-    #include <cstdlib>
-    #include <execinfo.h>
-    #include <libunwind.h>
-    #include <memory>
-    #include <unistd.h>
-    #include <vector>
-
-std::string getexepath() {
-    static char result[PATH_MAX];
-    ssize_t     count = readlink("/proc/self/exe", result, PATH_MAX);
-    return std::string(result, (count > 0) ? static_cast<unsigned long>(count) : 0);
-}
-
-std::string sh(std::string_view cmd) {
-    std::array<char, 256> buffer{};
-    std::string           result;
-    std::shared_ptr<FILE> pipe(popen(cmd.data(), "r"), pclose);
-    if(!pipe) return std::string();
-    while(!feof(pipe.get())) {
-        if(fgets(buffer.data(), 256, pipe.get()) != nullptr) { result += buffer.data(); }
-    }
-    return result;
-}
-
-std::string getErrorLocation(unw_word_t addr) {
-    // our program need to be passed after the -e parameter
-    auto exec_path = getexepath();
-    // Generate a hex address for addr2line
-    char hex_addr[16];
-    snprintf(hex_addr, 16, "0x%lx", addr);
-
-    // Generate the command and call it
-    std::string cmd       = "addr2line --exe " + exec_path + " --functions --demangle " + hex_addr;
-    auto        locString = sh(cmd);
-    if(locString.empty()) return locString;
-
-    // Format the string
-    size_t start_pos = 0;
-    while((start_pos = locString.find('\n', start_pos)) != std::string::npos) {
-        if(start_pos >= locString.size() - 1)
-            locString.replace(start_pos, 1, "");
-        else
-            locString.replace(start_pos, 1, " in file ");
-        start_pos += 1;
-    }
-    if(locString.find("??") != std::string::npos)
-        return std::string();
-    else
-        return locString;
-}
-
-void debug::print_stack_trace() {
-    unw_cursor_t  cursor;
-    unw_context_t context;
-    // Initialize cursor to current frame for local unwinding.
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
-    int count = 0;
-    // Unwind frames one by one, going up the frame stack.
-    while(unw_step(&cursor) > 0) {
-        unw_word_t offset, ip;
-        unw_get_reg(&cursor, UNW_REG_IP, &ip);
-        if(ip == 0) { break; }
-        char sym[1024];
-        if(unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
-            std::printf("#%-3d 0x%-10lx", count++, ip);
-            auto estr = getErrorLocation(ip);
-            if(not estr.empty())
-                std::printf(" %s\n", estr.c_str());
-            else {
-                int   status;
-                char *nameptr   = sym;
-                char *demangled = abi::__cxa_demangle(sym, nullptr, nullptr, &status);
-                if(status == 0) { nameptr = demangled; }
-                std::printf(" %s\n", nameptr);
-                std::free(demangled);
-            }
-
-        } else {
-            std::printf(" -- error: unable to obtain symbol name for this frame\n");
-        }
-    }
-}
-
 #else
 void debug::print_stack_trace() {}
 #endif
