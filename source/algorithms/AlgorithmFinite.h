5e82a705fa0ab85837654ee1452d1163cf615e31
DavidAce <aceituno@kth.se>
2022-08-10 13:30:43 +0200

    Add rebuild_mpo() to treat it the same as rebuild_mpo_square()

--- source/algorithms/AlgorithmFinite.h
+++ source/algorithms/AlgorithmFinite.h
@@ -34,6 +34,7 @@ class AlgorithmFinite : public AlgorithmBase {
     void         try_moving_sites();
     void         move_center_point(std::optional<long> num_moves = std::nullopt);
     void         shift_mpo_energy();
+    void         rebuild_mpo();
     void         rebuild_mpo_squared();
     void         update_variance_max_digits(std::optional<double> energy = std::nullopt) final;
     void         update_bond_dimension_limit() final;
