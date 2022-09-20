86e68badac491a6250445ae5b8fab8dfd8a4c2f8
DavidAce <aceituno@kth.se>
2022-09-20 10:21:35 +0200

    Improved deprecation messages

--- cmake/Deprecation.cmake
+++ cmake/Deprecation.cmake
@@ -10,7 +10,9 @@ endif()
 
 if(DMRG_PRINT_INFO)
     message(DEPRECATION "The option [DMRG_PRINT_INFO] has been deprecated. Replace by:\n"
-            "Use the built-in CMake CLI option --loglevel=[TRACE|DEBUG|VERBOSE|STATUS...] instead")
+            "CMake CLI option --loglevel=[TRACE|DEBUG|VERBOSE|STATUS...] or\n"
+            "set CMAKE_MESSAGE_LOG_LEVEL=[TRACE|DEBUG|VERBOSE|STATUS...]"
+            )
 endif()
 
 if(DMRG_PROFILE_BUILD)
@@ -30,7 +32,7 @@ endif()
 
 if(DMRG_ENABLE_LTO)
     message(FATAL_ERROR "The option [DMRG_ENABLE_LTO] has been deprecated. Replace by:\n"
-            "COMPILER_ENABLE_LTO:BOOL=[TRUE|FALSE]")
+            "COMPILER_ENABLE_LTOCMAKE_INTERPROCEDURAL_OPTIMIZATION:BOOL=[TRUE|FALSE]")
 endif()
 
 if(DMRG_ENABLE_PCH)
@@ -48,6 +50,10 @@ if(COMPILER_MARCH)
 endif()
 if(COMPILER_MTUNE)
     message(FATAL_ERROR "The option [COMPILER_MTUNE] has been deprecated.")
+endif()
+if(COMPILER_ENABLE_LTO)
+    message(FATAL_ERROR "The option [COMPILER_ENABLE_LTO] has been deprecated.. Replace by:\n"
+            "CMAKE_INTERPROCEDURAL_OPTIMIZATION:BOOL=[TRUE|FALSE]")
 endif()
 
 if(DMRG_MARCH)
