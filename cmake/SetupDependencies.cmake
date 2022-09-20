7c7849b796de81a557b18edd86ffc988c2d86ffc
DavidAce <aceituno@kth.se>
2022-09-20 10:27:04 +0200

    Remove lbfgspp and stlbfgs

--- cmake/SetupDependencies.cmake
+++ cmake/SetupDependencies.cmake
@@ -1,9 +1,5 @@
 
-# Find threads
-set(THREADS_PREFER_PTHREAD_FLAG TRUE CACHE INTERNAL "")
 find_package(Threads REQUIRED)
-
-# Find OpenMP
 find_package(OpenMP COMPONENTS CXX REQUIRED)
 target_link_libraries(dmrg-flags INTERFACE OpenMP::OpenMP_CXX)
 
@@ -21,12 +17,6 @@ include(cmake/InstallPackage.cmake)
 
 install_package(primme MODULE)
 target_link_libraries(dmrg-deps INTERFACE primme::primme)
-
-install_package(stlbfgs MODULE)
-target_link_libraries(dmrg-deps INTERFACE stlbfgs::stlbfgs)
-
-install_package(lbfgspp)
-target_link_libraries(dmrg-deps INTERFACE lbfgspp)
 
 if(DMRG_ENABLE_TBLIS)
     install_package(tblis MODULE)
