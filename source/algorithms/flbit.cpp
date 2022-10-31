d41dd154f6522829ce20520c37f11fc9a19f6272
DavidAce <aceituno@kth.se>
2022-10-31 13:05:03 +0100

    Tuned parameters to cover doring lbit stats calculation

--- source/algorithms/flbit.cpp
+++ source/algorithms/flbit.cpp
@@ -671,8 +671,8 @@ void flbit::write_to_file(StorageEvent storage_event, CopyPolicy copy_policy) {
         auto frange = std::vector<double>{settings::model::lbit::f_mixer};
         auto sample = settings::flbit::compute_lbit_stats;
         if(sample > 1) {
-            urange = num::range<size_t>(3, 5);
-            frange = num::range<double>(0.025, 0.425, 0.025);
+            urange = num::range<size_t>(4, 6);
+            frange = num::range<double>(0.1, 0.40, 0.05);
         }
         if(sample > 0) {
             tools::log->info("Computing the lbit characteristic length-scale");
