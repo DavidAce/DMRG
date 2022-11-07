07b454ecabe606e2d38e3ebfbee0e7d549d8e76d
DavidAce <aceituno@kth.se>
2022-11-07 11:20:22 +0100

    Add new fmt formatter specialization for complex types

--- source/io/fmt.h
+++ source/io/fmt.h
@@ -3,11 +3,23 @@
 #include <fmt/compile.h>
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <fmt/ostream.h>
 #include <fmt/ranges.h>
 
 #if defined(FMT_HEADER_ONLY)
     #pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
+#endif
+
+#if defined(FMT_FORMAT_H_) && !defined(FMT_USE_COMPLEX)
+    #define FMT_USE_COMPLEX
+    #include <complex>
+    #include <type_traits>
+template<typename T>
+struct fmt::formatter<std::complex<T>, char, std::enable_if_t<std::is_arithmetic_v<T>>> : fmt::formatter<typename std::complex<T>::value_type> {
+    template<typename FormatContext>
+    auto format(const std::complex<T> &number, FormatContext &ctx) const {
+        return fmt::format_to(ctx.out(), "{0}{1:+}i", number.real(), number.imag());
+    }
+};
 #endif
 
 #define FMT_EXTERN extern
