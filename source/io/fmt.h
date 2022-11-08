36e4d3263cd878a719f0bbcc28a544432e8c46a9
DavidAce <aceituno@kth.se>
2022-11-08 13:04:50 +0100

    Add new fmt/std.h header

--- source/io/fmt.h
+++ source/io/fmt.h
@@ -4,7 +4,7 @@
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
-
+#include <fmt/std.h>
 #if defined(FMT_HEADER_ONLY)
     #pragma message "{fmt} has been included as header-only library. This causes large compile-time overhead"
 #endif
