f40e6f13ebb086c8a14d95ba2250d8d68064e105
DavidAce <aceituno@kth.se>
2023-01-17 13:33:25 +0100

    Add new constructor and const overloads for tenx::span

--- source/math/tenx/span.h
+++ source/math/tenx/span.h
@@ -14,6 +14,7 @@ namespace tenx {
         span(T *ptr, size_type len) noexcept : ptr_{ptr}, len_{static_cast<std::size_t>(len)} {
             static_assert(std::is_integral_v<size_type>);
         }
+        span(T *bgn, T *end) noexcept : ptr_{bgn}, len_{static_cast<std::size_t>(std::distance(bgn, end))} {}
         template<auto rank, int options>
         span(Eigen::Tensor<T, rank, options> &t) noexcept : ptr_{t.data()}, len_{static_cast<std::size_t>(t.size())} {}
         span(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &m) noexcept : ptr_{m.data()}, len_{static_cast<std::size_t>(m.size())} {}
@@ -29,8 +30,11 @@ namespace tenx {
 
         [[nodiscard]] std::size_t size() const noexcept { return len_; }
 
-        T *begin() noexcept { return ptr_; }
-
-        T *end() noexcept { return ptr_ + len_; }
+        T       *data() noexcept { return ptr_; }
+        const T *data() const noexcept { return ptr_; }
+        T       *begin() noexcept { return ptr_; }
+        T       *end() noexcept { return ptr_ + len_; }
+        const T *begin() const noexcept { return ptr_; }
+        const T *end() const noexcept { return ptr_ + len_; }
     };
 }
