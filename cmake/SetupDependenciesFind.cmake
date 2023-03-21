5da5e51f024b0bb7b2fcc587af6f2bed75d7e108
DavidAce <aceituno@kth.se>
2023-03-21 18:20:05 +0100

    Improved cmake-conan integration

--- cmake/SetupDependenciesFind.cmake
+++ cmake/SetupDependenciesFind.cmake
@@ -4,9 +4,9 @@ if(DMRG_PACKAGE_MANAGER MATCHES "find")
     ##############################################################################
     ###  Optional Intel MKL support. Uses OpenBLAS as fall-back                ###
     ##############################################################################
-    if (DMRG_ENABLE_MKL)
+    if(DMRG_ENABLE_MKL)
         find_package(MKL COMPONENTS blas lapack gf gnu_thread lp64 REQUIRED MODULE)  # MKL - Intel's math Kernel Library, use the BLAS implementation in Eigen and Arpack. Includes lapack.
-    else ()
+    else()
         find_package(OpenBLAS 0.3.20 REQUIRED)
         target_compile_definitions(OpenBLAS::OpenBLAS INTERFACE OPENBLAS_AVAILABLE)
         target_compile_definitions(OpenBLAS::OpenBLAS INTERFACE lapack_complex_float=std::complex<float>)
@@ -15,24 +15,24 @@ if(DMRG_PACKAGE_MANAGER MATCHES "find")
         add_library(BLAS::BLAS ALIAS OpenBLAS::OpenBLAS)
         add_library(LAPACK::LAPACK ALIAS OpenBLAS::OpenBLAS)
         add_library(lapacke::lapacke ALIAS OpenBLAS::OpenBLAS)
-    endif ()
+    endif()
 
 
-    find_package(Lapacke REQUIRED)                                         # Lapacke needed by arpack++, included in MKL or OpenBLAS
-    find_package(pcg-cpp REQUIRED)
-    find_package(Eigen3 3.4.0 REQUIRED)                                         # Eigen3 numerical library (needed by ceres and h5pp)
-    find_package(h5pp 1.11.0 REQUIRED)                                         # h5pp for writing to file binary in format
-    find_package(Ceres 2.0 REQUIRED PATH_SUFFIXES ceres ceres/lib)           # ceres-solver (for L-BFGS routine)
-    find_package(CLI11 2.1.1 REQUIRED)                                         # Command line argument parser
-    find_package(arpack-ng 3.8.0 REQUIRED)                                         # Iterative Eigenvalue solver for a few eigenvalues/eigenvectors using Arnoldi method.
-    find_package(Backward 1.6 REQUIRED)
+    find_package(Lapacke           REQUIRED)                                         # Lapacke needed by arpack++, included in MKL or OpenBLAS
+    find_package(pcg-cpp           REQUIRED)
+    find_package(Eigen3     3.4.0  REQUIRED)                                         # Eigen3 numerical library (needed by ceres and h5pp)
+    find_package(h5pp       1.11.0 REQUIRED)                                         # h5pp for writing to file binary in format
+    find_package(Ceres      2.0    REQUIRED PATH_SUFFIXES ceres ceres/lib)           # ceres-solver (for L-BFGS routine)
+    find_package(CLI11      2.1.1  REQUIRED)                                         # Command line argument parser
+    find_package(arpack-ng  3.8.0  REQUIRED)                                         # Iterative Eigenvalue solver for a few eigenvalues/eigenvectors using Arnoldi method.
+    find_package(Backward   1.6    REQUIRED)
     # Arpack needs to link to extra libraries before moving on
-    if (arpack-ng_FOUND AND TARGET ARPACK::ARPACK)
+    if(arpack-ng_FOUND AND TARGET ARPACK::ARPACK)
         target_link_libraries(ARPACK::ARPACK INTERFACE BLAS::BLAS LAPACK::LAPACK gfortran::gfortran)
-    endif ()
-    if (TARGET arpack-ng::arpack-ng)
+    endif()
+    if(TARGET arpack-ng::arpack-ng)
         add_library(ARPACK::ARPACK ALIAS arpack-ng::arpack-ng)
-    endif ()
+    endif()
     find_package(arpack++ 2.3.0 REQUIRED)                                                 # C++ frontend for arpack-ng. Custom find module.
 
 
@@ -60,6 +60,4 @@ if(DMRG_PACKAGE_MANAGER MATCHES "find")
     else ()
         target_compile_definitions(dmrg-deps INTERFACE CERES_NO_CUDA)
     endif ()
-
-
 endif()
