4696e28a32cc4b3947cb6a938746f54a46e902a9
DavidAce <aceituno@kth.se>
2023-05-08 13:16:01 +0200

    clang-format

--- cmake/compile/Ceres.cpp
+++ cmake/compile/Ceres.cpp
@@ -1,16 +1,14 @@
 #include <ceres/ceres.h>
-            #include <glog/logging.h>
-            // f(x,y) = (1-x)^2 + 100(y - x^2)^2;
-            class Rosenbrock : public ceres::FirstOrderFunction {
+#include <glog/logging.h>
+// f(x,y) = (1-x)^2 + 100(y - x^2)^2;
+class Rosenbrock : public ceres::FirstOrderFunction {
     public:
     virtual ~Rosenbrock() {}
-    virtual bool Evaluate(const double* parameters,
-                          double* cost,
-                          double* gradient) const {
+    virtual bool Evaluate(const double *parameters, double *cost, double *gradient) const {
         const double x = parameters[0];
         const double y = parameters[1];
-        cost[0] = (1.0 - x) * (1.0 - x) + 100.0 * (y - x * x) * (y - x * x);
-        if (gradient != nullptr) {
+        cost[0]        = (1.0 - x) * (1.0 - x) + 100.0 * (y - x * x) * (y - x * x);
+        if(gradient != nullptr) {
             gradient[0] = -2.0 * (1.0 - x) - 200.0 * (y - x * x) * 2.0 * x;
             gradient[1] = 200.0 * (y - x * x);
         }
@@ -18,13 +16,14 @@
     }
     virtual int NumParameters() const { return 2; }
 };
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
-    double parameters[2] = {-1.2, 1.0};
+    double                                parameters[2] = {-1.2, 1.0};
     ceres::GradientProblemSolver::Options options;
     options.minimizer_progress_to_stdout = true;
     ceres::GradientProblemSolver::Summary summary;
-    ceres::GradientProblem problem(new Rosenbrock());
+    ceres::GradientProblem                problem(new Rosenbrock());
     ceres::Solve(options, problem, parameters, &summary);
     std::cout << summary.FullReport() << std::endl;
     return 0;
