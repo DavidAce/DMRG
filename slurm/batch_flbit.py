cccdee5c766b8c01b2b1a7d08d522b3d7a86a0ca
DavidAce <aceituno@kth.se>
2023-05-22 18:33:55 +0200

    Preparing batch for large system sizes

--- slurm/batch_flbit.py
+++ slurm/batch_flbit.py
@@ -33,13 +33,13 @@ input_prefix      = "input"
 output_prefix     = "output"
 
 
-sites               = [12,16]
+sites               = [24]
 J                   = [[0.00, 0.00, 0.00]]
 w                   = [[1.00, 1.00, 1.00]] # for w2, nearest neighbors have this order of magnitude
 x                   = [1.0]
 r                   = [-1]
 u_depth             = [16]
-u_fmix              = [0.2,0.3,0.4,0.5]
+u_fmix              = [0.2,0.3,0.4]
 u_tstd              = [1.0]
 u_cstd              = [1.0]
 u_tgw8              = ['IDENTITY']
@@ -113,11 +113,11 @@ for val_L,val_J,val_w, val_x, val_r, val_u, val_f, val_tstd, val_cstd, val_tgw8,
         "model::lbit::u_tgw8"                : str_tgw8,
         "model::lbit::u_cgw8"                : str_cgw8,
         "flbit::bond_max"                    : "2048",
-        "flbit::time_start_real"             : "1e-8",
+        "flbit::time_start_real"             : "1e-1",
         "flbit::time_start_imag"             : "0",
-        "flbit::time_final_real"             : "1e0", #"{:.1e}".format(max_time),
+        "flbit::time_final_real"             : "{:.1e}".format(max_time),
         "flbit::time_final_imag"             : "0",
-        "flbit::time_num_steps"              : "25",
+        "flbit::time_num_steps"              : "100",
         "flbit::cls::mpo_circuit_svd_bondlim": str_bond,
     }
     os.makedirs(input_prefix, exist_ok=True)
