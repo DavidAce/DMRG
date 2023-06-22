eb3f53d8815f52906155faa57fb7325d43cc32f1
DavidAce <aceituno@kth.se>
2023-06-22 12:48:24 +0200

    Tidy up rclone log messages

--- slurm/run_jobarray.sh
+++ slurm/run_jobarray.sh
@@ -62,11 +62,11 @@ rclone_copy_from_remote() {
   fi
   file_remote="$rclone_remote/$rclone_prefix/$1"
   file_remote_lsf=$(rclone lsf $file_remote)
-  echo "RCLONE CHECK REMOTE      : $file_remote"
-  echo "-- expected              : $2"
-  echo "-- found                 : $file_remote_lsf"
+  #echo "RCLONE CHECK REMOTE      : $file_remote"
+  #echo "-- expected              : $2"
+  #echo "-- found                 : $file_remote_lsf"
   if [ "$file_remote_lsf" == "$2" ]; then
-    echo "-- copy remote -> local  : $1"
+    echo "RCLONE COPY REMOTE->LOCAL: $1"
     rclone copyto $file_remote $1 -L --update
   fi
 }
