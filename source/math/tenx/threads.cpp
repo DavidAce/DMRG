1c778bcd43bb8e54ee71d05192f86c8fdb971afe
DavidAce <aceituno@kth.se>
2023-08-24 07:32:42 +0200

    Disabled openmp soft pause

--- source/math/tenx/threads.cpp
+++ source/math/tenx/threads.cpp
@@ -10,13 +10,14 @@ namespace tenx::threads {
     std::unique_ptr<Eigen::ThreadPool>       tp;
     std::unique_ptr<Eigen::ThreadPoolDevice> dev;
     void                                     setNumThreads(unsigned int num) {
-    #if defined(_OPENMP)
-        auto pause = omp_pause_resource_all(omp_pause_resource_t::omp_pause_soft);
-        if (pause != 0) throw std::runtime_error("omp_pause_resource_all(omp_pause_soft) failed");
-    #endif
+//    #if defined(_OPENMP)
+//        auto pause = omp_pause_resource_all(omp_pause_resource_t::omp_pause_soft);
+//        if (pause != 0) throw std::runtime_error("omp_pause_resource_all(omp_pause_soft) failed");
+//    #endif
 
         num_threads = std::clamp<unsigned int>(num, 1, std::thread::hardware_concurrency());
         if(not dev or not tp or tp->NumThreads() != static_cast<int>(num_threads)) {
+//            std::printf("Creating a new threadpool device\n");
             tp  = std::make_unique<Eigen::ThreadPool>(num_threads);
             dev = std::make_unique<Eigen::ThreadPoolDevice>(tp.get(), num_threads);
         }
