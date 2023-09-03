f06fc490f852d8f634f8c20483cd0f9099ead5df
DavidAce <aceituno@kth.se>
2023-09-03 09:28:22 +0200

    Simplified custom fmt formatters

--- source/io/fmt.h
+++ source/io/fmt.h
@@ -11,9 +11,9 @@
 
 #if defined(USE_QUADMATH)
     #include <quadmath.h>
-template<typename Char>
-struct fmt::formatter<__float128, Char> : fmt::formatter<double, Char> {
-    auto format(__float128 c, format_context &ctx) const {
+template<>
+struct fmt::formatter<__float128> : fmt::formatter<double> {
+    auto format(__float128 c, format_context &ctx) const -> decltype(ctx.out()) {
         std::string dec;
         dec.resize(64);
         quadmath_snprintf(dec.data(), 64, "%.36Qg", c);
@@ -29,52 +29,37 @@ struct fmt::formatter<__float128, Char> : fmt::formatter<double, Char> {
 
 template<typename T, typename Char>
 struct fmt::formatter<std::complex<T>, Char> : fmt::formatter<T, Char> {
-    typedef fmt::formatter<T, Char> base;
-    enum style { expr, pair } style_ = expr;
+    private:
+    typedef fmt::formatter<T, Char>         base;
     fmt::detail::dynamic_format_specs<Char> specs_;
-    FMT_CONSTEXPR auto                      parse(format_parse_context &ctx) -> decltype(ctx.begin()) {
-        using handler_type                            = fmt::detail::dynamic_specs_handler<format_parse_context>;
-        auto                                     type = fmt::detail::type_constant<T, Char>::value;
-        fmt::detail::specs_checker<handler_type> handler(handler_type(specs_, ctx), type);
-        auto                                     it = ctx.begin();
-        if(it != ctx.end()) {
-            switch(*it) {
-                case '$':
-                    style_ = style::expr;
-                    ctx.advance_to(++it);
-                    break;
-                case ',':
-                    style_ = style::pair;
-                    ctx.advance_to(++it);
-                    break;
-                default: break;
-            }
-        }
-        parse_format_specs(ctx.begin(), ctx.end(), handler);
-        // todo: fixup alignment
-        return base::parse(ctx);
-    }
+
+    public:
     template<typename FormatCtx>
     auto format(const std::complex<T> &x, FormatCtx &ctx) const -> decltype(ctx.out()) {
-        format_to(ctx.out(), "(");
-        if(style_ == style::pair) {
-            base::format(x.real(), ctx);
-            format_to(ctx.out(), ",");
-            base::format(x.imag(), ctx);
-            return format_to(ctx.out(), ")");
-        }
-        if(x.real() != 0.0 || x.imag() == 0.0) base::format(x.real(), ctx);
-        if(x.imag() != 0.0) {
-            if(x.real() >= 0 && x.imag() >= 0 && specs_.sign != sign::plus) format_to(ctx.out(), "+");
-            base::format(x.imag(), ctx);
-            format_to(ctx.out(), "i");
-            if(std::is_same<typename std::decay<T>::type, float>::value) format_to(ctx.out(), "f");
-            if(std::is_same<typename std::decay<T>::type, long double>::value) format_to(ctx.out(), "l");
-        }
-        return format_to(ctx.out(), ")");
+        base::format(x.real(), ctx);
+        if(x.imag() >= 0 && specs_.sign != sign::plus) format_to(ctx.out(), "+");
+        base::format(x.imag(), ctx);
+        return format_to(ctx.out(), "i");
     }
 };
 
+#endif
+#if defined(FMT_FORMAT_H_)
+    #include <complex>
+    #include <type_traits>
+
+template<typename T>
+struct fmt::formatter<std::reference_wrapper<T>> : fmt::formatter<T> {
+    private:
+    typedef fmt::formatter<T> base;
+
+    public:
+    template<typename FormatCtx>
+    auto format(const std::reference_wrapper<T> &x, FormatCtx &ctx) const -> decltype(ctx.out()) {
+        base::format(static_cast<T>(x), ctx);
+        return ctx.out();
+    }
+};
 #endif
 
 #define FMT_EXTERN extern
