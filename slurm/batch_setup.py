790bad91c3ddfcab55dfac0ec70f94f6d9088378
DavidAce <aceituno@kth.se>
2023-09-13 10:11:51 +0200

    Added test for initial state two down, distance 2  from the middle (2d2)

--- slurm/batch_setup.py
+++ slurm/batch_setup.py
@@ -249,7 +249,7 @@ def get_batch_setup(projectname):
 
             },
         },
-        'lbit104-2d1': {
+        'lbit103-nil': {
             'projectname': 'lbit103-nil',
             'batch': {  # Number of seeds to run
                 'L12|f0.2': {
@@ -312,6 +312,21 @@ def get_batch_setup(projectname):
         },
         'lbit104-2d1': {
             'projectname': 'lbit104-2d1',
+            'batch': {  # Number of seeds to run
+                'L16|f0.2': {
+                    'seed_extent': [50000],
+                    'seed_offset': [16200000],
+                    'time_steps': 200,
+                },
+                'L16|f0.4': {
+                    'seed_extent': [50000],
+                    'seed_offset': [16400000],
+                    'time_steps': 200,
+                },
+            },
+        },
+        'lbit105-2d2': {
+            'projectname': 'lbit105-2d2',
             'batch': {  # Number of seeds to run
                 'L16|f0.2': {
                     'seed_extent': [50000],
