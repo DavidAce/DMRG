3db748f0200f12e060256be7577ed50541b9800f
DavidAce <aceituno@kth.se>
2023-10-11 13:00:26 +0200

    Add more ranges to lbit109

--- slurm/batch-flbit-L16-linr.py
+++ slurm/batch-flbit-L16-linr.py
@@ -31,7 +31,7 @@ config_ranges = {
     "model::lbit::J1_wdth": ['1.00'],
     "model::lbit::J2_wdth": ['1.00'],
     "model::lbit::J3_wdth": ['0.00'],
-    "model::lbit::J2_span": ['1','2','3','4'],
+    "model::lbit::J2_span": ['0','1','2','3','4','5','6','7','8'],
     "model::lbit::xi_Jcls": ['1.00'],
     "model::lbit::u_depth": ['16'],
     "model::lbit::u_fmix": ['0.20'],
